e53f90adb79ca2201b056bb6d48a538c98147282
tech-somish <contact@somish.com>
2018-07-12 16:35:04 +0530

    Fix copyright header

--- contracts/imports/govblocks-protocol/MemberRoles.sol
+++ contracts/imports/govblocks-protocol/MemberRoles.sol
@@ -1,4 +1,4 @@
-/* Copyright (C) 2017 NexusMutual.io
+/* Copyright (C) 2017 GovBlocks.io
 
   This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by

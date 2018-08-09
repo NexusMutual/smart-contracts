72fac39d640b1e856bc187bc7b07761c5003e1b5
nervehammer <nervehammer.suraj@gmail.com>
2018-08-09 19:01:01 +0530

    Add basic scripts
    -based off https://github.com/OpenZeppelin/openzeppelin-solidity/tree/master/scripts

--- truffle.js
+++ truffle.js
@@ -1,24 +1,21 @@
-/*
- * NB: since truffle-hdwallet-provider 0.0.5 you must wrap HDWallet providers in a 
- * function when declaring them. Failure to do so will cause commands to hang. ex:
- * ```
- * mainnet: {
- *     provider: function() { 
- *       return new HDWalletProvider(mnemonic, 'https://mainnet.infura.io/<infura-key>') 
- *     },
- *     network_id: '1',
- *     gas: 4500000,
- *     gasPrice: 10000000000,
- *   },
- */
-
 module.exports = {
   networks: {
     development: {
-      gasPrice : 1,
-      host: "localhost",
+      host: 'localhost',
       port: 8545,
-      network_id: "*" // Match any network id
-    }
-  }
+      network_id: '*',
+    },
+    coverage: {
+      host: 'localhost',
+      network_id: '*', 
+      port: 8555,
+      gas: 0x67C280,
+      gasPrice: 0x01,
+    },
+    ganache: {
+      host: 'localhost',
+      port: 8545,
+      network_id: '*',
+    },
+  },
 };

28eeca18a00db5a9343fe2f5458f9b64db454d1f
nervehammer <nervehammer.suraj@gmail.com>
2018-08-19 07:49:54 +0200

    More test cases

--- truffle-config.js
+++ truffle-config.js
@@ -5,19 +5,19 @@ module.exports = {
   networks: {
     development: {
       host: '127.0.0.1',
-      port: 8069,
+      port: 8545,
       network_id: '5777', 
     },
     coverage: {
       host: '127.0.0.1',
       network_id: '5777', 
-      port: 8069,
+      port: 8545,
       gas: 0xfffffffffff,
       gasPrice: 0x01,
     },
     ganache: {
       host: '127.0.0.1',
-      port: 8069,
+      port: 8545,
       network_id: '5777',
     }
   },

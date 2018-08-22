92bc21a2966338428732d4e797a880b9078f4b67
nervehammer <nervehammer.suraj@gmail.com>
2018-08-22 09:59:46 +0200

    More Quotation test cases

--- test/5_Quotation.test.js
+++ test/5_Quotation.test.js
@@ -4,9 +4,12 @@ const NXMToken2 = artifacts.require("NXMToken2");
 const ClaimsReward = artifacts.require("ClaimsReward");
 const QuotationData = artifacts.require("QuotationData");
 const Quotation = artifacts.require("Quotation");
+const DAI = artifacts.require("DAI");
+
 const member = web3.eth.accounts[1];
 const coverHolder = web3.eth.accounts[4];
 const fee = web3.toWei(0.002);
+
 const QE = "0xb24919181daead6635e613576ca11c5aa5a4e133";
 const PID = 0;
 const PNAME = "0x5343430000000000";
@@ -14,15 +17,20 @@ const PHASH = "Smart Contract Cover";
 const smartConAdd = "0xd0a6e6c54dbc68db5db3a091b171a77407ff7ccf";
 const coverPeriod = 61;
 const coverDetails = [5,1176856034679443,260712457837714,7972408607];
+const coverDetailsDai = [5,16812229066849188,5694231991898,7972408607];
 const v = 27;
 const r = "0x30c1449cd8c7e4c25760e3eb31e6f5812efe9622a3db4a525b8f0e53cb749ed9";
 const s = "0x11ec3139bf601c2a31bd5e4af8e05b6f9ced428da9f4654ba5feb9018f828ee4";
+const r_dai = "0xdcaa177410672d90890f1c0a42a965b3af9026c04caedbce9731cb43827e8556";
+const s_dai = "0x2b9f34e81cbb79f9af4b8908a7ef8fdb5875dedf5a69f84cd6a80d2a4cc8efff";
 let P1;
 let nxmtk1;
 let nxmtk2;
 let cr;
 let qd;
 let qt;
+let cad;
+
 const BigNumber = web3.BigNumber;
 require('chai')
   .use(require('chai-bignumber')(BigNumber))
@@ -51,6 +59,9 @@ describe('Contract: 5_Quotation', function () {
 			return Quotation.deployed();
                 }).then(function(instance) {
                         qt = instance;
+			return DAI.deployed();
+                }).then(function(instance) {
+                        cad = instance;
                 });
 	});
 
@@ -104,7 +115,6 @@ describe('Contract: 5_Quotation', function () {
 		await qt.makeCoverUsingNXMTokens(PID, coverDetails, coverPeriod, "ETH", smartConAdd, v, r, s, {from: coverHolder});
 		let presentLockedCN = await nxmtk2.totalBalanceCNOfUser(coverHolder);
                 let presentAvailableToken = await cr.getTotalStakeCommission(member); // staker should get 20% of premium.
-                let presentPoolBalance = await P1.getEtherPoolBalance();
                 let newLockedCN = (initialLockedCN.plus(BN_5.times(new BigNumber(coverDetails[2].toString()).div(BN_100)))).toFixed(0);
                 let newAvailableToken = (initialAvailableToken.plus(BN_20.times(new BigNumber(coverDetails[2].toString()).div(BN_100)))).toFixed(0);
                 newLockedCN.should.be.bignumber.equal(presentLockedCN);
@@ -122,4 +132,35 @@ describe('Contract: 5_Quotation', function () {
                 checkd.should.equal(true);
         });
 
+	it('should able to purchase cover using currency assest i.e. DAI ', async function () {
+		await cad.transfer(coverHolder, tk);
+                let initialLockedCN = await nxmtk2.totalBalanceCNOfUser(coverHolder);
+		let initialCAbalance = await cad.balanceOf(coverHolder);
+                let initialAvailableToken = await cr.getTotalStakeCommission(member); // member=staker for smart contract
+		let initialPoolBalanceOfCA = await cad.balanceOf(P1.address);
+		let presentaCAbalance = await cad.balanceOf(coverHolder);
+		await cad.approve(P1.address, coverDetailsDai[1], {from: coverHolder});
+                await P1.makeCoverUsingCA(PID, smartConAdd,"DAI", coverDetailsDai, coverPeriod, v, r_dai, s_dai, {from: coverHolder});
+                let presentLockedCN = await nxmtk2.totalBalanceCNOfUser(coverHolder);
+		let presentCAbalance = await cad.balanceOf(coverHolder);
+                let presentAvailableToken = await cr.getTotalStakeCommission(member); // staker should get 20% of premium.
+                let presentPoolBalanceOfCA = await cad.balanceOf(P1.address);
+                let newLockedCN = (initialLockedCN.plus(BN_5.times(new BigNumber(coverDetailsDai[2].toString()).div(BN_100)))).toFixed(0);
+                let newAvailableToken = (initialAvailableToken.plus(BN_20.times(new BigNumber(coverDetailsDai[2].toString()).div(BN_100)))).toFixed(0);
+		presentCAbalance.should.be.bignumber.equal(initialCAbalance.minus(new BigNumber(coverDetailsDai[1].toString())));
+		presentPoolBalanceOfCA.should.be.bignumber.equal(initialPoolBalanceOfCA.plus(new BigNumber(coverDetailsDai[1].toString())));
+                newLockedCN.should.be.bignumber.equal(presentLockedCN);
+                newAvailableToken.should.be.bignumber.equal(presentAvailableToken);
+        });
+
+	it('should return correct cover details purchased with DAI', async function () {
+                let CID = await qd.getAllCoversOfUser(coverHolder);
+                let checkd = false;
+                let cdetails1 = await qd.getCoverDetailsByCoverID1(CID[2]);
+                let cdetails2 = await qd.getCoverDetailsByCoverID2(CID[2]);
+                if (cdetails2[1] == "0x44414900" && cdetails1[1] == PNAME && cdetails1[2] == coverHolder && cdetails1[3] == smartConAdd) {
+                        checkd = true;
+                }
+                checkd.should.equal(true);
+        });
 });

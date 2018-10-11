963e17e48e76069acc45f391c17f0402ffc01a01
nervehammer <suraj.raw120@gmail.com>
2018-10-11 16:15:06 +0530

    Add more test cases

--- test/11_MCR.js
+++ test/11_MCR.js
@@ -40,6 +40,29 @@ contract('MCR', function([owner, notOwner]) {
         await mcr.changeSF(1, { from: owner });
       });
     });
+    describe('Add new MCR Data', function() {
+      it('should be able to add MCR data', async function() {
+        await mcr.addMCRData(
+          18000,
+          10000,
+          2,
+          ['0x455448', '0x444149'],
+          [100, 65407],
+          20181011,
+          { from: owner }
+        );
+      });
+    });
+
+    describe('Adds MCR Data for last failed attempt', function() {
+      it('should be able to add MCR data', async function() {
+        await mcr.addLastMCRData(20181009, { from: owner });
+        await mcr.addLastMCRData(20181011, { from: owner });
+        console.log(await mcrd.getLastMCRDate());
+        await mcr.addLastMCRData(20181012, { from: owner });
+        console.log(await mcrd.getLastMCRDate());
+      });
+    });
   });
 
   describe('Misc', function() {
@@ -54,6 +77,47 @@ contract('MCR', function([owner, notOwner]) {
     });
     it('should be able to get all Sum Assurance', async function() {
       await mcr.getAllSumAssurance();
+      await mcrd.updateCurr3DaysAvg('0x44414900', 0, { from: owner });
+      await mcr.getAllSumAssurance();
+    });
+    it('should not be able to change master address', async function() {
+      await assertRevert(
+        mcr.changeMasterAddress(mcr.address, { from: notOwner })
+      );
+    });
+    it('should not be able to add currency', async function() {
+      await assertRevert(mcr.addCurrency('0x4c4f4c', { from: notOwner }));
+    });
+    it('should return 1 if required MCR is more than last MCR percentage', async function() {
+      await mcrd.changeMinReqMCR(19000, { from: owner });
+      (await mcr.checkForMinMCR()).should.be.bignumber.equal(1);
+    });
+    it('should not be able to add MCR data', async function() {
+      //TODO: use mock contract
+      await mcrd.pushMCRData(10000, 0, 0, 0, { from: owner });
+      await mcr.getMaxSellTokens();
+      await mcr.calculateTokenPrice(CA_ETH);
+      console.log((await mcr.calVtpAndMCRtp())[0].toString());
+      await mcr.addMCRData(
+        18000,
+        10000,
+        ether(4),
+        ['0x455448', '0x444149'],
+        [100, 65407],
+        20181013,
+        { from: owner }
+      );
+      await assertRevert(
+        mcr.addMCRData(
+          18000,
+          10000,
+          2,
+          ['0x455448', '0x444149'],
+          [100, 65407],
+          20181011,
+          { from: notOwner }
+        )
+      );
     });
   });
 });

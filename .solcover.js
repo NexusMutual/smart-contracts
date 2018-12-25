062190b5d83001fa48dd9092bd27e12eb605e8b8
nervehammer <suraj.raw120@gmail.com>
2018-12-25 18:13:55 +0530

    fix coverage

--- .solcover.js
+++ .solcover.js
@@ -2,10 +2,19 @@ module.exports = {
   port: 8555,
   norpc: true,
   deepSkip: true,
-  skipFiles: ['imports', 'Pool1.sol', 'Pool2.sol', 'PoolData.sol', 'mocks'],
+  skipFiles: [
+    'imports',
+    'Pool1.sol',
+    'Pool2.sol',
+    'PoolData.sol',
+    'EventCaller.sol',
+    'Governance.sol',
+    'ProposalCategory.sol',
+    'mocks'
+  ],
   forceParse: [
     'imports/ERC1132',
-    'imports/govblocks-protocol/Governed.sol',
+    'imports/govblocks-protocol/interfaces',
     'mocks'
   ]
 };

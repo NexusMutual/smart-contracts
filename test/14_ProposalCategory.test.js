d5896c46beef789d1527b37e040167104d7fda07
tech-somish <contact@somish.com>
2019-02-01 16:17:27 +0530

    updated governance test cases

--- test/14_ProposalCategory.test.js
+++ test/14_ProposalCategory.test.js
@@ -10,7 +10,7 @@ let tf;
 let nullAddress = '0x0000000000000000000000000000000000000000';
 const encode = require('./utils/encoder.js').encode;
 
-contract('Proposal Category', function([owner]) {
+contract('Proposal Category', function([owner, other]) {
   before(async function() {
     nxms = await NXMaster.deployed();
     let address = await nxms.getLatestAddress('PC');
@@ -18,12 +18,12 @@ contract('Proposal Category', function([owner]) {
     address = await nxms.getLatestAddress('GV');
     gv = await Governance.at(address);
     tf = await TokenFunctions.deployed();
-    await tf.payJoiningFee(owner, { value: 2000000000000000});
+    await tf.payJoiningFee(owner, { value: 2000000000000000 });
     await tf.kycVerdict(owner, true);
   });
 
   it('Should be initialized', async function() {
-  	await assertRevert(pc.proposalCategoryInitiate());
+    await assertRevert(pc.proposalCategoryInitiate());
     const g1 = await pc.totalCategories();
     const g2 = await pc.category(1);
     assert.equal(g2[1].toNumber(), 1);
@@ -33,47 +33,54 @@ contract('Proposal Category', function([owner]) {
     assert.equal(g6.toNumber(), 20);
   });
 
+  it('should not allow unauthorized to change master address', async function() {
+    await assertRevert(pc.changeMasterAddress(nxms.address, { from: other }));
+    await pc.changeMasterAddress(nxms.address);
+  });
+
   it('Should add a proposal category', async function() {
     let c1 = await pc.totalCategories();
-    await assertRevert(pc.addCategory('Yo',1,1,0,[1],1,'',nullAddress,'EX',[0, 0, 0]));
+    await assertRevert(
+      pc.addCategory('Yo', 1, 1, 0, [1], 1, '', nullAddress, 'EX', [0, 0, 0])
+    );
     //proposal to add category
-      let actionHash = encode(
-        'addCategory(string,uint,uint,uint,uint[],uint,string,address,bytes2,uint[])',
-        'Yo',
-        1,
-        1,
-        0,
-        [1],
-        1,
-        '',
-        nullAddress,
-        'EX',
-        [0, 0, 0]
-      );
-      let p1 = await gv.getProposalLength();
-      await gv.createProposalwithSolution(
-        'Add new member',
-        'Add new member',
-        'Addnewmember',
-        3,
-        'Add new member',
-        actionHash
-      );
-      await gv.submitVote(p1.toNumber(), 1);
-      await gv.closeProposal(p1.toNumber());
+    let actionHash = encode(
+      'addCategory(string,uint,uint,uint,uint[],uint,string,address,bytes2,uint[])',
+      'Yo',
+      1,
+      1,
+      0,
+      [1],
+      1,
+      '',
+      nullAddress,
+      'EX',
+      [0, 0, 0]
+    );
+    let p1 = await gv.getProposalLength();
+    await gv.createProposalwithSolution(
+      'Add new member',
+      'Add new member',
+      'Addnewmember',
+      3,
+      'Add new member',
+      actionHash
+    );
+    await gv.submitVote(p1.toNumber(), 1);
+    await gv.closeProposal(p1.toNumber());
     //proposal closed
-      // await pc.addCategory(
-      //   'Yo',
-      //   1,
-      //   1,
-      //   0,
-      //   [1],
-      //   1,
-      //   '',
-      //   nullAddress,
-      //   'EX',
-      //   [0, 0]
-      // );
+    // await pc.addCategory(
+    //   'Yo',
+    //   1,
+    //   1,
+    //   0,
+    //   [1],
+    //   1,
+    //   '',
+    //   nullAddress,
+    //   'EX',
+    //   [0, 0]
+    // );
     const c2 = await pc.totalCategories();
     assert.isAbove(c2.toNumber(), c1.toNumber(), 'category not added');
   });
@@ -82,33 +89,39 @@ contract('Proposal Category', function([owner]) {
     let c1 = await pc.totalCategories();
     c1 = c1.toNumber() - 1;
     const cat1 = await pc.category(c1);
-    await assertRevert(pc.updateCategory(c1,'Yo',1,1,0,[1],1,'',nullAddress,'EX',[0, 0, 0]));
+    await assertRevert(
+      pc.updateCategory(c1, 'Yo', 1, 1, 0, [1], 1, '', nullAddress, 'EX', [
+        0,
+        0,
+        0
+      ])
+    );
     //proposal to update category
-      let actionHash = encode(
-        'updateCategory(uint,string,uint,uint,uint,uint[],uint,string,address,bytes2,uint[])',
-        c1,
-        'YoYo',
-        3,
-        1,
-        20,
-        [1],
-        1,
-        '',
-        nullAddress,
-        'EX',
-        [0, 0, 0]
-      );
-      let p1 = await gv.getProposalLength();
-      await gv.createProposalwithSolution(
-        'Add new member',
-        'Add new member',
-        'Addnewmember',
-        4,
-        'Add new member',
-        actionHash
-      );
-      await gv.submitVote(p1.toNumber(), 1);
-      await gv.closeProposal(p1.toNumber());
+    let actionHash = encode(
+      'updateCategory(uint,string,uint,uint,uint,uint[],uint,string,address,bytes2,uint[])',
+      c1,
+      'YoYo',
+      3,
+      1,
+      20,
+      [1],
+      1,
+      '',
+      nullAddress,
+      'EX',
+      [0, 0, 0]
+    );
+    let p1 = await gv.getProposalLength();
+    await gv.createProposalwithSolution(
+      'Add new member',
+      'Add new member',
+      'Addnewmember',
+      4,
+      'Add new member',
+      actionHash
+    );
+    await gv.submitVote(p1.toNumber(), 1);
+    await gv.closeProposal(p1.toNumber());
     //proposal closed
     // will throw once owner's permissions are revoked
     // await pc.updateCategory(

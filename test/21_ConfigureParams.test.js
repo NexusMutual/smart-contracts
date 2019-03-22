ba47d7b67b425f4d8fb0dc4b2e3e595d5d760c0a
udkreddy <udkr1996@gmail.com>
2019-03-22 10:46:46 +0530

    test cases added for configurable global parameters

---
+++ test/21_ConfigureParams.test.js
@@ -0,0 +1,351 @@
+const Governance = artifacts.require('Governance');
+const ProposalCategory = artifacts.require('ProposalCategory');
+const MemberRoles = artifacts.require('MemberRoles');
+const NXMaster = artifacts.require('NXMaster');
+const PoolData = artifacts.require('PoolData');
+const EventCaller = artifacts.require('EventCaller');
+const ClaimsReward = artifacts.require('ClaimsReward');
+const TokenController = artifacts.require('TokenController');
+const TokenData = artifacts.require('TokenData');
+const NXMToken = artifacts.require('NXMToken');
+const QuotationData = artifacts.require('QuotationDataMock');
+const DAI = artifacts.require('MockDAI');
+const ClaimsData = artifacts.require('ClaimsData');
+const FactoryMock = artifacts.require('FactoryMock');
+const expectEvent = require('./utils/expectEvent');
+const assertRevert = require('./utils/assertRevert.js').assertRevert;
+const increaseTime = require('./utils/increaseTime.js').increaseTime;
+const gvProposal = require('./utils/gvProposal.js').gvProposal;
+const encode = require('./utils/encoder.js').encode;
+const AdvisoryBoard = '0x41420000';
+const TokenFunctions = artifacts.require('TokenFunctionMock');
+
+let tf;
+let gv;
+let cr;
+let pc;
+let mr;
+let tc;
+let pd;
+let td;
+let qd;
+let cd;
+let nxms;
+let proposalId;
+let pId;
+let nxmToken;
+let balance;
+let status;
+let voters;
+let maxAllowance =
+  '115792089237316195423570985008687907853269984665640564039457584007913129639935';
+
+const CLA = '0x434c41';
+const validity = 2592000;
+
+contract(
+  'Configure Global Parameters',
+  ([ab1, mem1, mem2, mem3, notMember]) => {
+    before(async function() {
+      nxms = await NXMaster.deployed();
+      tf = await TokenFunctions.deployed();
+      cr = await ClaimsReward.deployed();
+      nxmToken = await NXMToken.deployed();
+      let address = await nxms.getLatestAddress('GV');
+      gv = await Governance.at(address);
+      address = await nxms.getLatestAddress('PC');
+      pc = await ProposalCategory.at(address);
+      address = await nxms.getLatestAddress('MR');
+      mr = await MemberRoles.at(address);
+      tc = await TokenController.deployed();
+      pd = await PoolData.deployed();
+      td = await TokenData.deployed();
+      qd = await QuotationData.deployed();
+      cd = await ClaimsData.deployed();
+      await nxmToken.approve(tc.address, maxAllowance);
+      let bal = await nxmToken.balanceOf(ab1);
+      await nxmToken.approve(cr.address, maxAllowance, {
+        from: web3.eth.accounts[0]
+      });
+      await mr.payJoiningFee(web3.eth.accounts[0], {
+        value: 2000000000000000,
+        from: web3.eth.accounts[0]
+      });
+      await mr.kycVerdict(web3.eth.accounts[0], true, {
+        from: web3.eth.accounts[0]
+      });
+      // await nxmToken.transfer(notMember, 267600*1e18);
+      let balances = [150000, 150000, 150000, 150000];
+      for (let i = 1; i < 4; i++) {
+        await nxmToken.approve(cr.address, maxAllowance, {
+          from: web3.eth.accounts[i]
+        });
+        await mr.payJoiningFee(web3.eth.accounts[i], {
+          value: 2000000000000000,
+          from: web3.eth.accounts[i]
+        });
+        await mr.kycVerdict(web3.eth.accounts[i], true, {
+          from: web3.eth.accounts[0]
+        });
+        await nxmToken.transfer(web3.eth.accounts[i], balances[i] * 1e18);
+      }
+    });
+    async function updateParameter(
+      cId,
+      mrSequence,
+      code,
+      contractInst,
+      type,
+      proposedValue
+    ) {
+      let getterFunction;
+      if (type == 'uint') {
+        action = 'updateUintParameters(bytes8,uint)';
+        getterFunction = 'getUintParameters';
+      } else if (type == 'address') {
+        action = 'updateAddressParameters(bytes8,address)';
+        getterFunction = 'getAddressParameters';
+      } else if (type == 'owner') {
+        action = 'updateOwnerParameters(bytes8,address)';
+        getterFunction = 'getOwnerParameters';
+      }
+      // console.log(proposedValue);
+      let actionHash = encode(action, code, proposedValue);
+      await gvProposal(cId, actionHash, mr, gv, mrSequence);
+      let parameter = await contractInst[getterFunction](code);
+      assert.equal(parameter[1], proposedValue);
+    }
+
+    describe('Update Token Parameters', function() {
+      it('Should update Exponent in Token Price', async function() {
+        await updateParameter(19, 2, 'TOKEXP', td, 'uint', '7000000000000000');
+      });
+      it('Should update Token Step', async function() {
+        await updateParameter(19, 2, 'TOKSTEP', td, 'uint', '10000');
+      });
+      it('Should update Token Step', async function() {
+        await updateParameter(19, 2, 'QUOLOCKT', td, 'uint', '7000');
+      });
+    });
+
+    describe('Update Risk Assessment Parameters', function() {
+      it('Should update Stake Period', async function() {
+        await updateParameter(20, 2, 'RALOCKT', td, 'uint', '86400');
+      });
+      it('Should update Commission%', async function() {
+        await updateParameter(20, 2, 'RACOMM', td, 'uint', '90');
+      });
+      it('Should update Max Commission%', async function() {
+        await updateParameter(20, 2, 'RAMAXC', td, 'uint', '40');
+      });
+      it('Should update Extra CA Lock Period', async function() {
+        await updateParameter(20, 2, 'CALOCKT', td, 'uint', '86400');
+      });
+      it('Should update Extra Member Lock Period', async function() {
+        await updateParameter(20, 2, 'MVLOCKT', td, 'uint', '86400');
+      });
+      it('Should update Claim  Assessor Velocity', async function() {
+        await updateParameter(20, 2, 'CABOOKT', td, 'uint', '7000');
+      });
+      it('Should update Membership joining fee', async function() {
+        await updateParameter(20, 2, 'JOINFEE', td, 'uint', '6000000000000000');
+      });
+    });
+
+    describe('Update Governance Parameters', function() {
+      it('Should update Governance Token Holding Time', async function() {
+        await updateParameter(21, 2, 'GOVHOLD', gv, 'uint', '86400');
+      });
+      it('Should update Max Advisory Board Members', async function() {
+        await updateParameter(21, 2, 'MAXAB', gv, 'uint', '10');
+      });
+      it('Should update Emergency Pause Time', async function() {
+        await updateParameter(21, 2, 'EPTIME', gv, 'uint', '86400');
+      });
+    });
+
+    describe('Update Quotation Parameters', function() {
+      it('Should update Short Term Load Period', async function() {
+        await updateParameter(22, 2, 'STLP', qd, 'uint', '40');
+      });
+      it('Should update Short Term Load', async function() {
+        await updateParameter(22, 2, 'STL', qd, 'uint', '1000');
+      });
+      it('Should update Profit Margin', async function() {
+        await updateParameter(22, 2, 'PM', qd, 'uint', '60');
+      });
+      it('Should update Minimum Cover Period', async function() {
+        await updateParameter(22, 2, 'QUOMIND', qd, 'uint', '86400');
+      });
+      it('Should update Tokens Retained', async function() {
+        await updateParameter(22, 2, 'QUOTOK', qd, 'uint', '10000');
+      });
+    });
+
+    describe('Update Claims Assessment Parameters', function() {
+      it('Should update Max Vote Period', async function() {
+        await updateParameter(23, 2, 'CAMAXVT', cd, 'uint', '3600');
+      });
+      it('Should update Min Vote Period', async function() {
+        await updateParameter(23, 2, 'CAMINVT', cd, 'uint', '3600');
+      });
+      it('Should update Payout Retry Time', async function() {
+        await updateParameter(23, 2, 'CAPRETRY', cd, 'uint', '3600');
+      });
+      it('Should update Min Lock Period', async function() {
+        await updateParameter(23, 2, 'CADEPT', cd, 'uint', '86400');
+      });
+      it('Should update Reward%', async function() {
+        await updateParameter(23, 2, 'CAREWPER', cd, 'uint', '40');
+      });
+      it('Should update Min Vote Threshold', async function() {
+        await updateParameter(23, 2, 'CAMINTH', cd, 'uint', '30');
+      });
+      it('Should update Max Vote Threshold', async function() {
+        await updateParameter(23, 2, 'CAMAXTH', cd, 'uint', '30');
+      });
+      it('Should update CA Consensus%', async function() {
+        await updateParameter(23, 2, 'CACONPER', cd, 'uint', '40');
+      });
+    });
+
+    describe('Update Investment module Parameters', function() {
+      it('Should update IA  Variation%', async function() {
+        await updateParameter(24, 2, 'IMZ', pd, 'uint', '40');
+      });
+      it('Should update IA Exchange Rate Feed', async function() {
+        await updateParameter(24, 2, 'IMRATET', pd, 'uint', '3600');
+      });
+      it('Should update Uniswap Order Deadline', async function() {
+        await updateParameter(24, 2, 'IMUNIDL', pd, 'uint', '60');
+      });
+      it('Should update Liquidity Trade Callback', async function() {
+        await updateParameter(24, 2, 'IMLIQT', pd, 'uint', '3600');
+      });
+      it('Should update Uniswap Exchange Min Liquidity', async function() {
+        await updateParameter(24, 2, 'IMETHVL', pd, 'uint', '40');
+      });
+    });
+
+    describe('Update Capital Model Parameters', function() {
+      it('Should update MCR Post Time', async function() {
+        await updateParameter(25, 2, 'IMZ', pd, 'uint', '3600');
+      });
+      it('Should update MCR Fail Post Time', async function() {
+        await updateParameter(25, 2, 'IMRATET', pd, 'uint', '3600');
+      });
+      it('Should update Min Capital Required', async function() {
+        await updateParameter(25, 2, 'IMUNIDL', pd, 'uint', '60');
+      });
+      it('Should update Shock Parameter', async function() {
+        await updateParameter(25, 2, 'IMLIQT', pd, 'uint', '60');
+      });
+      it('Should update Capacity Limit%', async function() {
+        await updateParameter(25, 2, 'IMETHVL', pd, 'uint', '40');
+      });
+      it('Should update Factor C', async function() {
+        await updateParameter(25, 2, 'C', pd, 'uint', '40');
+      });
+      it('Should update Factor A', async function() {
+        await updateParameter(25, 2, 'A', pd, 'uint', '40');
+      });
+    });
+
+    describe('Update Address Parameters', function() {
+      it('Should update Event Caller Address', async function() {
+        let newEventCaller = await EventCaller.new();
+        await updateParameter(
+          26,
+          2,
+          'EVCALL',
+          nxms,
+          'address',
+          newEventCaller.address
+        );
+      });
+      it('Should update Master Contract Address', async function() {
+        let eventCaller = EventCaller.deployed();
+        let newMaster = await NXMaster.new(
+          eventCaller.address,
+          nxmToken.address
+        );
+        await updateParameter(
+          26,
+          2,
+          'MASTADD',
+          nxms,
+          'address',
+          newMaster.address
+        );
+      });
+    });
+
+    describe('Update Owner Parameters', function() {
+      it('Should update Multi sig Wallet Address', async function() {
+        await updateParameter(
+          27,
+          3,
+          'MSWALLET',
+          nxms,
+          'owner',
+          web3.eth.accounts[1]
+        );
+      });
+      it('Should update MCR Notarise Address', async function() {
+        await updateParameter(
+          27,
+          3,
+          'MCRNOTA',
+          nxms,
+          'owner',
+          web3.eth.accounts[1]
+        );
+      });
+      it('Should updateDAI Feed Address', async function() {
+        let newDai = await DAI.new();
+        await updateParameter(27, 3, 'DAIFEED', nxms, 'owner', newDai.address);
+      });
+      it('Should update Uniswap Factory Address', async function() {
+        var newUniswap = await FactoryMock.new();
+        await updateParameter(
+          27,
+          3,
+          'UNISWADD',
+          nxms,
+          'owner',
+          newUniswap.address
+        );
+      });
+      it('Should update Owner Address', async function() {
+        await updateParameter(
+          27,
+          3,
+          'OWNER',
+          nxms,
+          'owner',
+          web3.eth.accounts[1]
+        );
+      });
+      it('Should update Quote Engine Address', async function() {
+        await updateParameter(
+          27,
+          3,
+          'QUOAUTH',
+          nxms,
+          'owner',
+          web3.eth.accounts[1]
+        );
+      });
+      it('Should update KYC Authorised Address', async function() {
+        await updateParameter(
+          27,
+          3,
+          'KYCAUTH',
+          nxms,
+          'owner',
+          web3.eth.accounts[1]
+        );
+      });
+    });
+  }
+);

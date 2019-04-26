56abc41f8364d59f72eb949c10da08889fc9511e
PremYekkali <yekkaliprem@gmail.com>
2019-04-26 12:00:28 +0530

    added check in tranferFrom for member vote

--- contracts/NXMToken.sol
+++ contracts/NXMToken.sol
@@ -252,6 +252,7 @@ contract NXMToken is IERC20 {
         canTransfer(to)
         returns (bool)
     {
+        require(isLockedForMV[from] < now); // if not voted under governance
         require(value <= _balances[from]);
         require(value <= _allowed[from][msg.sender]);
         _transferFrom(from, to, value);

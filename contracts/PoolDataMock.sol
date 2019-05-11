aa4e7445930dec7de571030b6b429bdf07ba5c27
tech-somish <contact@somish.com>
2019-05-11 18:07:51 +0530

    updated PoolDataMock

--- contracts/PoolDataMock.sol
+++ contracts/PoolDataMock.sol
@@ -7,6 +7,9 @@ import "./PoolData.sol";
 
 contract PoolDataMock is PoolData {
 
+    constructor(address _notariseAdd, address _daiFeedAdd, address _daiAdd) public PoolData(_notariseAdd, _daiFeedAdd, _daiAdd) {
+	}
+
 	function changeCurrencyAssetBaseMin(bytes4 curr, uint baseMin) external {
 	    allCurrencyAssets[curr].baseMin = baseMin;
 	}

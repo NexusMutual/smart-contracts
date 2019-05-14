e5933760b5db96aebc1ca3e819aef618fa2c13b3
tech-somish <contact@somish.com>
2019-05-14 14:49:41 +0530

    upated governance test cases

--- test/utils/gvProposal.js
+++ test/utils/gvProposal.js
@@ -20,4 +20,27 @@ async function gvProposal(...args) {
   assert.equal(proposal[2].toNumber(), 3);
 }
 
-module.exports = { gvProposal };
+async function gvProposalWithIncentive(...args) {
+  let catId = args[0];
+  let actionHash = args[1];
+  let mr = args[2];
+  let gv = args[3];
+  let seq = args[4];
+  let incentive = args[5];
+  let p = await gv.getProposalLength();
+  await gv.createProposal('proposal', 'proposal', 'proposal', 0);
+  await gv.categorizeProposal(p, catId, incentive);
+  await gv.submitProposalWithSolution(p, 'proposal', actionHash);
+  let members = await mr.members(seq);
+  let iteration = 0;
+  for (iteration = 0; iteration < members[1].length; iteration++)
+    await gv.submitVote(p, 1, {
+      from: members[1][iteration]
+    });
+  // console.log(await gv.proposalDetails(p));
+  if (seq != 3) await gv.closeProposal(p);
+  let proposal = await gv.proposal(p);
+  assert.equal(proposal[2].toNumber(), 3);
+}
+
+module.exports = { gvProposalWithIncentive, gvProposal };

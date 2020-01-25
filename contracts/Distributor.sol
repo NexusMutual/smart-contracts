8d2eaf6d322ace4bdf31879114a4c55f09041fe9
Dan Octavian <danoctavian91@gmail.com>
2020-01-25 15:03:25 +0800

    add qualified imports to not pollute namespace

--- contracts/Distributor.sol
+++ contracts/Distributor.sol
@@ -1,27 +1,31 @@
 pragma solidity 0.5.7;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721Full.sol";
+import * as ERC721 from "@openzeppelin/contracts/token/ERC721/ERC721Full.sol";
 import "@openzeppelin/contracts/ownership/Ownable.sol";
 import "./INXMMaster.sol";
+import * as Quotation from "./Quotation.sol";
 
-contract Distributor is ERC721Full, Ownable {
+contract Distributor is ERC721.ERC721Full, Ownable {
 
   INXMMaster nxMaster;
-  constructor(address _masterAddress) ERC721Full("NexusMutualDistributorNFT", "NMDNFT") public {
+  constructor(address _masterAddress) public {
     nxMaster = INXMMaster(_masterAddress);
+    
   }
 
-  // function buyCover(
-  //       address smartCAdd,
-  //       bytes4 coverCurr,
-  //       uint[] memory coverDetails,
-  //       uint16 coverPeriod,
-  //       uint8 _v,
-  //       bytes32 _r,
-  //       bytes32 _s
-  //   )
-  //    public
-  //    payable 
-  // {
-  // }
+  function buyCover(
+        address smartContractAddress,
+        bytes4 coverCurr,
+        uint[] memory coverDetails,
+        uint16 coverPeriod,
+        uint8 _v,
+        bytes32 _r,
+        bytes32 _s
+    )
+     public
+     payable 
+  {
+    Quotation.Quotation qt = Quotation.Quotation(nxMaster.getLatestAddress("QT"));
+    require(qt.verifySign(coverDetails, coverPeriod, coverCurr, smartContractAddress, _v, _r, _s));
+  }
 }

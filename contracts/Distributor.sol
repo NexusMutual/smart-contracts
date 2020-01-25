4bf53de90e342fe397885b34101bab9d68cb69ca
Dan Octavian <danoctavian91@gmail.com>
2020-01-25 13:01:08 +0800

    add Ownable inheritance

--- contracts/Distributor.sol
+++ contracts/Distributor.sol
@@ -1,8 +1,9 @@
 pragma solidity 0.5.7;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721Full.sol";
+import "@openzeppelin/contracts/ownership/Ownable.sol";
 
-contract Distributor is ERC721Full {
+contract Distributor is ERC721Full, Ownable {
   constructor() ERC721Full("NexusMutualDistributorNFT", "NMDNFT") public {
   }
 }

87af6123cf195a26557bf9191ed2dec9daad9482
Dan Octavian <danoctavian91@gmail.com>
2020-01-25 14:14:47 +0800

    pass in master contract

--- contracts/Distributor.sol
+++ contracts/Distributor.sol
@@ -2,8 +2,26 @@ pragma solidity 0.5.7;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721Full.sol";
 import "@openzeppelin/contracts/ownership/Ownable.sol";
+import "./INXMMaster.sol";
 
 contract Distributor is ERC721Full, Ownable {
-  constructor() ERC721Full("NexusMutualDistributorNFT", "NMDNFT") public {
+
+  INXMMaster nxMaster;
+  constructor(address _masterAddress) ERC721Full("NexusMutualDistributorNFT", "NMDNFT") public {
+    nxMaster = INXMMaster(_masterAddress);
   }
+
+  // function buyCover(
+  //       address smartCAdd,
+  //       bytes4 coverCurr,
+  //       uint[] memory coverDetails,
+  //       uint16 coverPeriod,
+  //       uint8 _v,
+  //       bytes32 _r,
+  //       bytes32 _s
+  //   )
+  //    public
+  //    payable 
+  // {
+  // }
 }

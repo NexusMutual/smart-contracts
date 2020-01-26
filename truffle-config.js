fdfe494a98851fa6029734089900a141dff558fa
Dan Octavian <danoctavian91@gmail.com>
2020-01-26 14:54:56 +0800

    remove skipMigrations network

--- truffle-config.js
+++ truffle-config.js
@@ -18,12 +18,6 @@ module.exports = {
       port: 8545,
       gas: 8000000,
       network_id: '5777'
-    },
-    skipMigrations: {
-      host: '127.0.0.1',
-      port: 8545,
-      gas: 8000000,
-      network_id: '5777'
     }
   },
   compilers: {

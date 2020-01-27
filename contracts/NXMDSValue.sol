3a8bcc538e65215f56490d142d2907cdc1ee6331
PremSOMISH <prem@somish.com>
2020-01-27 18:56:27 +0530

    Reframed comments in contract

--- contracts/NXMDSValue.sol
+++ contracts/NXMDSValue.sol
@@ -24,16 +24,16 @@ contract Aggregator {
 contract NXMDSValue {
 
     /// @dev Gets DAI feed address from chainlink feed and converts into desired format.
-    /// @return It returns DAI Feed rate in 10^18 format.  
+    /// @return It return DAI Feed rate in 10^18 format.  
     function read() public view returns (bytes32)
     {
         
         // Instance to get DAI feed from chainlink feed.
         Aggregator aggregator = Aggregator(0x79fEbF6B9F76853EDBcBc913e6aAE8232cFB9De9);
         
-        // Chainlink feed is returning value in rate * 10^8 format and we need in rate * 10^18 format
-        // Hence, multiplying with 10^10.
-        // Chainlink feed is returning value in uint but we are expecting it in bytes32.
+        // Chainlink feed returns value = rate * 10^8.
+        // Multiplying value by 10^10 since we need value = rate * 10^18 format to follow the DSValue format.
+        // Chainlink feed returns  uint, converting to bytes32 to follow the DSValue format.
         return bytes32(aggregator.currentAnswer()*10**10);
     }
 }

11869c06985b14ad163f5666f6771729d75b3f72
Dan Octavian <danoctavian91@gmail.com>
2020-01-28 21:50:23 +0800

    add pay with DAI

--- contracts/Distributor.sol
+++ contracts/Distributor.sol
@@ -1,10 +1,12 @@
 pragma solidity 0.5.7;
 
 import * as ERC721 from "@openzeppelin/contracts/token/ERC721/ERC721Full.sol";
+import * as IERC20 from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import * as Ownable from "@openzeppelin/contracts/ownership/Ownable.sol";
 import * as SafeMath from "./external/openzeppelin-solidity/math/SafeMath.sol";
 import * as INXMMaster from "./INXMMaster.sol";
 import * as Pool1 from "./Pool1.sol";
+import * as PoolData from "./PoolData.sol";
 import * as Claims from "./Claims.sol";
 import * as NXMToken from "./NXMToken.sol";
 import * as QuotationData from "./QuotationData.sol";
@@ -44,12 +46,53 @@ contract Distributor is ERC721.ERC721Full("NXMDistributorNFT", "NXMDNFT"), Ownab
      public
      payable 
   {
-    Pool1.Pool1 p1 = Pool1.Pool1(nxMaster.getLatestAddress("P1"));
     uint requiredValue = priceLoadPercentage.mul(coverDetails[1]).div(100).add(coverDetails[1]);
     require(msg.value == requiredValue, "Incorrect value sent");
 
+    Pool1.Pool1 p1 = Pool1.Pool1(nxMaster.getLatestAddress("P1"));
     p1.makeCoverBegin.value(coverDetails[1])(coveredContractAddress, coverCurrency, coverDetails, coverPeriod, _v, _r, _s);
 
+    QuotationData.QuotationData quotationData = QuotationData.QuotationData(nxMaster.getLatestAddress("QD"));
+    // *assumes* the newly created claim is appended at the end of the list covers
+    uint coverId = quotationData.getCoverLength().sub(1);
+
+    uint256 nextTokenId = tokenIdCounter++;
+    uint expirationTimestamp = block.timestamp + CLAIM_VALIDITY_MAX_DAYS_OVER_COVER_PERIOD + coverPeriod * 1 days; 
+    allTokenData[nextTokenId] = TokenData(expirationTimestamp, msg.sender, coverCurrency, coverDetails, coverId);
+    _mint(msg.sender, nextTokenId);
+  }
+
+  function buyCoverUsingCA(
+        address coveredContractAddress,
+        bytes4 coverCurrency,
+        uint[] memory coverDetails,
+        uint16 coverPeriod,
+        uint8 _v,
+        bytes32 _r,
+        bytes32 _s
+    ) 
+    public
+  {
+    uint requiredValue = priceLoadPercentage.mul(coverDetails[1]).div(100).add(coverDetails[1]);
+    PoolData.PoolData pd = PoolData.PoolData(nxMaster.getLatestAddress("PD"));
+    IERC20.IERC20 erc20 = IERC20.IERC20(pd.getCurrencyAssetAddress(coverCurrency));
+    require(erc20.transferFrom(msg.sender, address(this), requiredValue), "Transfer failed");
+
+    address payable pool1Address = nxMaster.getLatestAddress("P1");
+    Pool1.Pool1 p1 = Pool1.Pool1(pool1Address);
+    erc20.approve(pool1Address, coverDetails[1]);
+    p1.makeCoverUsingCA(coveredContractAddress, coverCurrency, coverDetails, coverPeriod, _v, _r, _s);
+
+    mintToken(coverCurrency, coverDetails, coverPeriod);
+  }
+
+  function mintToken(
+    bytes4 coverCurrency,
+    uint[] memory coverDetails,
+    uint16 coverPeriod
+  )
+  internal
+  {
     QuotationData.QuotationData quotationData = QuotationData.QuotationData(nxMaster.getLatestAddress("QD"));
     // *assumes* the newly created claim is appended at the end of the list covers
     uint coverId = quotationData.getCoverLength().sub(1);
@@ -68,6 +111,7 @@ contract Distributor is ERC721.ERC721Full("NXMDistributorNFT", "NXMDNFT"), Ownab
   {
     require(_isApprovedOrOwner(msg.sender, tokenId), "Not approved or owner");
     require(allTokenData[tokenId].expirationTimestamp > block.timestamp, "Token is expired");
+    require(allTokenData[tokenId].coverCurrency == "ETH");
     uint coverAmount = allTokenData[tokenId].coverDetails[1];
     require(msg.value == CLAIM_DEPOSIT_PERCENTAGE.mul(coverAmount).div(100), "Deposit value is incorrect");
  

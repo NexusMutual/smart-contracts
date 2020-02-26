a9784c699e957984784b7688e65da0fb62470a1e
PremSOMISH <prem@somish.com>
2020-02-26 17:58:29 +0530

    changes in truffle configuration

--- truffle-config.js
+++ truffle-config.js
@@ -3,20 +3,21 @@ module.exports = {
     development: {
       host: '127.0.0.1',
       port: 8545,
-      gas: 8000000,
-      network_id: '5777'
+      gas: 10000000,
+      network_id: '5777',
+      gasPrice: 0x01
     },
     coverage: {
       host: '127.0.0.1',
       network_id: '5777',
       port: 8555,
-      gas: 8000000,
+      gas: 10000000,
       gasPrice: 0x01
     },
     ganache: {
       host: '127.0.0.1',
       port: 8545,
-      gas: 8000000,
+      gas: 10000000,
       network_id: '5777'
     }
   },
@@ -27,7 +28,8 @@ module.exports = {
         optimizer: {
           enabled: true,
           runs: 200
-        }
+        },
+        evmVersion: 'constantinople'
       }
     }
   }

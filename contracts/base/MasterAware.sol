15e2f4f066e6d1a3e961ce0e637884eb1e3c3ff7
shark0der <tolean.dj@gmail.com>
2020-03-09 16:48:09 +0200

    Initial setup

---
+++ contracts/base/MasterAware.sol
@@ -0,0 +1,57 @@
+/*
+    Copyright (C) 2020 NexusMutual.io
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see http://www.gnu.org/licenses/
+*/
+
+pragma solidity ^0.5.16;
+
+import "@openzeppelin/upgrades/contracts/Initializable.sol";
+import "../interfaces/INXMMaster.sol";
+
+contract MasterAware is Initializable {
+
+  INXMMaster internal master;
+
+  /**
+   * @dev Ensure the call is from internal contracts only
+   */
+  modifier onlyInternal {
+    require(master.isInternal(msg.sender), "Caller is not an internal contract");
+    _;
+  }
+
+  /**
+   * @dev Ensure the call is from internal members only
+   */
+  modifier onlyMembers {
+    require(master.isInternal(msg.sender), "Caller is not a member");
+    _;
+  }
+
+  /**
+   * @dev Ensure the calling address is authorized to govern
+   */
+  modifier onlyGoverned {
+    require(
+      master.checkIsAuthToGoverned(msg.sender),
+      "Caller is not authorized to govern"
+    );
+    _;
+  }
+
+  function initialize(address masterAddress) public {
+    master = INXMMaster(masterAddress);
+  }
+}
---
+++ test-environment.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+
+  accounts: {
+    amount: 10, // Number of unlocked accounts
+    ether: 100, // Initial balance of unlocked accounts (in ether)
+  },
+
+  contracts: {
+    type: 'truffle', // Contract abstraction to use: 'truffle' for @truffle/contract or 'web3' for web3-eth-contract
+    defaultGas: 6e6, // Maximum gas for contract calls (when unspecified)
+
+    // Options available since v0.1.2
+    defaultGasPrice: 20e9, // Gas price for contract calls (when unspecified)
+    artifactsDir: 'build/contracts', // Directory where contract artifacts are stored
+  },
+
+  blockGasLimit: 8e6, // Maximum gas per block
+};

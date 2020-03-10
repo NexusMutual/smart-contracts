cc0f7284293ae037e49d4ec47d7f890161c2cf2a
shark0der <tolean.dj@gmail.com>
2020-03-10 19:27:12 +0200

    Add constants for roles and parameter types

---
+++ test/utils/constants.js
@@ -0,0 +1,18 @@
+const ParamType = {
+  MIN_DEPOSIT_AMOUNT: 0,
+  MIN_STAKE_PERCENTAGE: 1,
+  MAX_LEVERAGE: 2,
+  UNSTAKE_LOCK_TIME: 3,
+};
+
+const Role = {
+  NonMember: 0,
+  Member: 1,
+  AdvisoryBord: 2,
+  Owner: 3,
+};
+
+module.exports = {
+  ParamType,
+  Role,
+};

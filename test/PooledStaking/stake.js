c4591c1afe8fde391b98aaf8abd80ff3705db00a
shark0der <tolean.dj@gmail.com>
2020-03-16 15:10:21 +0200

    Add more stake tests

--- test/PooledStaking/stake.js
+++ test/PooledStaking/stake.js
@@ -3,13 +3,14 @@ const { assert } = require('chai');
 
 const accounts = require('../utils/accounts');
 const setup = require('../utils/setup');
+const { ParamType } = require('../utils/constants');
 
 const {
   nonMembers: [nonMember],
-  // members: [member],
+  members: [member],
   // advisoryBoardMembers: [advisoryBoardMember],
   // internalContracts: [internalContract],
-  // governanceContracts: [governanceContract],
+  governanceContracts: [governanceContract],
 } = accounts;
 
 describe('stake', function () {
@@ -25,6 +26,37 @@ describe('stake', function () {
       staking.stake(ether('1'), { from: nonMember }),
       'Caller is not a member',
     );
+  });
+
+  it('should revert if staked amount is less than MIN_DEPOSIT_AMOUNT', async function () {
+
+    const { staking } = this;
+
+    await staking.updateParameter(
+      ParamType.MIN_DEPOSIT_AMOUNT,
+      ether('10'),
+      { from: governanceContract },
+    );
+
+    await expectRevert(
+      staking.stake(ether('1'), { from: member }),
+      'Amount is less than minimum allowed',
+    );
+
+  });
+
+  it('should add the staked amount to the total user stake', async function () {
+
+    const { staking } = this;
+    const { staked: stakedBefore } = await staking.stakers(member, { from: member });
+    const stakedAmount = ether('1');
+
+    assert(stakedBefore.eqn(0), 'initial amount should be 0');
+
+    await staking.stake(stakedAmount, { from: member });
+    const { staked: stakedAfter } = await staking.stakers(member, { from: member });
+
+    assert(stakedAfter.eq(stakedAmount), 'final amount should be equal to staked amount');
   });
 
 });

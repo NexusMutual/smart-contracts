02f8abf767ebec3ad7308d6db1c422f85f65e5ea
shark0der <tolean.dj@gmail.com>
2020-04-10 23:58:26 +0300

    Add kovan & mainnet networks. Deploy on kovan

--- networks.js
+++ networks.js
@@ -1,5 +1,23 @@
+require('dotenv').config();
+
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const kovanMnemonic = process.env.KOVAN_MNEMONIC;
+
 module.exports = {
   networks: {
+    mainnet: {
+      protocol: 'https',
+      host: 'parity.nexusmutual.io',
+      port: 443,
+      gas: 5000000,
+      gasPrice: 5e9,
+      networkId: 1,
+    },
+    kovan: {
+      gasPrice: 5e9,
+      networkId: 42,
+      provider: () => new HDWalletProvider(kovanMnemonic, 'https://parity.govblocks.io'),
+    },
     development: {
       protocol: 'http',
       host: 'localhost',

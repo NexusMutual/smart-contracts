e6d99504e9efdb51dbcf3e88f75fb4ceb298bb66
shark0der <tolean.dj@gmail.com>
2020-05-26 13:27:41 +0300

    Change pragma to ^0.5.17

--- contracts/mocks/TokenMock.sol
+++ contracts/mocks/TokenMock.sol
@@ -15,7 +15,7 @@
     along with this program.  If not, see http://www.gnu.org/licenses/
 */
 
-pragma solidity ^0.5.7;
+pragma solidity ^0.5.17;
 
 import "@openzeppelin/upgrades/contracts/Initializable.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
--- truffle-config.js
+++ truffle-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   compilers: {
     solc: {
-      version: '0.5.7',
+      version: '0.5.17',
       settings: {
         optimizer: {
           enabled: true,

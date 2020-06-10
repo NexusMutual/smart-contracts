2c143ea918299cbcc4192bc5ac1932041d6ec411
Dan Octavian <danoctavian91@gmail.com>
2020-06-10 18:03:23 +0800

    go back to functioning version of encode

--- test/fork/external.js
+++ test/fork/external.js
@@ -2,22 +2,23 @@ const abi = require('ethereumjs-abi');
 const { hex } = require('../utils/helpers');
 
 function encode(...args) {
-  if (args.length == 1) {
-    return '0x';
-  }
-  let fn = args[0];
-  let params = args.slice(1);
-  let types = fn
-    .slice(0, fn.length - 1)
+  var signature = args[0];
+  var datatypes = signature
+    .substring(0, signature.length - 1)
     .split('(')[1]
     .split(',');
-  for (let i = 0; i < types.length; i++) {
-    if (types[i].includes('bytes') && !params[i].startsWith('0x')) {
-      params[i] = hex(params[i]);
+  var params = args.slice(1);
+  for (let i = 0; i < datatypes.length; i++) {
+    if (datatypes[i].includes('byte') && !datatypes[i].includes('[]')) {
+      // console.log(params[i]);
+
+      if (!params[i].startsWith('0x')) {
+        params[i] = hex(params[i]);
+        args[i + 1] = params[i];
+      }
     }
   }
-  args = [types, params];
-  let encoded = abi.rawEncode.apply(this, args);
+  var encoded = abi.simpleEncode.apply(this, args);
   encoded = encoded.toString('hex');
   return '0x' + encoded;
 }

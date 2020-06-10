adc79a01f229f881a6dda3d8fd5b88a91a686f45
Dan Octavian <danoctavian91@gmail.com>
2020-06-10 15:28:42 +0800

    replace with new encode function

--- test/fork/external.js
+++ test/fork/external.js
@@ -1,27 +1,31 @@
 const abi = require('ethereumjs-abi');
 const { hex } = require('../utils/helpers');
+
 function encode(...args) {
-  var signature = args[0];
-  var datatypes = signature
-    .substring(0, signature.length - 1)
+  if (args.length == 1) {
+    return '0x';
+  }
+  let fn = args[0];
+  let params = args.slice(1);
+  let types = fn
+    .slice(0, fn.length - 1)
     .split('(')[1]
     .split(',');
-  var params = args.slice(1);
-  for (let i = 0; i < datatypes.length; i++) {
-    if (datatypes[i].includes('byte') && !datatypes[i].includes('[]')) {
-      // console.log(params[i]);
-
-      if (!params[i].startsWith('0x')) {
-        params[i] = hex(params[i]);
-        args[i + 1] = params[i];
-      }
+  for (let i = 0; i < types.length; i++) {
+    if (types[i].includes('bytes') && !params[i].startsWith('0x')) {
+      params[i] = hex(params[i]);
     }
   }
-  var encoded = abi.simpleEncode.apply(this, args);
+  args = [types, params];
+  let encoded = abi.rawEncode.apply(this, args);
   encoded = encoded.toString('hex');
   return '0x' + encoded;
 }
 
-module.exports = {
-  encode
+function encode1(...args) {
+  var encoded = abi.rawEncode.apply(this, args);
+  encoded = encoded.toString('hex');
+  return '0x' + encoded;
 }
+
+module.exports = {encode, encode1};

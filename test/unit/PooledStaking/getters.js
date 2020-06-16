4e0280cf5f87366eef339cd834bc302dda12fac3
shark0der <tolean.dj@gmail.com>
2020-06-16 19:55:52 +0300

    Remove stakerProcessedDeposit test since the function was deleted

--- test/unit/PooledStaking/getters.js
+++ test/unit/PooledStaking/getters.js
@@ -34,52 +34,6 @@ describe('getters', function () {
 
   beforeEach(setup);
 
-  it('stakerProcessedDeposit', async function () {
-
-    const { staking, token } = this;
-    const amount = ether('10');
-
-    await fundAndApprove(token, staking, amount, memberOne);
-    await fundAndApprove(token, staking, amount, memberTwo);
-    await fundAndApprove(token, staking, amount, memberThree);
-
-    // Stake
-    await staking.depositAndStake(ether('6'), [firstContract], [ether('6')], { from: memberOne });
-    await staking.depositAndStake(
-      ether('10'),
-      [firstContract, secondContract, thirdContract],
-      [ether('4'), ether('5'), ether('10')],
-      { from: memberTwo },
-    );
-    await staking.depositAndStake(
-      ether('7'),
-      [firstContract, thirdContract],
-      [ether('5'), ether('7')],
-      { from: memberThree },
-    );
-
-    const totalStakedFirstContract = await staking.contractStake(firstContract);
-
-    // Burn firstContract for 12
-    const burnAmount = ether('12');
-    await staking.pushBurn(firstContract, burnAmount, { from: internalContract });
-
-    // Check stakerProcessedDeposit for memberOne
-    const stakerProcessedStakeOne = await staking.stakerProcessedDeposit(memberOne, { from: internalContract });
-    const expectedDepositOne = ether('6').sub(burnAmount.mul(ether('6')).div(totalStakedFirstContract));
-    assert(stakerProcessedStakeOne.eq(expectedDepositOne));
-
-    // Check stakerProcessedDeposit for memberTwo
-    const stakerProcessedStakeTwo = await staking.stakerProcessedDeposit(memberTwo, { from: internalContract });
-    const expectedDepositTwo = ether('10').sub(burnAmount.mul(ether('4')).div(totalStakedFirstContract));
-    assert(stakerProcessedStakeTwo.eq(expectedDepositTwo));
-
-    // Check stakerProcessedDeposit for memberThree
-    const stakerProcessedStakeThree = await staking.stakerProcessedDeposit(memberThree, { from: internalContract });
-    const expectedDepositThree = ether('7').sub(burnAmount.mul(ether('5')).div(totalStakedFirstContract));
-    assert(stakerProcessedStakeThree.eq(expectedDepositThree));
-  });
-
   it('stakerContractStake', async function () {
 
     const { token, staking } = this;

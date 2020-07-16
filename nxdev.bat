df5ef5da4b3e21f73b02dea519d2ded977b93d00
shark0der <tolean.dj@gmail.com>
2020-07-16 20:09:49 +0300

    Remove unused scripts

--- nxdev.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-title Nexus Dev Kitchen !!!
-echo Loading Resources . . .
-cd node_modules/.bin/
-echo starting ganche-cli  . . .
-start cmd.exe /k "ganache-cli --gasLimit 0xfffffffffff -i 5777 -p 8545 -m 'grocery obvious wire insane limit weather parade parrot patrol stock blast ivory' -a 30 -e 10000000"
-ping 127.0.0.1 -n 5 > nul
-echo starting oraclize ethereum bridge . . .
-start cmd.exe /k "ethereum-bridge -H localhost:8545 -a 29 --dev"

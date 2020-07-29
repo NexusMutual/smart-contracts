b375d58f554810f8461b49966f503af252f6b6c6
Everett Hildenbrandt <everett.hildenbrandt@gmail.com>
2020-07-29 15:14:23 +0000

    test/29_Migrations.firefly.test.js: remove flaky test

--- test/29_Migrations.firefly.test.js
+++ test/29_Migrations.firefly.test.js
@@ -20,13 +20,6 @@ contract('Migrations', (accounts) => {
 			await assertRevert(obj.upgrade(arg0));
 		});
 
-		it('call func owner with blackbox random args', async () => {
-			// Might adjust constructor accordingly
-			const obj = await Migrations.new();
-			const res = await obj.owner();
-			res.toString().should.be.equal("0x004B7D0721cbffcB87Aeae35Bf88196dd07281D1");
-		});
-
 		it('call func lastCompletedMigration with blackbox random args', async () => {
 			// Might adjust constructor accordingly
 			const obj = await Migrations.new();

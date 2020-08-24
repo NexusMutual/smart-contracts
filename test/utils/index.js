00054adfa2a5cb4d881d29b6703295f23dce1723
shark0der <tolean.dj@gmail.com>
2020-08-25 02:37:38 +0300

    Change tenderly function to tenderlyFactory

--- test/utils/index.js
+++ test/utils/index.js
@@ -2,7 +2,7 @@ const { web3 } = require('@openzeppelin/test-environment');
 const { constants, helpers } = require('../../lib');
 const accounts = require('./accounts');
 
-const tenderly = async tx => helpers.tenderly(web3, tx);
+const tenderly = async tx => helpers.tenderlyFactory(web3)(tx);
 
 module.exports = {
   accounts,

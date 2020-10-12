19da7b929d9e80c84cd2bcdf1c4d7fcf60beb0b4
Dragos Horodnic <dragos@horodnic.com>
2020-10-12 20:15:57 +0300

    Rename cid to coverId, and add more test cases

--- test/unit/ClaimProofs/addProof.js
+++ test/unit/ClaimProofs/addProof.js
@@ -1,21 +1,50 @@
 const { defaultSender } = require('@openzeppelin/test-environment');
 const BN = require('bn.js');
 const { assert } = require('chai');
+const accounts = require('../utils').accounts;
+
+const {
+  nonMembers: [nonMember],
+  members: [memberOne, memberTwo],
+} = accounts;
 
 describe('addProof', function () {
 
-  it('should emit an event with cid, ipfsHash and sender when called', async function () {
+  it('should emit an event with coverId, ipfsHash and sender when called', async function () {
 
     const { claimProofs } = this;
 
-    const cid = new BN(1);
+    const coverId = new BN(1);
     const ipfsHash = 'mockedIpfsHash';
-    const res = await claimProofs.addProof(cid, ipfsHash);
+    const res = await claimProofs.addProof(coverId, ipfsHash);
     const proofAddedLog = res.logs.find(x => x.event === 'ProofAdded');
     assert(proofAddedLog !== undefined, 'Expected a ProofAdded event');
-    assert(proofAddedLog.args[0].eq(cid), `Expected ProofAdded event first argument to be ${cid}`);
+    assert(proofAddedLog.args[0].eq(coverId), `Expected ProofAdded event first argument to be ${coverId}`);
     assert(proofAddedLog.args[1] === defaultSender, `Expected ProofAdded event first argument to be ${defaultSender}`);
     assert(proofAddedLog.args[2] === ipfsHash, `Expected ProofAdded event first argument to be ${ipfsHash}`);
+
+  });
+
+  it('should allow anyone to submit a proof for the same coverId', async function () {
+
+    const { claimProofs } = this;
+
+    const coverId = new BN(1);
+    const ipfsHash = 'mockedIpfsHash';
+    assert(await claimProofs.addProof(coverId, ipfsHash, { from: memberOne }));
+    assert(await claimProofs.addProof(coverId, ipfsHash, { from: nonMember }));
+    assert(await claimProofs.addProof(coverId, ipfsHash, { from: memberTwo }));
+
+  });
+
+  it('should allow someone to submit a proof multiple times', async function () {
+
+    const { claimProofs } = this;
+
+    const coverId = new BN(1);
+    const ipfsHash = 'mockedIpfsHash';
+    assert(await claimProofs.addProof(coverId, ipfsHash, { from: nonMember }));
+    assert(await claimProofs.addProof(coverId, ipfsHash, { from: nonMember }));
 
   });
 

b2cc1c6b42c6d00acff023e4322cdb7c1000b8bf
shark0der <tolean.dj@gmail.com>
2020-10-26 15:40:41 +0200

    Remove tenderly

--- test/utils/index.js
+++ test/utils/index.js
@@ -1,19 +1,10 @@
-const { web3 } = require('hardhat');
 const { constants, helpers } = require('../../lib');
 const accounts = require('./accounts');
 const snapshot = require('./snapshot');
 
-/**
- * Export tx to tenderly. Accepts a tx hash string or a promise that resolves to a receipt.
- * Returns the receipt if a promise was passed.
- * @param {string|Promise} txPromise
- * @return {Promise<undefined|{}>}
- */
-const tenderly = async txPromise => helpers.tenderlyFactory(web3)(txPromise);
-
 module.exports = {
   accounts,
   constants,
-  helpers: { ...helpers, tenderly },
+  helpers,
   snapshot,
 };

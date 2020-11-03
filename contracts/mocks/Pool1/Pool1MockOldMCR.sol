a0c01d95531aa8249664dece388dfc592e52eaa7
Dan Octavian <dan@Dans-MacBook-Pro.local>
2020-11-03 12:59:55 +0800

    rm mock MCR and Pool1 unrelated functions

--- contracts/mocks/Pool1/Pool1MockOldMCR.sol
+++ contracts/mocks/Pool1/Pool1MockOldMCR.sol
@@ -56,59 +56,6 @@ contract Pool1MockOldMCR is Iupgradable {
   );
 
   /**
-   * @dev Adds new MCR data.
-   * @param mcrP  Minimum Capital Requirement in percentage.
-   * @param vF Pool1 fund value in Ether used in the last full daily calculation of the Capital model.
-   * @param onlyDate  Date(yyyymmdd) at which MCR details are getting added.
-   */
-  function addMCRData(
-    uint mcrP,
-    uint mcrE,
-    uint vF,
-    bytes4[] calldata curr,
-    uint[] calldata _threeDayAvg,
-    uint64 onlyDate
-  )
-  external
-  checkPause
-  {
-    require(proposalCategory.constructorCheck());
-    require(pd.isnotarise(msg.sender));
-    if (mr.launched() && pd.capReached() != 1) {
-
-      if (mcrP >= 10000)
-        pd.setCapReached(1);
-
-    }
-    uint len = pd.getMCRDataLength();
-    _addMCRData(len, onlyDate, curr, mcrE, mcrP, vF, _threeDayAvg);
-  }
-
-  /**
-   * @dev Adds MCR Data for last failed attempt.
-   */
-  function addLastMCRData(uint64 date) external checkPause onlyInternal {
-    uint64 lastdate = uint64(pd.getLastMCRDate());
-    uint64 failedDate = uint64(date);
-    if (failedDate >= lastdate) {
-      uint mcrP;
-      uint mcrE;
-      uint vF;
-      (mcrP, mcrE, vF,) = pd.getLastMCR();
-      uint len = pd.getAllCurrenciesLen();
-      pd.pushMCRData(mcrP, mcrE, vF, date);
-      for (uint j = 0; j < len; j++) {
-        bytes4 currName = pd.getCurrenciesByIndex(j);
-        pd.updateCAAvgRate(currName, pd.getCAAvgRate(currName));
-      }
-
-      emit MCREvent(date, block.number, new bytes4[](0), new uint[](0), mcrE, mcrP, vF);
-      // Oraclize call for next MCR calculation
-      _callOracliseForMCR();
-    }
-  }
-
-  /**
    * @dev Iupgradable Interface to update dependent contract address
    */
   function changeDependentContractAddress() public onlyInternal {
@@ -202,28 +149,6 @@ contract Pool1MockOldMCR is Iupgradable {
     return _calVtpAndMCRtp(poolBalance);
   }
 
-  function getThresholdValues(uint vtp, uint vF, uint totalSA, uint minCap) public view returns (uint lowerThreshold, uint upperThreshold)
-  {
-    minCap = (minCap.mul(minCapFactor)).add(variableMincap);
-    uint lower = 0;
-    if (vtp >= vF) {
-      // Max Threshold = [MAX(Vtp, Vfull) x 120] / mcrMinCap
-      upperThreshold = vtp.mul(120).mul(100).div((minCap));
-    } else {
-      upperThreshold = vF.mul(120).mul(100).div((minCap));
-    }
-
-    if (vtp > 0) {
-      lower = totalSA.mul(DECIMAL1E18).mul(pd.shockParameter()).div(100);
-      if (lower < minCap.mul(11).div(10))
-        lower = minCap.mul(11).div(10);
-    }
-    if (lower > 0) {
-      // Min Threshold = [Vtp / MAX(TotalActiveSA x ShockParameter, mcrMinCap x 1.1)] x 100
-      lowerThreshold = vtp.mul(100).mul(100).div(lower);
-    }
-  }
-
   /**
    * @dev Gets max numbers of tokens that can be sold at the moment.
    */
@@ -241,53 +166,6 @@ contract Pool1MockOldMCR is Iupgradable {
       maxTokens = (((uint(lastMCRPerc).sub(10000)).mul(2000)).mul(DECIMAL1E18)).div(10000);
     if (maxTokens > maxTokensAccPoolBal)
       maxTokens = maxTokensAccPoolBal;
-  }
-
-  /**
-   * @dev Gets Uint Parameters of a code
-   * @param code whose details we want
-   * @return string value of the code
-   * @return associated amount (time or perc or value) to the code
-   */
-  function getUintParameters(bytes8 code) external view returns (bytes8 codeVal, uint val) {
-    codeVal = code;
-    if (code == "DMCT") {
-      val = dynamicMincapThresholdx100;
-
-    } else if (code == "DMCI") {
-
-      val = dynamicMincapIncrementx100;
-
-    }
-
-  }
-
-  /**
-   * @dev Updates Uint Parameters of a code
-   * @param code whose details we want to update
-   * @param val value to set
-   */
-  function updateUintParameters(bytes8 code, uint val) public {
-    require(ms.checkIsAuthToGoverned(msg.sender));
-    if (code == "DMCT") {
-      dynamicMincapThresholdx100 = val;
-
-    } else if (code == "DMCI") {
-
-      dynamicMincapIncrementx100 = val;
-
-    }
-    else {
-      revert("Invalid param code");
-    }
-
-  }
-
-  /**
-   * @dev Calls oraclize query to calculate MCR details after 24 hours.
-   */
-  function _callOracliseForMCR() internal {
-    p1.mcrOraclise(pd.mcrTime());
   }
 
   /**
@@ -319,57 +197,4 @@ contract Pool1MockOldMCR is Iupgradable {
     tokenPrice = tokenPrice.mul(getCAAvgRate * 10);
     tokenPrice = (tokenPrice).div(10 ** 3);
   }
-
-  /**
-   * @dev Adds MCR Data. Checks if MCR is within valid
-   * thresholds in order to rule out any incorrect calculations
-   */
-  function _addMCRData(
-    uint len,
-    uint64 newMCRDate,
-    bytes4[] memory curr,
-    uint mcrE,
-    uint mcrP,
-    uint vF,
-    uint[] memory _threeDayAvg
-  )
-  internal
-  {
-    uint vtp = 0;
-    uint lowerThreshold = 0;
-    uint upperThreshold = 0;
-    if (len > 1) {
-      (vtp,) = _calVtpAndMCRtp(address(p1).balance);
-      (lowerThreshold, upperThreshold) = getThresholdValues(vtp, vF, getAllSumAssurance(), pd.minCap());
-
-    }
-    if (mcrP > dynamicMincapThresholdx100)
-      variableMincap = (variableMincap.mul(dynamicMincapIncrementx100.add(10000)).add(minCapFactor.mul(pd.minCap().mul(dynamicMincapIncrementx100)))).div(10000);
-
-
-    // Explanation for above formula :-
-    // actual formula -> variableMinCap =  variableMinCap + (variableMinCap+minCap)*dynamicMincapIncrement/100
-    // Implemented formula is simplified form of actual formula.
-    // Let consider above formula as b = b + (a+b)*c/100
-    // here, dynamicMincapIncrement is in x100 format.
-    // so b+(a+b)*cx100/10000 can be written as => (10000.b + b.cx100 + a.cx100)/10000.
-    // It can further simplify to (b.(10000+cx100) + a.cx100)/10000.
-    if (len == 1 || (mcrP) >= lowerThreshold
-    && (mcrP) <= upperThreshold) {
-      // due to stack to deep error,we are reusing already declared variable
-      vtp = pd.getLastMCRDate();
-      pd.pushMCRData(mcrP, mcrE, vF, newMCRDate);
-      for (uint i = 0; i < curr.length; i++) {
-        pd.updateCAAvgRate(curr[i], _threeDayAvg[i]);
-      }
-      emit MCREvent(newMCRDate, block.number, curr, _threeDayAvg, mcrE, mcrP, vF);
-      // Oraclize call for next MCR calculation
-      if (vtp < newMCRDate) {
-        _callOracliseForMCR();
-      }
-    } else {
-      p1.mcrOracliseFail(newMCRDate, pd.mcrFailTime());
-    }
-  }
-
 }

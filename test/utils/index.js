aac5e4ca7bf14b106eb899fcee3eff7deb7810c3
shark0der <tolean.dj@gmail.com>
2020-11-12 00:46:00 +0200

    Add hardhat impersonation and time helper

--- test/utils/index.js
+++ test/utils/index.js
@@ -1,11 +1,13 @@
-const { web3 } = require('hardhat');
 const { constants, helpers } = require('../../lib');
+
 const accounts = require('./accounts');
+const hardhat = require('./hardhat');
 const snapshot = require('./snapshot');
 
 module.exports = {
   accounts,
   constants,
+  hardhat,
   helpers,
   snapshot,
 };

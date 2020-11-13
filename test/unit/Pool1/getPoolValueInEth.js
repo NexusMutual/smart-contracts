a9aef83f7cdf8f1a3d346ee22388a949ae1063ea
Dan Octavian <danoctavian91@gmail.com>
2020-11-13 16:30:49 +0800

    add missing aggregator mock file and fix pool value test

--- test/unit/Pool1/getPoolValueInEth.js
+++ test/unit/Pool1/getPoolValueInEth.js
@@ -13,20 +13,22 @@ const {
 
 describe('getPoolValueInEth', function () {
   it.only('calculates total pool value correctly', async function () {
-    const { pool1, poolData, tokenData, mcr, chainlinkAggregators } = this;
+    const { pool1, poolData, tokenData, mcr, chainlinkAggregators, dai } = this;
 
     const initialAssetValue = new BN('210959924071154460525457');
     const mcrEth = new BN('162424730681679380000000');
-    const mcrRatio = initialAssetValue.muln(1e4).div(mcrEth);
     const ethToDaiRate = new BN((394.59 * 1e18).toString());
     const daiToEthRate = new BN(10).pow(new BN(36)).div(ethToDaiRate);
     const ethRate = new BN('100');
+    const daiAmount = ether('10000');
 
     await setupContractState(
       { fundSource, initialAssetValue, mcrEth, daiRate: daiToEthRate, ethRate, mcr, pool1, poolData, tokenData, chainlinkAggregators },
     );
-    const expectedPrice = getTokenSpotPrice(mcrRatio, mcrEth);
+
+    await dai.mint(pool1.address, daiAmount);
+    const expectedPoolValue = initialAssetValue.add(daiAmount.mul(daiToEthRate).div(ether('1')));
     const poolValue = await pool1.getPoolValueInEth();
-    assert.equal(poolValue.toString(), initialAssetValue.toString());
+    assert.equal(poolValue.toString(), expectedPoolValue.toString());
   });
 });

94e19c747a72371cbe355478d9cd5fb4ee478258
Dan Octavian <danoctavian91@gmail.com>
2020-11-18 22:40:22 +0800

    add new version of unit test

--- test/unit/Pool1/calculateNXMForEth.js
+++ test/unit/Pool1/calculateNXMForEth.js
@@ -2,8 +2,14 @@ const { ether } = require('@openzeppelin/test-helpers');
 const { web3 } = require('hardhat');
 const { assert } = require('chai');
 const { hex } = require('../utils').helpers;
-const { calculatePurchasedTokens } = require('../utils').tokenPrice;
+const Decimal = require('decimal.js');
+const { calculatePurchasedTokens, calculatePurchasedTokensWithFullIntegral, calculateNXMForEthRelativeError } = require('../utils').tokenPrice;
 const { BN } = web3.utils;
+
+function errorMessage (tokenValue, expectedIdealTokenValue, relativeError) {
+  return `Resulting token value ${tokenValue.toString()} is not close enough to expected ${expectedIdealTokenValue.toFixed()} 
+    Relative error: ${relativeError}`;
+}
 
 describe('calculateNXMForEth', function () {
   it('calculates token value received for an increment in total assets correctly', async function () {
@@ -21,13 +27,13 @@ describe('calculateNXMForEth', function () {
     );
 
     const { tokens: expectedtokenValue } = calculatePurchasedTokens(
-      initialAssetValue, deltaEth, mcrEth, c, a.mul(new BN(1e13.toString())), tokenExponent,
+      initialAssetValue, deltaEth, mcrEth
     );
 
     assert.equal(tokenValue.toString(), expectedtokenValue.toString());
   });
 
-  it('calculates token value received for an increment in total assets correctly for asset value = 0', async function () {
+  it('calculates NXM for ETH at totalAssetValue = 0', async function () {
     const { pool1, poolData, tokenData } = this;
 
     const { _a: a, _c: c } = await poolData.getTokenPriceDetails(hex('ETH'));
@@ -46,5 +52,21 @@ describe('calculateNXMForEth', function () {
     );
 
     assert.equal(tokenValue.toString(), expectedtokenValue.toString());
+  });
+
+  it.only('calculates NXM for ETH at mcrEth = 160k, MCR% = 200%, buyValue = 0.0001', async function () {
+    const { pool1 } = this;
+
+    const mcrEth = ether('160000');
+    const totalAssetValue = mcrEth.mul(new BN(2));
+    const buyValue = ether('0.0001');
+    const maxRelativeError = Decimal(0.0006);
+
+    const tokenValue = await pool1.calculateNXMForEth(
+      buyValue, totalAssetValue, mcrEth,
+    );
+
+    const { relativeError, expectedIdealTokenValue } = calculateNXMForEthRelativeError(totalAssetValue, buyValue, mcrEth, tokenValue);
+    assert(relativeError.lt(maxRelativeError), errorMessage(tokenValue, expectedIdealTokenValue, relativeError));
   });
 });

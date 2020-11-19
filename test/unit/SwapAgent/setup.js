0d6ffb22360355bf38450be2fbb4421be5b1ad7c
shark0der <tolean.dj@gmail.com>
2020-11-19 20:39:11 +0200

    Add setup for SwapAgent unit tests

--- test/unit/SwapAgent/setup.js
+++ test/unit/SwapAgent/setup.js
@@ -1,31 +1,90 @@
 const { accounts, artifacts, web3 } = require('hardhat');
 
-const { toWei } = web3.utils;
-const zeroAddress = '0x0000000000000000000000000000000000000000';
+const { impersonateAccount } = require('../utils').hardhat;
+const { ether } = require('../utils').helpers;
+
+const uniswapContract = contractName => {
+  const TruffleContract = require('@truffle/contract');
+  const jsonPath = `@uniswap/v2-core/build/${contractName}.json`;
+  const contract = TruffleContract(require(jsonPath));
+  contract.setProvider(web3.currentProvider);
+  return contract;
+};
+
+const [owner] = accounts;
+const uniswapDeployer = '0x9c33eacc2f50e39940d3afaf2c7b8246b681a374';
+const uniswapOwner = '0xc0a4272bb5df52134178df25d77561cfb17ce407';
 
 const ERC20Mock = artifacts.require('ERC20Mock');
-const Pool = artifacts.require('Pool');
+const WETH9 = artifacts.require('WETH9');
+const TwapOracle = artifacts.require('TwapOracle');
+
+const UniswapV2Factory = artifacts.require('UniswapV2Factory');
+const UniswapV2Pair = artifacts.require('UniswapV2Pair');
+const UniswapV2Router01 = artifacts.require('UniswapV2Router01');
+
+// will be assigned by setup()
+let contracts;
 
 async function setup () {
 
-  const assetA = await ERC20Mock.new();
-  const assetB = await ERC20Mock.new();
+  await impersonateAccount(uniswapDeployer);
+  await web3.eth.sendTransaction({
+    from: owner,
+    to: uniswapDeployer,
+    value: ether('1'),
+  });
 
-  const pool = await Pool.new(
-    [assetA.address, assetB.address],
-    [0, 0],
-    [toWei('10'), toWei('10')],
-    zeroAddress,
-    zeroAddress,
-    zeroAddress,
-    zeroAddress,
+  const weth = await WETH9.new();
+  const tokenA = await ERC20Mock.new();
+  const tokenB = await ERC20Mock.new();
+
+  // deploying factory using truffle contract to have the correct addresses
+  // use hardhat's artifacts later on for interaction
+  const _factory = await uniswapContract('UniswapV2Factory').new(
+    uniswapOwner,
+    { from: uniswapDeployer },
   );
 
-  this.contracts = {
-    assetA,
-    assetB,
-    pool,
+  /** @var {UniswapV2FactoryInstance} factory */
+  const factory = await UniswapV2Factory.at(_factory.address);
+  const router = await UniswapV2Router01.new(factory.address, weth.address);
+  const oracle = await TwapOracle.new(factory.address);
+
+  await factory.createPair(weth.address, tokenA.address);
+  await factory.createPair(weth.address, tokenB.address);
+
+  const wethAPairAddress = await oracle.pairFor(weth.address, tokenA.address);
+  const wethBPairAddress = await oracle.pairFor(weth.address, tokenB.address);
+
+  const wethAPair = await UniswapV2Pair.at(wethAPairAddress);
+  const wethBPair = await UniswapV2Pair.at(wethBPairAddress);
+
+  contracts = {
+    factory,
+    oracle,
+    router,
+    tokenA,
+    tokenB,
+    weth,
+    wethAPair,
+    wethBPair,
   };
 }
 
 module.exports = setup;
+
+/**
+ * @typedef {object} SwapAgentContracts
+ * @property {UniswapV2FactoryInstance} factory
+ * @property {TwapOracleInstance} oracle
+ * @property {UniswapV2Router01Instance} router
+ * @property {ERC20MockInstance} tokenA
+ * @property {ERC20MockInstance} tokenB
+ * @property {Weth9Instance} weth
+ * @property {UniswapV2PairInstance} wethAPair
+ * @property {UniswapV2PairInstance} wethBPair
+ *
+ * @returns {SwapAgentContracts}
+ */
+module.exports.contracts = () => contracts;

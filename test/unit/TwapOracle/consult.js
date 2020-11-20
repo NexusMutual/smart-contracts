6b4f752fbbe7686ecba49018ec6180b9ad1617c3
shark0der <tolean.dj@gmail.com>
2020-11-20 22:32:31 +0200

    Add last TwapOracle unit tests

--- test/unit/TwapOracle/consult.js
+++ test/unit/TwapOracle/consult.js
@@ -1,19 +1,122 @@
-const { artifacts, web3 } = require('hardhat');
+const { accounts, artifacts, web3 } = require('hardhat');
 const { assert } = require('chai');
-const { ether } = require('@openzeppelin/test-helpers');
+const { ether, expectRevert } = require('@openzeppelin/test-helpers');
 
 const { contracts } = require('./setup');
-const { setNextBlockTime } = require('../utils').hardhat;
+const { setNextBlockTime, mineNextBlock } = require('../utils').hardhat;
 
 const { toBN } = web3.utils;
+const [owner] = accounts;
 
 const PERIOD_SIZE = 1800;
 const PERIODS_PER_WINDOW = 8;
+const WINDOW_SIZE = PERIOD_SIZE * PERIODS_PER_WINDOW;
+
 const timestampToBucket = timestamp => toBN(timestamp).divn(PERIOD_SIZE).modn(PERIODS_PER_WINDOW);
 
-/** @var {ToMockUniswapPairContract} UniswapPair */
-const UniswapPair = artifacts.require('TOMockUniswapPair');
-
 describe('consult', function () {
+
+  it('reverts when missing historical readings', async function () {
+
+    const { oracle } = contracts();
+    const bogus0 = '0xc0ffeec0ffeec0ffeec0ffeec0ffeec0ffee0000';
+    const bogus1 = '0xc0ffeec0ffeec0ffeec0ffeec0ffeec0ffee0001';
+
+    await expectRevert(
+      oracle.consult(bogus0, ether('1'), bogus1),
+      'TWAP: missing historical reading',
+    );
+  });
+
+  it('reverts when the bucket reading is too old', async function () {
+
+    const { oracle, router, tokenA, weth, wethAPair } = contracts();
+
+    await tokenA.mint(owner, ether('100'));
+    await tokenA.approve(router.address, -1);
+
+    await weth.deposit({ value: ether('100') });
+    await weth.approve(router.address, -1);
+
+    await router.addLiquidity(
+      tokenA.address, // tokenA
+      weth.address, // tokenB
+      ether('100'), // amountADesired
+      ether('50'), // amountBDesired
+      ether('0'), // amountAMin
+      ether('0'), // amountBMin
+      owner, // send lp tokens to
+      -1, // deadline infinity
+    );
+
+    // window 0, period 0
+    const period0 = toBN(1800000000);
+    // window 1, period 7 (8 + 7 = 15)
+    const period15 = toBN(1800000000 + WINDOW_SIZE * 2 - PERIOD_SIZE);
+
+    assert.deepEqual(
+      [period0, period15].map(p => timestampToBucket(p).toString()),
+      ['0', '7'],
+      'bucket index assertion failed',
+    );
+
+    await setNextBlockTime(period0.toNumber());
+    await oracle.update([wethAPair.address]);
+
+    await setNextBlockTime(period15.toNumber());
+    await mineNextBlock();
+
+    await expectRevert(
+      oracle.consult(tokenA.address, ether('1'), weth.address),
+      'TWAP: missing historical reading',
+    );
+  });
+
+  it('works with a start of period update and an end of period consult', async function () {
+
+    const { oracle, router, tokenA, weth, wethAPair } = contracts();
+
+    await tokenA.mint(owner, ether('100'));
+    await tokenA.approve(router.address, -1);
+
+    await weth.deposit({ value: ether('100') });
+    await weth.approve(router.address, -1);
+
+    await router.addLiquidity(
+      tokenA.address, // tokenA
+      weth.address, // tokenB
+      ether('100'), // amountADesired
+      ether('50'), // amountBDesired
+      ether('0'), // amountAMin
+      ether('0'), // amountBMin
+      owner, // send lp tokens to
+      -1, // deadline infinity
+    );
+
+    // [period 3, period 2] window
+    const period3 = toBN(1800000000 + PERIOD_SIZE * 3); // period start
+    const period2 = period3.addn(WINDOW_SIZE - 1); // period end
+
+    assert.deepEqual(
+      [period3, period2].map(p => timestampToBucket(p).toString()),
+      ['3', '2'],
+      'bucket index assertion failed',
+    );
+
+    await setNextBlockTime(period3.toNumber());
+    await oracle.update([wethAPair.address]);
+
+    const bucket3 = await oracle.buckets(wethAPair.address, '3');
+    assert.strictEqual(
+      bucket3.timestamp.toString(),
+      period3.toString(),
+      'oracle update failed',
+    );
+
+    await setNextBlockTime(period2.toNumber());
+    await mineNextBlock();
+
+    await oracle.consult(tokenA.address, ether('1'), weth.address);
+  });
 
 });
--- test/unit/TwapOracle/setup.js
+++ test/unit/TwapOracle/setup.js
@@ -1,22 +1,85 @@
-const { artifacts } = require('hardhat');
+const { accounts, artifacts, web3 } = require('hardhat');
 
-const TwapOracle = artifacts.require('TwapOracle');
+const { impersonateAccount } = require('../utils').hardhat;
+const { ether } = require('@openzeppelin/test-helpers');
+
+// actual uniswap factory address on all chains
+const UNISWAP_FACTORY = '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f';
 
 // will be assigned by setup()
-let contracts;
+const instances = {};
+
+const uniswapContract = contractName => {
+  const TruffleContract = require('@truffle/contract');
+  const jsonPath = `@uniswap/v2-core/build/${contractName}.json`;
+  const contract = TruffleContract(require(jsonPath));
+  contract.setProvider(web3.currentProvider);
+  return contract;
+};
 
 async function setup () {
 
-  // needs actual uniswap factory address
-  const factoryAddress = '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f';
-  const oracle = await TwapOracle.new(factoryAddress);
+  const [owner] = accounts;
+  const uniswapDeployer = '0x9c33eacc2f50e39940d3afaf2c7b8246b681a374';
+  const uniswapOwner = '0xc0a4272bb5df52134178df25d77561cfb17ce407';
 
-  contracts = {
-    oracle,
-  };
+  /* load artifacts */
+
+  const ERC20Mock = artifacts.require('ERC20Mock');
+  const WETH9 = artifacts.require('WETH9');
+  const TwapOracle = artifacts.require('TwapOracle');
+
+  const UniswapV2Factory = artifacts.require('UniswapV2Factory');
+  const UniswapV2Pair = artifacts.require('UniswapV2Pair');
+  const UniswapV2Router01 = artifacts.require('UniswapV2Router01');
+
+  /* deploy tokens */
+
+  const tokenA = await ERC20Mock.new();
+  const tokenB = await ERC20Mock.new();
+  const weth = await WETH9.new();
+
+  /* deploy uniswap */
+
+  await impersonateAccount(uniswapDeployer);
+  await web3.eth.sendTransaction({
+    from: owner,
+    to: uniswapDeployer,
+    value: ether('1'),
+  });
+
+  // deploying factory using truffle contract to have the correct addresses
+  // use hardhat's artifacts later on for interaction
+  const _factory = await uniswapContract('UniswapV2Factory').new(
+    uniswapOwner,
+    { from: uniswapDeployer },
+  );
+
+  // check that we landed at the correct address
+  assert.strictEqual(_factory.address, UNISWAP_FACTORY);
+
+  /** @var {UniswapV2FactoryInstance} factory */
+  const factory = await UniswapV2Factory.at(_factory.address);
+  const router = await UniswapV2Router01.new(factory.address, weth.address);
+  const oracle = await TwapOracle.new(factory.address);
+
+  await factory.createPair(weth.address, tokenA.address);
+  await factory.createPair(weth.address, tokenB.address);
+
+  const wethAPairAddress = await oracle.pairFor(weth.address, tokenA.address);
+  const wethBPairAddress = await oracle.pairFor(weth.address, tokenB.address);
+
+  const wethAPair = await UniswapV2Pair.at(wethAPairAddress);
+  const wethBPair = await UniswapV2Pair.at(wethBPairAddress);
+
+  /* deploy our contracts */
+
+  const main = { factory, router, oracle };
+  const tokens = { weth, tokenA, tokenB };
+  const pairs = { wethAPair, wethBPair };
+
+  Object.assign(instances, { ...main, ...tokens, ...pairs });
 }
-
-module.exports = setup;
 
 /**
  * @typedef {object} TwapOracleContracts
@@ -28,7 +91,9 @@ module.exports = setup;
  * @property {Weth9Instance} weth
  * @property {UniswapV2PairInstance} wethAPair
  * @property {UniswapV2PairInstance} wethBPair
- *
- * @returns {TwapOracleContracts}
  */
-module.exports.contracts = () => contracts;
+
+module.exports = setup;
+
+/** @returns {TwapOracleContracts} */
+module.exports.contracts = () => instances;

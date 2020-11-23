50035672304d5f71b81b8224f32248c0fe8ddaba
shark0der <tolean.dj@gmail.com>
2020-11-23 16:46:19 +0200

    Replace typchain generation plugin with a task. Fixes typechain generation error

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,5 @@
 require('@nomiclabs/hardhat-web3');
 require('@nomiclabs/hardhat-truffle5');
-require('hardhat-typechain');
 
 const { task } = require('hardhat/config');
 const ether = n => `${n}${'0'.repeat(18)}`;
@@ -9,6 +8,21 @@ task('test', async (_, hre, runSuper) => {
   hre.accounts = await hre.web3.eth.getAccounts();
   const testFiles = _.testFiles.length ? _.testFiles : ['./test/index.js'];
   await runSuper({ testFiles });
+});
+
+task('typechain', async (_, { config }) => {
+
+  const { tsGenerator } = require('ts-generator');
+  const { TypeChain } = require('typechain/dist/TypeChain');
+
+  const cwd = process.cwd();
+  const rawConfig = {
+    files: `${config.paths.artifacts}/!(build-info|hardhat)/**/+([a-zA-Z0-9]).json`,
+    outDir: 'types',
+    target: 'truffle-v5',
+  };
+
+  await tsGenerator({ cwd }, new TypeChain({ cwd, rawConfig }));
 });
 
 module.exports = {

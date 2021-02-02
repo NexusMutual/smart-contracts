2cfe1718f700abe417b4e0544ec346a2cb5a3983
Dragos Horodnic <dragos@horodnic.com>
2021-02-02 17:58:38 +0200

    Replace tabs with spaces

--- contracts/mocks/DisposableGovernance.sol
+++ contracts/mocks/DisposableGovernance.sol
@@ -112,11 +112,11 @@ contract DisposableGovernance is IGovernance, Iupgradable {
   uint internal actionWaitingTime;
 
   /* function required for Iupgradable and IGovernance implementation */
-	function changeDependentContractAddress() public {
-		tokenInstance = TokenController(ms.dAppLocker());
-		memberRole = MemberRoles(ms.getLatestAddress("MR"));
-		proposalCategory = ProposalCategory(ms.getLatestAddress("PC"));
-	}
+  function changeDependentContractAddress() public {
+    tokenInstance = TokenController(ms.dAppLocker());
+    memberRole = MemberRoles(ms.getLatestAddress("MR"));
+    proposalCategory = ProposalCategory(ms.getLatestAddress("PC"));
+  }
 
   function createProposal(string calldata, string calldata, string calldata, uint) external {}
 

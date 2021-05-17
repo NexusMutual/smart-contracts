90c5646c752a506ca836e96e0c10d4a67579c1db
shark0der <commits@shark0der.com>
2021-05-17 21:23:03 +0300

    Fix SPDX license identifier

--- contracts/interfaces/INXMaster.sol
+++ contracts/interfaces/INXMaster.sol
@@ -1,22 +1,5 @@
-// SPDX-License-Identifier: AGPL-3.0-only
-
-/* Copyright (C) 2020 NexusMutual.io
-
-  This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-  This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-  You should have received a copy of the GNU General Public License
-    along with this program.  If not, see http://www.gnu.org/licenses/ */
-
+// SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.0;
-
 
 interface INXMMaster {
   function getLatestAddress(bytes2 _contractName) external view returns (address payable contractAddress);

f5e220e5e493c5e9784cc73a9f3c690ac4599dc2
Dragos Horodnic <dragos@horodnic.com>
2021-05-18 16:51:59 +0300

    Remove misleading comment
    The issue was actually caused by two transactions sent with the
    purpose to advance the nonce that for some reason consumed wai too
    much gas if not provided with explicit gas value.

--- .solcover.js
+++ .solcover.js
@@ -7,9 +7,7 @@ module.exports = {
     'modules/token/external',
     'modules/governance/external',
   ],
-  // Prevents these errors (probably because coverage is compiled without gas optimizations)
-  // Error: Returned error: sender doesn't have enough funds to send tx. The upfront cost is: 8796093022200000000000 and the sender's account only has: 975594920000000000
   providerOptions: {
-      default_balance_ether: 100000000,
-  }
+    default_balance_ether: 100000000,
+  },
 };

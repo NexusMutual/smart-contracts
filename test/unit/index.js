f973e22710d466a19b6f1262e334a2f2c7794ab0
Rox <roxanadanila24@gmail.com>
2021-05-20 14:57:49 +0300

    Additional hack for extra speed on first test run

--- test/unit/index.js
+++ test/unit/index.js
@@ -1,10 +1,10 @@
 describe('UNIT TESTS', function () {
 
+  require('./TokenController');
   require('./ClaimProofs');
   require('./PooledStaking');
   require('./Pool');
   require('./SwapOperator');
   require('./MCR');
-  require('./TokenController');
 
 });

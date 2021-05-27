dec7797d1d7e72bd206ac872e51f9265d89de04b
Dan Octavian <danoctavian91@gmail.com>
2021-05-27 15:08:58 +0800

    fix steth require issue that prohibits multiple sequential swap calls

--- test/unit/SwapOperator/swapETHForStETH.js
+++ test/unit/SwapOperator/swapETHForStETH.js
@@ -112,6 +112,30 @@ describe('swapEthForStETH', function () {
     );
   });
 
+  it('should revert if balanceAfter > max', async function () {
+    const { pool, swapOperator, lido } = contracts();
+    const windowStart = await nextWindowStartTime();
+
+    const minStEthAmount = ether('100');
+    const maxStEthAmount = ether('1000');
+    await pool.setAssetDetails(
+      lido.address,
+      minStEthAmount, // asset minimum
+      maxStEthAmount, // asset maximum
+      ether('0.01'), // max slippage
+      { from: governance },
+    );
+
+    // should be able to swap only during the last period within the window
+    await setNextBlockTime(windowStart + periodSize * 7);
+
+    const etherIn = maxStEthAmount.addn(10001);
+    await expectRevert(
+      swapOperator.swapETHForStETH(etherIn),
+      'SwapOperator: balanceAfter > max',
+    );
+  });
+
   it('should swap asset for eth and emit a Swapped event with correct values', async function () {
     const { pool, tokenA, swapOperator, lido } = contracts();
     const windowStart = await nextWindowStartTime();
@@ -149,5 +173,118 @@ describe('swapEthForStETH', function () {
       amountIn: etherIn,
       amountOut: tokensReceived,
     });
+  });
+
+  it('should swap asset for eth for a dust amount of wei equal to the precision error tolerance', async function () {
+    const { pool, tokenA, swapOperator, lido } = contracts();
+    const windowStart = await nextWindowStartTime();
+
+    await pool.setAssetDetails(
+      lido.address,
+      ether('100'), // asset minimum
+      ether('1000'), // asset maximum
+      ether('0.01'), // max slippage
+      { from: governance },
+    );
+
+    // should be able to swap only during the last period within the window
+    await setNextBlockTime(windowStart + periodSize * 7);
+
+    const etherBefore = toBN(await web3.eth.getBalance(pool.address));
+    const tokensBefore = await tokenA.balanceOf(pool.address);
+
+    // amounts in/out of the trade
+    const etherIn = toBN('10000');
+    const minTokenOut = etherIn.subn(1);
+    await swapOperator.swapETHForStETH(etherIn);
+    const etherAfter = toBN(await web3.eth.getBalance(pool.address));
+    const tokensAfter = await lido.balanceOf(pool.address);
+    const etherSent = etherBefore.sub(etherAfter);
+    const tokensReceived = tokensAfter.sub(tokensBefore);
+
+    assert.strictEqual(etherSent.toString(), etherIn.toString());
+    assert(tokensReceived.gte(minTokenOut), 'tokensReceived < minTokenOut');
+  });
+
+  it('should swap asset for eth in 3 sequential calls', async function () {
+    const { pool, tokenA, swapOperator, lido, twapOracle } = contracts();
+    const windowStart = await nextWindowStartTime();
+
+    const minStEthAmount = ether('100');
+
+    await pool.setAssetDetails(
+      lido.address,
+      minStEthAmount, // asset minimum
+      ether('1000'), // asset maximum
+      ether('0.01'), // max slippage
+      { from: governance },
+    );
+
+    // should be able to swap only during the last period within the window
+    await setNextBlockTime(windowStart + periodSize * 7);
+
+    {
+      const etherBefore = toBN(await web3.eth.getBalance(pool.address));
+      const tokensBefore = await tokenA.balanceOf(pool.address);
+
+      // amounts in/out of the trade
+      const etherIn = minStEthAmount.divn(3);
+      const minTokenOut = etherIn.subn(1);
+      const swapTx = await swapOperator.swapETHForStETH(etherIn);
+
+      const etherAfter = toBN(await web3.eth.getBalance(pool.address));
+      const tokensAfter = await lido.balanceOf(pool.address);
+      const etherSent = etherBefore.sub(etherAfter);
+      const tokensReceived = tokensAfter.sub(tokensBefore);
+
+      assert.strictEqual(etherSent.toString(), etherIn.toString());
+      assert(tokensReceived.gte(minTokenOut), 'tokensReceived < minTokenOut');
+    }
+
+    await time.increase(periodSize);
+
+    {
+      const etherBefore = toBN(await web3.eth.getBalance(pool.address));
+      const tokensBefore = await tokenA.balanceOf(pool.address);
+
+      // amounts in/out of the trade
+      const etherIn = minStEthAmount.divn(3);
+      const minTokenOut = etherIn.subn(1);
+      await swapOperator.swapETHForStETH(etherIn);
+
+      const etherAfter = toBN(await web3.eth.getBalance(pool.address));
+      const tokensAfter = await lido.balanceOf(pool.address);
+      const etherSent = etherBefore.sub(etherAfter);
+      const tokensReceived = tokensAfter.sub(tokensBefore);
+
+      assert.strictEqual(etherSent.toString(), etherIn.toString());
+      assert(tokensReceived.gte(minTokenOut), 'tokensReceived < minTokenOut');
+    }
+
+    await time.increase(periodSize);
+
+    {
+      const etherBefore = toBN(await web3.eth.getBalance(pool.address));
+      const tokensBefore = await tokenA.balanceOf(pool.address);
+
+      // amounts in/out of the trade
+      const etherIn = minStEthAmount.divn(2);
+      const minTokenOut = etherIn.subn(1);
+      await swapOperator.swapETHForStETH(etherIn);
+
+      const etherAfter = toBN(await web3.eth.getBalance(pool.address));
+      const tokensAfter = await lido.balanceOf(pool.address);
+      const etherSent = etherBefore.sub(etherAfter);
+      const tokensReceived = tokensAfter.sub(tokensBefore);
+
+      assert.strictEqual(etherSent.toString(), etherIn.toString());
+      assert(tokensReceived.gte(minTokenOut), 'tokensReceived < minTokenOut');
+    }
+
+    const etherIn = minStEthAmount.divn(2);
+    await expectRevert(
+      swapOperator.swapETHForStETH(etherIn),
+      'SwapOperator: balanceBefore >= min',
+    );
   });
 });

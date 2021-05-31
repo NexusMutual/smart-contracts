10af400318d21e382d9fef6f8f436c1a2710df95
Dan Octavian <danoctavian91@gmail.com>
2021-05-31 14:59:24 +0800

    add distributor

---
+++ contracts/interfaces/v0.8/INXMaster.sol
@@ -0,0 +1,29 @@
+/* Copyright (C) 2021 NexusMutual.io
+
+  This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+    along with this program.  If not, see http://www.gnu.org/licenses/ */
+
+pragma solidity ^0.8.0;
+
+interface INXMaster {
+
+    function masterInitialized() external view returns(bool);
+
+    function isPause() external view returns(bool check);
+
+    function isMember(address _add) external view returns(bool);
+
+    function getLatestAddress(bytes2 _contractName) external view returns(address payable contractAddress);
+
+    function tokenAddress() external view returns (address);
+}
---
+++ contracts/modules/distributor/DistributorFactory.sol
@@ -0,0 +1,44 @@
+pragma solidity ^0.8.0;
+
+import "./Distributor.sol";
+import "../../interfaces/v0.8/INXMaster.sol";
+import "../../interfaces/v0.8/IMemberRoles.sol";
+
+contract DistributorFactory {
+    INXMaster immutable public master;
+
+    event DistributorCreated(
+        address contractAddress,
+        address owner,
+        uint feePercentage,
+        address treasury
+    );
+
+    constructor (address masterAddress) {
+        master = INXMaster(masterAddress);
+    }
+
+    function newDistributor(
+        uint _feePercentage,
+        address payable treasury,
+        string memory tokenName,
+        string memory tokenSymbol
+    ) public payable returns (address) {
+
+        IMemberRoles memberRoles = IMemberRoles(master.getLatestAddress("MR"));
+        Distributor d = new Distributor(
+            master.getLatestAddress("GW"),
+            master.tokenAddress(),
+            address(master),
+            _feePercentage,
+            treasury,
+            tokenName,
+            tokenSymbol
+        );
+        d.transferOwnership(msg.sender);
+        memberRoles.payJoiningFee{ value: msg.value}(address(d));
+
+        emit DistributorCreated(address(d), msg.sender, _feePercentage, treasury);
+        return address(d);
+    }
+}

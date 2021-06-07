026c887de85f1db2778a312c953b131e16c68a62
shark0der <commits@shark0der.com>
2021-06-07 13:37:55 +0300

    Remove .only from distributor unit tests

--- test/unit/Distributor/index.js
+++ test/unit/Distributor/index.js
@@ -1,7 +1,7 @@
 const { takeSnapshot, revertToSnapshot, reset } = require('../utils').evm;
 const { setup } = require('./setup');
 
-describe.only('Distributor unit tests', function () {
+describe('Distributor unit tests', function () {
   before(reset);
   before(setup);
 

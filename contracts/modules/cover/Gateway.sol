6db67299bd42a7edb8fc4e31dede1d71ecf8497d
shark0der <commits@shark0der.com>
2021-06-10 12:54:46 +0300

    Add DAI initializer to Gateway.sol

--- contracts/modules/cover/Gateway.sol
+++ contracts/modules/cover/Gateway.sol
@@ -81,6 +81,10 @@ contract Gateway is MasterAware {
     memberRoles = MemberRoles(master.getLatestAddress("MR"));
   }
 
+  function initializeDAI() external {
+    DAI = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
+  }
+
   function getCoverPrice (
     address /* contractAddress */,
     address /* coverAsset */,

778cf4950043f3044eee2ea5cb0019db3317278e
Dan Octavian <danoctavian91@gmail.com>
2021-06-16 18:46:42 +0800

    add basic functionality tests

---
+++ test/fork/sample-test.js
@@ -0,0 +1,64 @@
+const fetch = require('node-fetch');
+const { artifacts, web3, accounts, network } = require('hardhat');
+const { ether, time } = require('@openzeppelin/test-helpers');
+
+const {
+  submitGovernanceProposal,
+  getAddressByCodeFactory,
+  Address,
+  fund,
+  unlock,
+} = require('./utils');
+const { hex } = require('../utils').helpers;
+const { ProposalCategory, CoverStatus } = require('../utils').constants;
+
+const OwnedUpgradeabilityProxy = artifacts.require('OwnedUpgradeabilityProxy');
+const MemberRoles = artifacts.require('MemberRoles');
+const NXMaster = artifacts.require('NXMaster');
+const NXMToken = artifacts.require('NXMToken');
+const Governance = artifacts.require('Governance');
+const TokenFunctions = artifacts.require('TokenFunctions');
+const Quotation = artifacts.require('Quotation');
+const TokenController = artifacts.require('TokenController');
+const Gateway = artifacts.require('Gateway');
+const Incidents = artifacts.require('Incidents');
+const ERC20MintableDetailed = artifacts.require('ERC20MintableDetailed');
+const Pool = artifacts.require('Pool');
+
+describe('sample test', function () {
+
+  this.timeout(0);
+
+  it('initializes contracts', async function () {
+
+    const { mainnet: { abis } } = await fetch('https://api.nexusmutual.io/version-data/data.json').then(r => r.json());
+    const getAddressByCode = getAddressByCodeFactory(abis);
+
+    this.token = await NXMToken.at(getAddressByCode('NXMTOKEN'));
+    this.memberRoles = await MemberRoles.at(getAddressByCode('MR'));
+    this.master = await NXMaster.at(getAddressByCode(('NXMASTER')));
+    this.governance = await Governance.at(getAddressByCode('GV'));
+    this.tokenController = await TokenController.at(getAddressByCode('TC'));
+    this.quotation = await Quotation.at(getAddressByCode('QT'));
+    this.incidents = await Incidents.at(getAddressByCode('IC'));
+    this.pool = await Pool.at(getAddressByCode('IC'));
+    this.dai = await ERC20MintableDetailed.at(Address.DAI);
+  });
+
+  it('funds accounts', async function () {
+
+    console.log('Funding accounts');
+
+    const { memberArray: boardMembers } = await this.memberRoles.members('1');
+    const voters = boardMembers.slice(1, 4);
+
+    for (const member of [...voters, Address.NXMHOLDER]) {
+      await fund(member);
+      await unlock(member);
+    }
+
+    this.voters = voters;
+  });
+
+  require('./basic-functionality-tests');
+});

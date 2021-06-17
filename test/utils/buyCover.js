d06127960b3abba5ee41a75831c81cb86e7b2c50
Dan Octavian <danoctavian91@gmail.com>
2021-06-17 16:46:49 +0800

    add gateway cover purchase test

--- test/utils/buyCover.js
+++ test/utils/buyCover.js
@@ -1,6 +1,9 @@
+const { ether, time } = require('@openzeppelin/test-helpers');
 const { getQuoteSignature } = require('./getQuote');
 const { web3 } = require('hardhat');
 const { toBN } = web3.utils;
+
+const ETH = '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE';
 
 function coverToCoverDetailsArray (cover) {
   return [cover.amount, cover.price, cover.priceNXM, cover.expireTime, cover.generationTime];
@@ -54,4 +57,56 @@ async function buyCoverWithDai ({ cover, coverHolder, qt, p1, dai }) {
   );
 }
 
-module.exports = { buyCover, coverToCoverDetailsArray, buyCoverWithDai };
+async function getBuyCoverDataParameter ({ qt, coverData }) {
+
+  // encoded data and signature uses unit price.
+  const unitAmount = toBN(coverData.amount).div(ether('1')).toString();
+  const [v, r, s] = await getQuoteSignature(
+    coverToCoverDetailsArray({ ...coverData, amount: unitAmount }),
+    coverData.currency,
+    coverData.period,
+    coverData.contractAddress,
+    qt.address,
+  );
+  return web3.eth.abi.encodeParameters(
+    ['uint', 'uint', 'uint', 'uint', 'uint8', 'bytes32', 'bytes32'],
+    [coverData.price, coverData.priceNXM, coverData.expireTime, coverData.generationTime, v, r, s],
+  );
+}
+
+async function buyCoverThroughGateway ({ coverData, gateway, coverHolder, qt, dai }) {
+
+  const price = toBN(coverData.price);
+  // encoded data and signature uses unit price.
+  const data = await getBuyCoverDataParameter({ qt, coverData });
+
+  if (coverData.asset === ETH) {
+    return gateway.buyCover(
+      coverData.contractAddress,
+      coverData.asset,
+      coverData.amount,
+      coverData.period,
+      coverData.type,
+      data, {
+        from: coverHolder,
+        value: price,
+      });
+  } else if (coverData.asset === dai.address) {
+    await dai.approve(gateway.address, price, {
+      from: coverHolder,
+    });
+    return gateway.buyCover(
+      coverData.contractAddress,
+      coverData.asset,
+      coverData.amount,
+      coverData.period,
+      coverData.type,
+      data, {
+        from: coverHolder,
+      });
+  }
+
+  throw new Error(`Unknown asset ${coverData.asset}`);
+}
+
+module.exports = { buyCover, coverToCoverDetailsArray, buyCoverWithDai, buyCoverThroughGateway };

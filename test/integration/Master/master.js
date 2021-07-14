3e631a369336086dd289fd2b911abc5b2be385a4
Dan Octavian <danoctavian91@gmail.com>
2021-07-14 22:37:27 +0800

    add governance utils

--- test/integration/Master/master.js
+++ test/integration/Master/master.js
@@ -2,9 +2,15 @@ const { accounts, web3 } = require('hardhat');
 const { expectEvent, expectRevert, time } = require('@openzeppelin/test-helpers');
 const { assert } = require('chai');
 const { ProposalCategory } = require('../utils').constants;
-const { hex } = require('../utils').helpers;
+const { submitProposal } = require('../utils').governance;
 
 const [owner, emergencyAdmin, unknown] = accounts;
 
 describe('master', function () {
+
+  it('adds new contract', async function () {
+    const { master, gv, pc, tk } = this.contracts;
+
+    await submitProposal(gv, pc, ProposalCategory.newContract);
+  });
 });

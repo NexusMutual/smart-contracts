6c3f7b49833d952d5a46ba982e7acdf548549661
Dan Octavian <danoctavian91@gmail.com>
2021-08-17 16:15:01 +0800

    ignore monitoring coverage

--- .solcover.js
+++ .solcover.js
@@ -7,6 +7,7 @@ module.exports = {
     'utils/',
     'modules/token/external',
     'modules/governance/external',
+    'modules/monitoring',
   ],
   providerOptions: {
     default_balance_ether: 100000000,

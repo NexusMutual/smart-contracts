ebbcd093e5de97356b0741a265863ca4097045ad
Dragos Horodnic <dragos@horodnic.com>
2021-08-20 13:08:19 +0300

    Move AssessmentViews to utils folder

---
+++ contracts/utils/AssessmentViews.sol
@@ -0,0 +1,127 @@
+// SPDX-License-Identifier: GPL-3.0-only
+
+pragma solidity ^0.8.0;
+
+import "../../interfaces/IAssessment.sol";
+import "../../abstract/MasterAwareV2.sol";
+import "../../libraries/Assessment/AssessmentUtilsLib.sol";
+
+contract AssessmentViews is MasterAwareV2 {
+  /*
+   *  Claim structure but in a human-friendly format.
+   *
+   *  Contains aggregated values that give an overall view about the claim and other relevant
+   *  pieces of information such as cover period, asset symbol etc. This structure is not used in
+   *  any storage variables.
+   */
+  struct ClaimDisplay {
+    uint id;
+    uint productId;
+    uint coverId;
+    uint amount;
+    string assetSymbol;
+    uint coverStart;
+    uint coverEnd;
+    uint start;
+    uint end;
+    string claimStatus;
+    string payoutStatus;
+  }
+
+  constructor(address _master) {
+    master = INXMMaster(_master);
+  }
+
+  function assessment() internal view returns (IAssessment) {
+    return IAssessment(getInternalContractAddress(ID.AS));
+  }
+
+  function changeDependentContractAddress() external override {
+    INXMMaster master = INXMMaster(master);
+    internalContracts[uint(ID.AS)] = master.getLatestAddress("AS");
+  }
+
+  /**
+   *  Returns a Claim aggregated in a human-friendly format.
+   *
+   *  @dev This view is meant to be used in user interfaces to get a claim in a format suitable for
+   *  displaying all relevant information in as few calls as possible. See ClaimDisplay struct.
+   *
+   *  @param id    Claim identifier for which the ClaimDisplay is returned
+   */
+  function getClaimToDisplay (uint id) public view returns (ClaimDisplay memory) {
+    (
+      IAssessment.Poll memory poll,
+      IAssessment.ClaimDetails memory details
+    ) = assessment().claims(id);
+
+    string memory claimStatusDisplay;
+    string memory payoutStatusDisplay;
+    {
+      IAssessment.PollStatus claimStatus = AssessmentUtilsLib._getPollStatus(poll);
+      if (claimStatus == IAssessment.PollStatus.ACCEPTED) {
+        claimStatusDisplay = "Accepted";
+      } else if (claimStatus == IAssessment.PollStatus.DENIED) {
+        claimStatusDisplay = "Denied";
+      } else if (claimStatus == IAssessment.PollStatus.PENDING) {
+        claimStatusDisplay = "Pending";
+      }
+
+      if (claimStatus == IAssessment.PollStatus.DENIED) {
+        payoutStatusDisplay = "Denied";
+      } else if (claimStatus == IAssessment.PollStatus.ACCEPTED && details.payoutRedeemed) {
+        payoutStatusDisplay = "Complete";
+      } else {
+        payoutStatusDisplay = "Pending";
+      }
+    }
+
+    // [todo] Get from covers contract
+    uint coverStart = block.timestamp;
+    uint coverPeriod = 365;
+    uint coverEnd = coverStart + coverPeriod * 1 days;
+    uint productId = 1;
+
+    string memory assetSymbol;
+    {
+      if (IAssessment.Asset(details.payoutAsset) == IAssessment.Asset.ETH) {
+        assetSymbol = "ETH";
+      } else if (IAssessment.Asset(details.payoutAsset) == IAssessment.Asset.DAI) {
+        assetSymbol = "DAI";
+      }
+    }
+
+    return ClaimDisplay(
+      id,
+      productId,
+      details.coverId,
+      details.amount,
+      assetSymbol,
+      coverStart,
+      coverEnd,
+      poll.start,
+      poll.end,
+      claimStatusDisplay,
+      payoutStatusDisplay
+    );
+  }
+
+  /**
+   *  Returns an array of claims aggregated in a human-friendly format.
+   *
+   *  @dev This view is meant to be used in user interfaces to get claims in a format suitable for
+   *  displaying all relevant information in as few calls as possible. It can be used to paginate
+   *  claims by providing the following paramterers:
+   *
+   *  @param from  First claim identifier from the requested range
+   *  @param to    Last claim identifier from the requested range
+   */
+  function getClaimsToDisplay (uint104 from, uint104 to)
+  external view returns (ClaimDisplay[] memory) {
+    ClaimDisplay[] memory claimDisplays = new ClaimDisplay[](to-from+1);
+    for (uint104 id = from; id <= to; id++) {
+      claimDisplays[id - from] = getClaimToDisplay(id);
+    }
+    return claimDisplays;
+  }
+}

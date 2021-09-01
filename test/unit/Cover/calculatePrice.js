ccf2bdb9ee8846dab3f03c8ca20d99230211ad38
Dan Octavian <danoctavian91@gmail.com>
2021-09-01 12:57:41 +0300

    add further test for price

--- test/unit/Cover/calculatePrice.js
+++ test/unit/Cover/calculatePrice.js
@@ -1,12 +1,16 @@
 const { assert } = require('chai');
+const { web3 } = require('hardhat');
 const { ether, time, expectRevert } = require('@openzeppelin/test-helpers');
 const { hex } = require('../utils').helpers;
 
 const accounts = require('../utils').accounts;
+const { calculatePrice } = require('./helpers');
 
-describe('calculatePrice', function () {
+const { toBN } = web3.utils;
 
-  it.only('should return gearedMCR = 0 if there are no active covers', async function () {
+describe.only('calculatePrice', function () {
+
+  it('should calculate price correctly for high active cover', async function () {
     const { cover } = this;
 
     const amount = ether('1000');
@@ -22,9 +26,56 @@ describe('calculatePrice', function () {
       capacity,
     );
 
-    assert.equal();
-    console.log({
-      price: price.toString(),
-    });
+    const expectedPrice = calculatePrice(
+      amount, basePrice, activeCover, capacity,
+    );
+
+    assert.equal(price.toString(), expectedPrice.toString());
+  });
+
+  it('should calculate price correctly for medium-range active cover', async function () {
+    const { cover } = this;
+
+    const amount = ether('1000');
+
+    const basePrice = ether('2.6');
+    const activeCover = ether('5000');
+    const capacity = ether('10000');
+
+    const price = await cover.calculatePrice(
+      amount,
+      basePrice,
+      activeCover,
+      capacity,
+    );
+
+    const expectedPrice = calculatePrice(
+      amount, basePrice, activeCover, capacity,
+    );
+
+    assert.equal(price.toString(), expectedPrice.toString());
+  });
+
+  it('should calculate price correctly for low-range active cover', async function () {
+    const { cover } = this;
+
+    const amount = ether('1000');
+
+    const basePrice = ether('2.6');
+    const activeCover = ether('1000');
+    const capacity = ether('10000');
+
+    const price = await cover.calculatePrice(
+      amount,
+      basePrice,
+      activeCover,
+      capacity,
+    );
+
+    const expectedPrice = calculatePrice(
+      amount, basePrice, activeCover, capacity,
+    );
+
+    assert.equal(price.toString(), expectedPrice.toString());
   });
 });

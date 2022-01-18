527229b37f3e4ec5c54302ebcdf0948fe9a168b9
shark0der <commits@shark0der.com>
2022-01-18 19:46:02 +0200

    ve wip

--- contracts/modules/v2/StakingVe.sol
+++ contracts/modules/v2/StakingVe.sol
@@ -4,36 +4,47 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts-v4/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts-v4/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts-v4/utils/math/SafeCast.sol";
 
 contract StakingVe is ERC721 {
+  using SafeCast for uint;
 
   /* storage */
 
-  uint96 public stakeActive;
-  uint96 public stakeInactive;
-  uint96 public stakeBurned;
+  uint128 public stakeActive;
+  uint128 public accRewardPerToken;
+  uint32 public lastRewardUpdate;
 
-  uint96 public shareSupply;
+  uint16 public currentGroupId;
+  uint16 public lastGroupId;
 
-  uint16 firstGroupId;
-  uint16 lastGroupId;
-
-  uint totalSupply;
+  // erc721 related
+  uint32 public totalSupply;
 
   // stakers are grouped based on the timelock expiration
-  // group index is calculated from the expiration
+  // group index is calculated based on the expiration date
   // the initial proposal is to have 4 groups per year (1 group per quarter)
-  // when staking, all non-expired groups have to be incremented by the amount
-  // we can pack 2 groups in a slot, resulting in 2 slots per year, 8 slots for 4 years
-  // group id => amount
-  mapping(uint => uint) public stakeGroups;
+  struct StakeGroup {
+    uint128 stake;
+    uint128 shares;
+  }
 
-  // group id =>
-  mapping(uint => uint) public earnings;
+  // group id => amount
+  mapping(uint16 => StakeGroup) public stakeGroups;
+
+  // user => group id => amount of shares
+  mapping(address => mapping(uint16 => uint)) public balanceOf;
+
+  // group id => earned
+  mapping(uint16 => uint) public earned;
 
   /* immutables */
 
   IERC20 public immutable nxm;
+
+  /* constants */
+
+  uint GROUP_SIZE = 90 days;
 
   constructor (
     string memory _name,
@@ -43,27 +54,67 @@ contract StakingVe is ERC721 {
     nxm = _token;
   }
 
-  function deposit(uint amount, uint fromGroup, uint toGroup) external {
+  function processGroups() public {
 
-    // transfer nxm from staker
-    nxm.transfer(msg.sender, amount);
+    uint16 targetId = (block.timestamp / GROUP_SIZE).toUint16();
+    uint16 currentId = currentGroupId;
+    uint stake = stakeActive;
 
-    // update buckets
-    for (uint i = fromGroup; i <= toGroup; i++) {
-      stakeGroups[i] += amount;
+    while (currentId < targetId) {
+      // TODO: update group rewards
+      stake += stakeGroups[currentId].stake;
+      currentId++;
     }
 
+    currentGroupId = currentId;
+  }
+
+  function deposit(uint amount, uint16 groupId) external {
+
+    processGroups();
+
+    // require groupId not to be expired
+    require(groupId >= currentGroupId);
+
+    // transfer nxm from staker
+    nxm.transferFrom(msg.sender, address(this), amount);
+
+    uint stake = stakeGroups[groupId].stake;
+    uint shares = stakeGroups[groupId].shares;
+
+    uint userShares = balanceOf[msg.sender][groupId];
+    uint newShares = amount * shares / stake;
+
+    // 1 SSTORE update group stake and shares
+    stakeGroups[groupId].stake = (stake + amount).toUint128();
+    stakeGroups[groupId].shares = (shares + newShares).toUint128();
+
+    // 1 SSTORE update staker's group shares
+    balanceOf[msg.sender][groupId] = (userShares + newShares).toUint128();
+
+    // 1 SSTORE update total active stake
+    stakeActive += amount.toUint128();
+
+    // 2 SSTORE mint nft
     _mint(msg.sender, totalSupply++);
   }
 
   // O(16) ie. O(1)
   function burn(uint amount) public {
 
-    uint first = firstGroupId;
-    uint last = lastGroupId;
+    processGroups();
 
-    for (uint i = first + 1; i <= last; i++) {
-      stakeGroups[i] -= amount;
+    // 1 SLOAD
+    uint totalStake = stakeActive;
+    uint16 first = currentGroupId;
+    uint16 last = lastGroupId;
+
+    for (uint16 i = first; i <= last; i++) {
+      // 1 SLOAD
+      uint stake = stakeGroups[i].stake;
+      uint burnAmount = stake * amount / totalStake;
+      // 1 SSTORE
+      stakeGroups[i].stake -= burnAmount.toUint128();
     }
   }
 

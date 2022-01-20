618a9f71f09a9ea5a7668079faa005886b3fb821
Dan Octavian <danoctavian91@gmail.com>
2022-01-20 18:00:16 +0200

    add interpolatePrice test

---
+++ test/unit/StakingPool/interpolatePrice.js
@@ -0,0 +1,51 @@
+const { assert } = require('chai');
+const { web3, ethers } = require('hardhat');
+const { ether, time, expectRevert } = require('@openzeppelin/test-helpers');
+const { hex } = require('../utils').helpers;
+
+const accounts = require('../utils').accounts;
+const { calculatePrice } = require('./helpers');
+
+const { toBN } = web3.utils;
+
+describe('interpolatePrice', function () {
+
+  it('should interpolate price correctly based on time elapsed when price is decreasing', async function () {
+    const { stakingPool } = this;
+
+    const lastPrice = ethers.utils.parseEther('10');
+    const targetPrice = ethers.utils.parseEther('5');
+    const lastPriceUpdate = '0';
+    const now = (24 * 3600).toString();
+
+    const price = await stakingPool.interpolatePrice(
+      lastPrice,
+      targetPrice,
+      lastPriceUpdate,
+      now,
+    );
+
+    const expectedPrice = lastPrice.sub(lastPrice.sub(targetPrice).div(100));
+
+    assert.equal(price.toString(), expectedPrice.toString());
+  });
+
+  it('should set price to target price when price is increasing', async function () {
+    const { stakingPool } = this;
+
+    const lastPrice = ethers.utils.parseEther('5');
+    const targetPrice = ethers.utils.parseEther('10');
+    const lastPriceUpdate = '0';
+    const now = (24 * 3600).toString();
+
+    const price = await stakingPool.interpolatePrice(
+      lastPrice,
+      targetPrice,
+      lastPriceUpdate,
+      now,
+    );
+
+    assert.equal(price.toString(), targetPrice.toString());
+  });
+
+});

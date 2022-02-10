a1c1091d6e5b0d2d27b402b61e0972f177720740
shark0der <commits@shark0der.com>
2022-02-10 12:13:22 +0200

    Update Deployer to revert if deploy fails and check the expected address

--- contracts/external/Deployer.sol
+++ contracts/external/Deployer.sol
@@ -1,13 +1,20 @@
 // SPDX-License-Identifier: GPL-3.0-only
 
-pragma solidity ^0.5.0;
+pragma solidity ^0.8.0;
 
 contract Deployer {
 
-  function deploy(bytes memory data, uint256 salt) public {
-    assembly {
-      pop(create2(0, add(data, 32), mload(data), salt))
-    }
+  function deploy(bytes memory code, uint256 salt) external {
+    address deploymentAddress;
+    assembly { deploymentAddress := create2(0, add(code, 0x20), mload(code), salt) }
+    require(deploymentAddress != address(0), "Contract creation failed");
+  }
+
+  function deployAt(bytes memory code, uint256 salt, address expectedAddress) external {
+    address deploymentAddress;
+    assembly { deploymentAddress := create2(0, add(code, 0x20), mload(code), salt) }
+    require(deploymentAddress != address(0), "Contract creation failed");
+    require(deploymentAddress == expectedAddress, "Contract deployed at an unexpected address");
   }
 
 }

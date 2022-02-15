67b18d7064bf5287a922c19fa635afc6443532c6
Armando Andini <armando.andini@gmail.com>
2022-02-15 09:50:47 -0300

    Include GP helper contracts, add getUID and getDigest functions

--- contracts/modules/capital/CowSwapOperator.sol
+++ contracts/modules/capital/CowSwapOperator.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0-only
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts-v4/token/ERC20/IERC20.sol";
+import "../../external/cow/GPv2Order.sol";
 
 interface ICowSettlement {
     function setPreSignature(bytes calldata orderUid, bool signed) external;
@@ -22,5 +22,17 @@ contract CowSwapOperator {
 
     function approveVaultRelayer(address token) public {
         IERC20(token).approve(cowVaultRelayer, 2 ** 256 - 1); // infinite approval
+    }
+
+    function getDigest(GPv2Order.Data calldata order, bytes32 domainSeparator) public pure returns (bytes32) {
+        bytes32 hash = GPv2Order.hash(order, domainSeparator);
+        return hash;
+    }
+
+    function getUID(GPv2Order.Data calldata order, bytes32 domainSeparator, address owner, uint32 validTo) public pure returns (bytes memory){
+        bytes memory uid = new bytes(56);
+        bytes32 digest = getDigest(order, domainSeparator);
+        GPv2Order.packOrderUidParams(uid, digest, owner, validTo);
+        return uid;
     }
 }

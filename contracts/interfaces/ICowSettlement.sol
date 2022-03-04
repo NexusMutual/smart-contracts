9cfc662f26ab80f2a1dd5bcda876c53c7c967bc6
Armando Andini <armando.andini@gmail.com>
2022-03-04 13:24:54 -0300

    Get domainSeparator from settlement contract

--- contracts/interfaces/ICowSettlement.sol
+++ contracts/interfaces/ICowSettlement.sol
@@ -7,4 +7,6 @@ interface ICowSettlement {
   function filledAmount(bytes calldata orderUid) external view returns (uint256);
 
   function vaultRelayer() external view returns (address);
+
+  function domainSeparator() external view returns (bytes32);
 }

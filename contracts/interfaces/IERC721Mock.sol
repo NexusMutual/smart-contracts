731434c9bf3eabb6716e9fdab8b32c27a3a61290
Armando Andini <armando.andini@gmail.com>
2022-03-17 13:39:59 -0300

    Interfaces version and double quotes

--- contracts/interfaces/IERC721Mock.sol
+++ contracts/interfaces/IERC721Mock.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: GPL-3.0-only
 
-pragma solidity ^0.8.9;
+pragma solidity ^0.8.0;
 
-import '@openzeppelin/contracts-v4/token/ERC721/IERC721.sol';
+import "@openzeppelin/contracts-v4/token/ERC721/IERC721.sol";
 
 interface IERC721Mock is IERC721 {
   function safeMint(address to, uint256 tokenId) external;
--- contracts/mocks/Tokens/ERC20Mock.sol
+++ contracts/mocks/Tokens/ERC20Mock.sol
@@ -2,11 +2,11 @@
 
 pragma solidity ^0.5.0;
 
-import '@openzeppelin/contracts/token/ERC20/ERC20Detailed.sol';
-import '@openzeppelin/contracts/token/ERC20/ERC20Mintable.sol';
+import "@openzeppelin/contracts/token/ERC20/ERC20Detailed.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20Mintable.sol";
 
 contract ERC20Mock is ERC20Mintable, ERC20Detailed {
-  constructor() public ERC20Detailed('ERC20 mock', 'MOCK', 18) {
+  constructor() public ERC20Detailed("ERC20 mock", "MOCK", 18) {
     /* noop */
   }
 

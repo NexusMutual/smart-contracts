fc684c8270a2ac3dd53daf161f452ffdf3edc54f
Armando Andini <armando.andini@gmail.com>
2022-04-05 13:07:52 -0300

    Remove old SwapOperator tests

--- test/unit/PriceFeedOracle/index.js
+++ test/unit/PriceFeedOracle/index.js
@@ -1,7 +1,7 @@
 const { takeSnapshot, revertToSnapshot } = require('../utils').evm;
 const setup = require('./setup');
 
-describe.only('PriceFeedOracle unit tests', function () {
+describe('PriceFeedOracle unit tests', function () {
   before(setup);
 
   beforeEach(async function () {

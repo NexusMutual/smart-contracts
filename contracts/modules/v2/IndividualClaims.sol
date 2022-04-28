64fe4faf2c02144997dbfafa548a4c726811b548
Dragos Horodnic <dragos@horodnic.com>
2022-04-28 19:18:50 +0300

    No need for separate internal function, make it public

--- contracts/modules/v2/IndividualClaims.sol
+++ contracts/modules/v2/IndividualClaims.sol
@@ -75,11 +75,19 @@ contract IndividualClaims is IIndividualClaims, MasterAwareV2 {
     return claims.length;
   }
 
-  function _getAssessmentDepositAndReward(
+  /// Returns the required deposit and total reward for a new claim
+  ///
+  /// @dev This view is meant to be used either by users or user interfaces to determine the
+  /// minimum deposit value of the submitClaim tx.
+  ///
+  /// @param requestedAmount  The amount that is claimed
+  /// @param segmentPeriod    The cover period of the segment in days
+  /// @param payoutAsset      The asset in which the payout would be made
+  function getAssessmentDepositAndReward(
     uint requestedAmount,
     uint segmentPeriod,
     uint payoutAsset
-  ) internal view returns (uint, uint) {
+  ) public view returns (uint, uint) {
     IPool poolContract = pool();
     uint nxmPriceInPayoutAsset = poolContract.getTokenPrice(payoutAsset);
     uint nxmPriceInETH = poolContract.getTokenPrice(0);
@@ -101,22 +109,6 @@ contract IndividualClaims is IIndividualClaims, MasterAwareV2 {
     uint deposit = minDeposit > dynamicDeposit ? minDeposit : dynamicDeposit;
 
     return (deposit, totalReward);
-  }
-
-  /// Returns the required deposit and total reward for a new claim
-  ///
-  /// @dev This view is meant to be used either by users or user interfaces to determine the
-  /// minimum deposit value of the submitClaim tx.
-  ///
-  /// @param requestedAmount  The amount that is claimed
-  /// @param segmentPeriod    The cover period of the segment in days
-  /// @param payoutAsset      The asset in which the payout would be made
-  function getAssessmentDepositAndReward(
-    uint requestedAmount,
-    uint segmentPeriod,
-    uint payoutAsset
-  ) external view returns (uint, uint) {
-    return _getAssessmentDepositAndReward(requestedAmount, segmentPeriod, payoutAsset);
   }
 
   /// Returns a Claim aggregated in a human-friendly format.
@@ -223,7 +215,7 @@ contract IndividualClaims is IIndividualClaims, MasterAwareV2 {
 
   /// Submits a claim for assessment
   ///
-  /// @dev This function requires an ETH assessment fee. See: _getAssessmentDepositAndReward
+  /// @dev This function requires an ETH assessment fee. See: getAssessmentDepositAndReward
   ///
   /// @param coverId          Cover identifier
   /// @param requestedAmount  The amount expected to be received at payout
@@ -292,7 +284,7 @@ contract IndividualClaims is IIndividualClaims, MasterAwareV2 {
       false // payoutRedeemed
     );
 
-    (uint deposit, uint totalReward) = _getAssessmentDepositAndReward(
+    (uint deposit, uint totalReward) = getAssessmentDepositAndReward(
       requestedAmount,
       segment.period,
       coverData.payoutAsset

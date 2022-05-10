b957e99c6aa6861cc7f00cb455612af88948191c
Dragos Horodnic <dragos@horodnic.com>
2022-05-10 15:07:09 +0300

    Import setup from file

--- test/unit/MemberRoles/index.js
+++ test/unit/MemberRoles/index.js
@@ -2,10 +2,7 @@ const { takeSnapshot, revertToSnapshot } = require('../utils').evm;
 const setup = require('./setup');
 
 describe.only('MemberRoles unit tests', function () {
-  before(async function () {
-    console.log(setup);
-    console.log('work');
-  });
+  before(setup);
 
   beforeEach(async function () {
     this.snapshotId = await takeSnapshot();

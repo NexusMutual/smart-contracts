d02694eb59af129777cc1258526a7d4fa1227b75
Dragos Horodnic <dragos@horodnic.com>
2022-05-16 16:42:41 +0300

    Keep only the v2 internal contracts

--- contracts/interfaces/IMasterAwareV2.sol
+++ contracts/interfaces/IMasterAwareV2.sol
@@ -4,8 +4,17 @@ pragma solidity >=0.5.0;
 
 interface IMasterAwareV2 {
 
-  // [todo] Are there any missing contracts here?
-  enum ID {GW, GV, MR, CL, CR, MC, P1, QT, QD, PC, PS, TC, TD, IC, AS, CO}
+  enum ID {
+    TC, // TokenController.sol
+    GV, // Governance.sol
+    P1, // Pool.sol
+    MR, // MemberRoles.sol
+    MC, // MCR.sol
+    CO  // Cover.sol
+    AS, // Assessment.sol
+    IC, // IndividualClaims.sol
+    YT, // YieldTokenIncidents.sol
+  }
 
   function changeMasterAddress(address masterAddress) external;
 

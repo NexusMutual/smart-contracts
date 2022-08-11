1d375b3af635e2f2316cc92c93865993ab46e933
shark0der <commits@shark0der.com>
2022-08-11 19:04:33 +0300

    Rename argument to prevent shadowing

--- contracts/modules/staking/StakingTypesLib.sol
+++ contracts/modules/staking/StakingTypesLib.sol
@@ -21,10 +21,10 @@ library StakingTypesLib {
   }
 
   function newCoverAmount(
-    uint48 activeCoverAmount,
+    uint48 coverAmount,
     uint16 bucketId
   ) internal pure returns (CoverAmount) {
-    return CoverAmount.wrap((uint64(activeCoverAmount) << 16) | bucketId);
+    return CoverAmount.wrap((uint64(coverAmount) << 16) | bucketId);
   }
 
   // CoverAmountGroup

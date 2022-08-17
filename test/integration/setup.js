5fb7f314c6c64d270581d6a9323c64f42c6ee83a
danoctavian <danoctavian91@gmail.com>
2022-08-17 10:04:19 +0300

    add test for DAI

--- test/integration/setup.js
+++ test/integration/setup.js
@@ -9,6 +9,7 @@ const { enrollMember } = require('./utils/enroll');
 const { BN } = web3.utils;
 const { getAccounts, stakingPoolManagers } = require('../utils').accounts;
 
+
 // Convert web3 instances to ethers.js
 const web3ToEthers = (x, signers) => {
   const { contracts, rates } = x;

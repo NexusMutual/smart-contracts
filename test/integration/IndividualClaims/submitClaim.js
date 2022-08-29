a1f116aa0b00951b0bd68e08bcc6b0b2f23ef5f8
danoctavian <danoctavian91@gmail.com>
2022-08-29 03:58:56 +0300

    re-enable integration tests

--- test/integration/IndividualClaims/submitClaim.js
+++ test/integration/IndividualClaims/submitClaim.js
@@ -16,7 +16,7 @@ const setTime = async timestamp => {
 
 const priceDenominator = '10000';
 
-describe.skip('submitClaim', function () {
+describe.only('submitClaim', function () {
 
   it('submits ETH claim and approves claim', async function () {
     const { DEFAULT_PRODUCT_INITIALIZATION } = this;

10f3f072931d779c3fdfab740315fc4894b2a5b3
danoctavian <danoctavian91@gmail.com>
2022-09-08 19:02:55 +0300

    remove only

--- test/unit/CoverViewer/index.js
+++ test/unit/CoverViewer/index.js
@@ -1,7 +1,7 @@
 const { takeSnapshot, revertToSnapshot } = require('../utils').evm;
 const setup = require('./setup');
 
-describe.only('CoverViewer unit tests', function () {
+describe('CoverViewer unit tests', function () {
   before(setup);
 
   beforeEach(async function () {

3c446c8094c4a3f6c0109ffbdbc91be381a4b797
Paterson <86971279+paterson1@users.noreply.github.com>
2022-11-07 20:36:48 -0300

    stronger assertions

--- test/unit/Cover/updateUintParameters.js
+++ test/unit/Cover/updateUintParameters.js
@@ -1,6 +1,10 @@
 const { expect } = require('chai');
 
 describe('updateUintParameters', function () {
+  const COVER_ASSETS_FALLBACK = 1;
+  const GLOBAL_REWARDS_RATIO = 2;
+  const GLOBAL_CAPACITY_RATIO = 3;
+
   it('should revert if caller is not governance', async function () {
     const { cover, accounts } = this;
 
@@ -12,46 +16,42 @@ describe('updateUintParameters', function () {
   it('should allow to update coverAssetsFallback', async function () {
     const { cover, accounts } = this;
 
-    const coverAssetsFallbackBefore = await cover.coverAssetsFallback();
-    await cover.connect(accounts.governanceContracts[0]).updateUintParameters([2], ['1']);
-    const coverAssetsFallbackAfter = await cover.coverAssetsFallback();
+    await cover.connect(accounts.governanceContracts[0]).updateUintParameters([2], [COVER_ASSETS_FALLBACK]);
+    const coverAssetsFallback = await cover.coverAssetsFallback();
 
-    expect(coverAssetsFallbackBefore).to.not.be.eq(coverAssetsFallbackAfter);
+    expect(coverAssetsFallback).to.be.eq(COVER_ASSETS_FALLBACK);
   });
 
   it('should allow to update globalRewardsRatio', async function () {
     const { cover, accounts } = this;
 
-    const globalRewardsRatioBefore = await cover.globalRewardsRatio();
-    await cover.connect(accounts.governanceContracts[0]).updateUintParameters([1], ['1']);
-    const globalRewardsRatioAfter = await cover.globalRewardsRatio();
+    await cover.connect(accounts.governanceContracts[0]).updateUintParameters([1], [GLOBAL_REWARDS_RATIO]);
+    const globalRewardsRatio = await cover.globalRewardsRatio();
 
-    expect(globalRewardsRatioBefore).to.not.be.eq(globalRewardsRatioAfter);
+    expect(globalRewardsRatio).to.be.eq(GLOBAL_REWARDS_RATIO);
   });
 
   it('should allow to update globalCapacityRatio', async function () {
     const { cover, accounts } = this;
 
-    const globalCapacityRatioBefore = await cover.globalCapacityRatio();
-    await cover.connect(accounts.governanceContracts[0]).updateUintParameters([0], ['1']);
-    const globalCapacityRatioAfter = await cover.globalCapacityRatio();
+    await cover.connect(accounts.governanceContracts[0]).updateUintParameters([0], [GLOBAL_CAPACITY_RATIO]);
+    const globalCapacityRatio = await cover.globalCapacityRatio();
 
-    expect(globalCapacityRatioBefore).to.not.be.eq(globalCapacityRatioAfter);
+    expect(globalCapacityRatio).to.be.eq(GLOBAL_CAPACITY_RATIO);
   });
 
   it('should allow to update all parameters', async function () {
     const { cover, accounts } = this;
 
-    const globalCapacityRatioBefore = await cover.globalCapacityRatio();
-    const globalRewardsRatioBefore = await cover.globalRewardsRatio();
-    const coverAssetsFallbackBefore = await cover.coverAssetsFallback();
-    await cover.connect(accounts.governanceContracts[0]).updateUintParameters([0, 1, 2], ['1', '2', '3']);
-    const globalCapacityRatioAfter = await cover.globalCapacityRatio();
-    const globalRewardsRatioAfter = await cover.globalRewardsRatio();
-    const coverAssetsFallbackAfter = await cover.coverAssetsFallback();
+    await cover
+      .connect(accounts.governanceContracts[0])
+      .updateUintParameters([0, 1, 2], [GLOBAL_CAPACITY_RATIO, GLOBAL_REWARDS_RATIO, COVER_ASSETS_FALLBACK]);
+    const globalCapacityRatio = await cover.globalCapacityRatio();
+    const globalRewardsRatio = await cover.globalRewardsRatio();
+    const coverAssetsFallback = await cover.coverAssetsFallback();
 
-    expect(globalCapacityRatioBefore).to.not.be.eq(globalCapacityRatioAfter);
-    expect(globalRewardsRatioBefore).to.not.be.eq(globalRewardsRatioAfter);
-    expect(coverAssetsFallbackBefore).to.not.be.eq(coverAssetsFallbackAfter);
+    expect(globalCapacityRatio).to.be.eq(GLOBAL_CAPACITY_RATIO);
+    expect(globalRewardsRatio).to.be.eq(GLOBAL_REWARDS_RATIO);
+    expect(coverAssetsFallback).to.be.eq(COVER_ASSETS_FALLBACK);
   });
 });

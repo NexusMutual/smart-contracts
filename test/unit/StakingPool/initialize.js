2c29d91dc88bb0035fb67f39258795f768919a75
Paterson <86971279+paterson1@users.noreply.github.com>
2022-11-16 13:26:19 -0300

    more initialize tests

--- test/unit/StakingPool/initialize.js
+++ test/unit/StakingPool/initialize.js
@@ -2,7 +2,7 @@ const { expect } = require('chai');
 const { ethers } = require('hardhat');
 const { setEtherBalance } = require('../../utils/evm');
 
-describe('initialize', function () {
+describe.only('initialize', function () {
   const product0 = {
     productId: 0,
     weight: 100,
@@ -205,6 +205,105 @@ describe('initialize', function () {
       stakingPool
         .connect(coverSigner)
         .initialize(manager.address, isPrivatePool, initialPoolFee, maxPoolFee, [...validProducts], poolId),
+    ).to.not.be.reverted;
+  });
+
+  it('correctly initilize pool parameters', async function () {
+    const {
+      stakingPool,
+      cover,
+      accounts: { defaultSender: manager },
+    } = this;
+
+    const { poolId, initialPoolFee, maxPoolFee, isPrivatePool } = initializeParams;
+
+    const coverSigner = await ethers.getImpersonatedSigner(cover.address);
+    await setEtherBalance(coverSigner.address, ethers.utils.parseEther('1'));
+
+    await stakingPool
+      .connect(coverSigner)
+      .initialize(manager.address, isPrivatePool, initialPoolFee, maxPoolFee, [], poolId);
+
+    expect(await stakingPool.poolFee()).to.be.eq(initialPoolFee);
+    expect(await stakingPool.maxPoolFee()).to.be.eq(maxPoolFee);
+    expect(await stakingPool.isPrivatePool()).to.be.eq(isPrivatePool);
+  });
+
+  it('mints the ownership nft to manager', async function () {
+    const {
+      stakingPool,
+      cover,
+      accounts: { defaultSender: manager },
+    } = this;
+
+    const { poolId, initialPoolFee, maxPoolFee, isPrivatePool } = initializeParams;
+
+    const coverSigner = await ethers.getImpersonatedSigner(cover.address);
+    await setEtherBalance(coverSigner.address, ethers.utils.parseEther('1'));
+
+    await stakingPool
+      .connect(coverSigner)
+      .initialize(manager.address, isPrivatePool, initialPoolFee, maxPoolFee, [], poolId);
+
+    const ownerNft = await stakingPool.ownerOf(0);
+    expect(ownerNft).to.be.eq(manager.address);
+  });
+
+  it('correctly initializes the list of products', async function () {
+    const {
+      stakingPool,
+      cover,
+      accounts: { defaultSender: manager },
+    } = this;
+
+    const { poolId, initialPoolFee, maxPoolFee, isPrivatePool } = initializeParams;
+
+    const coverSigner = await ethers.getImpersonatedSigner(cover.address);
+    await setEtherBalance(coverSigner.address, ethers.utils.parseEther('1'));
+
+    const validProducts = Array(3)
+      .fill(product0)
+      .map((value, index) => {
+        return {
+          ...value,
+          productId: index,
+          weight: (index + 1) * 10,
+          initialPrice: ((index + 1) * 500).toString(),
+          targetPrice: ((index + 1) * 500).toString(),
+        };
+      });
+
+    await stakingPool
+      .connect(coverSigner)
+      .initialize(manager.address, isPrivatePool, initialPoolFee, maxPoolFee, [...validProducts], poolId);
+
+    for (const [index, product] of validProducts.entries()) {
+      const [lastEffectiveWeight, targetWeight, targetPrice, nextPrice, nextPriceUpdateTime] =
+        await stakingPool.products(index);
+      expect(targetWeight).to.be.eq(product.weight);
+      expect(targetPrice.toString()).to.be.eq(product.targetPrice);
+      expect(nextPrice.toString()).to.be.eq(product.initialPrice);
+      expect(nextPriceUpdateTime).to.not.be.eq(0);
+      expect(lastEffectiveWeight).to.be.eq(0);
+    }
+  });
+
+  it('works if the list of products params is empty', async function () {
+    const {
+      stakingPool,
+      cover,
+      accounts: { defaultSender: manager },
+    } = this;
+
+    const { poolId, initialPoolFee, maxPoolFee, isPrivatePool } = initializeParams;
+
+    const coverSigner = await ethers.getImpersonatedSigner(cover.address);
+    await setEtherBalance(coverSigner.address, ethers.utils.parseEther('1'));
+
+    await expect(
+      stakingPool
+        .connect(coverSigner)
+        .initialize(manager.address, isPrivatePool, initialPoolFee, maxPoolFee, [], poolId),
     ).to.not.be.reverted;
   });
 });

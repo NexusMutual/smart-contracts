26b33d95cb340f3527b1f625032598449fbd688f
shark0der <commits@shark0der.com>
2022-11-29 10:46:37 +0200

    Avoid passing the entire block object and only pass the timestamp

--- test/unit/StakingPool/helpers.js
+++ test/unit/StakingPool/helpers.js
@@ -116,13 +116,13 @@ function toDecimal(x) {
   return new Decimal(x.toString());
 }
 
-function calculateFirstTrancheId(lastBlock, period, gracePeriod) {
-  return Math.floor((lastBlock.timestamp + period + gracePeriod) / (91 * 24 * 3600));
+function calculateFirstTrancheId(timestamp, period, gracePeriod) {
+  return Math.floor((timestamp + period + gracePeriod) / (91 * 24 * 3600));
 }
 
 async function getTranches(period, gracePeriod) {
   const lastBlock = await ethers.provider.getBlock('latest');
-  const firstActiveTrancheId = calculateFirstTrancheId(lastBlock, period, gracePeriod);
+  const firstActiveTrancheId = calculateFirstTrancheId(lastBlock.timestamp, period, gracePeriod);
   const maxTranche = firstActiveTrancheId + MAX_ACTIVE_TRANCHES - 1;
 
   return {

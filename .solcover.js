ff1535dffa50344d92177ea0b59c1145b3e0bd20
shark0der <commits@shark0der.com>
2023-01-09 20:34:24 +0200

    Fix coverage job failing due to instrumentation step which changes the bytecode

--- .solcover.js
+++ .solcover.js
@@ -1,4 +1,54 @@
+const fs = require('fs');
+const path = require('path');
+
+const { artifacts, config } = require('hardhat');
+const { keccak256 } = require('ethereum-cryptography/keccak');
+const { bytesToHex, hexToBytes } = require('ethereum-cryptography/utils');
+
+const stakingLibraryPath = 'contracts/libraries/StakingPoolLibrary.sol';
+
+function findFiles(dir) {
+  const results = [];
+
+  for (const filename of fs.readdirSync(dir)) {
+    const filepath = path.join(dir, filename);
+    const stat = fs.statSync(filepath);
+    const fileOrContents = stat && stat.isDirectory() ? findFiles(filepath) : [filepath];
+    results.push(...fileOrContents);
+  }
+
+  return results;
+}
+
+async function onCompileComplete() {
+  const { bytecode } = await artifacts.readArtifact('MinimalBeaconProxy');
+  const requiredHash = bytesToHex(keccak256(hexToBytes(bytecode.replace(/^0x/i, ''))));
+
+  const stakingLibrary = fs.readFileSync(stakingLibraryPath, 'utf8').toString();
+  const hardcodedHash = stakingLibrary.match(/hex'([0-9a-f]+)' \/\/ init code hash/i)[1];
+
+  console.log('Required hash:', requiredHash);
+  console.log('Hardcoded hash:', hardcodedHash);
+
+  if (hardcodedHash === requiredHash) {
+    console.log('Artifact patching not required');
+    return;
+  }
+
+  const files = findFiles(config.paths.artifacts);
+  const foundArtifacts = files.filter(file => file.match(/\.json/));
+
+  for (const file of foundArtifacts) {
+    const contents = fs.readFileSync(file, 'utf8').toString();
+    if (contents.includes(hardcodedHash)) {
+      console.log(`Patching ${file}`);
+      fs.writeFileSync(file, contents.replace(hardcodedHash, requiredHash), 'utf8');
+    }
+  }
+}
+
 module.exports = {
+  onCompileComplete,
   skipFiles: [
     'abstract/',
     'external/',

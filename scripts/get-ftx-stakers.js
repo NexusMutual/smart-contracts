8ada7cb4bcc4c898b8e5301c23878eeb21a3a1c8
danoctavian <danoctavian91@gmail.com>
2023-01-23 18:01:23 +0200

    add ftx stakers fetching

---
+++ scripts/get-ftx-stakers.js
@@ -0,0 +1,71 @@
+const { ethers } = require('hardhat');
+const fs = require('fs');
+const path = require('path');
+const fetch = require('node-fetch');
+const Decimal = require('decimal.js');
+
+const { PROVIDER_URL } = process.env;
+const VERSION_DATA_URL = 'https://api.nexusmutual.io/version-data/data.json';
+
+const getContractFactory = async providerOrSigner => {
+  const data = await fetch(VERSION_DATA_URL).then(r => r.json());
+  const abis = data.mainnet.abis
+    .map(item => ({ ...item, abi: JSON.parse(item.contractAbi) }))
+    .reduce((data, item) => ({ ...data, [item.code]: item }), {});
+
+  return async code => {
+    const { abi, address } = abis[code];
+    return new ethers.Contract(address, abi, providerOrSigner);
+  };
+};
+
+const ROLE_MEMBER = 2;
+
+async function getWithdrawableCoverNotes(i, qt, mr) {
+  const { 0: member, 1: active } = await mr.memberAtIndex(ROLE_MEMBER, i);
+
+  if (!active) {
+    return { member, withdrawableAmount: '0' };
+  }
+
+  const withdrawableAmount = await qt.getWithdrawableCoverNotesAmount(member);
+  return {
+    withdrawableAmount: withdrawableAmount.toString(),
+    member,
+  };
+}
+
+async function main(provider) {
+  const factory = await getContractFactory(provider);
+  const ps = await factory('PS');
+
+  console.log('Fetching ftx stakes...');
+
+  const ftx = '0xC57d000000000000000000000000000000000011';
+  const stakers = await ps.contractStakersArray(ftx);
+  console.log({
+    stakers,
+  });
+
+  const allStakes = {};
+
+  for (const staker of stakers) {
+    const ftxStake = await ps.stakerContractStake(staker, ftx);
+
+    allStakes[staker] = Decimal(ftxStake.toString()).div(1e18).toString();
+  }
+
+  console.log(allStakes);
+}
+
+if (require.main === module) {
+  const provider = new ethers.providers.JsonRpcProvider(PROVIDER_URL);
+  main(provider)
+    .then(() => process.exit(0))
+    .catch(e => {
+      console.log('Unhandled error encountered: ', e.stack);
+      process.exit(1);
+    });
+}
+
+module.exports = main;

e1b6b830faf12af572e3b027399e540644c138f6
shark0der <commits@shark0der.com>
2023-01-23 15:02:39 +0200

    Fix deposit script

--- scripts/mocks/depositToStakingPools.js
+++ scripts/mocks/depositToStakingPools.js
@@ -1,68 +1,64 @@
 const { config, network, ethers } = require('hardhat');
 const { AddressZero, MaxUint256 } = ethers.constants;
-const { parseEther } = ethers.utils;
+
+const { getCreate2Address, parseEther } = ethers.utils;
+
+const INIT_CODE_HASH = '203b477dc328f1ceb7187b20e5b1b0f0bc871114ada7e9020c9ac112bbfb6920';
+const STAKING_POOL_FACTORY = '0x68B1D87F95878fE05B998F19b66F4baba5De1aed';
+const TOKEN = '0x0B306BF915C4d645ff596e518fAf3F9669b97016';
+const TOKEN_CONTROLLER = '0x0B306BF915C4d645ff596e518fAf3F9669b97016';
+
+/**
+ * @param {Number} poolId
+ * @param {Signer} signer
+ * @returns {Promise<StakingPool>}
+ */
+const getStakingPool = async (poolId, signer) => {
+  const salt = Buffer.from(poolId.toString(16).padStart(64, '0'), 'hex');
+  const initCodeHash = Buffer.from(INIT_CODE_HASH, 'hex');
+  const stakingPoolAddress = getCreate2Address(STAKING_POOL_FACTORY, salt, initCodeHash);
+  return await ethers.getContractAt('StakingPool', stakingPoolAddress, signer);
+};
 
 async function main() {
   console.log(`Using network: ${network.name}`);
   console.log('Network config:', config.networks[network.name]);
 
   const [owner] = await ethers.getSigners();
-
-  const stakingPoolFactory = await ethers.getContractAt(
-    'StakingPoolFactory',
-    '0x68B1D87F95878fE05B998F19b66F4baba5De1aed',
-  );
-
   console.log('OWNER ADDRESS', owner.address);
+
+  const staker = await ethers.getSigner('0x15d34AAf54267DB7D7c367839AAf71A00a2C6A65');
 
   // Staking inputs
   const stakingAmount = parseEther('100');
-  const lastBlock = await ethers.provider.getBlock('latest');
-  const period = 3600 * 24 * 30; // 30 days
-  const gracePeriod = 3600 * 24 * 30;
-  const firstTrancheId = Math.floor((lastBlock.timestamp + period + gracePeriod) / (91 * 24 * 3600));
+  const { timestamp: now } = await ethers.provider.getBlock('latest');
+  const firstActiveTrancheId = Math.floor(now / (91 * 24 * 3600));
+  const lastActiveTrancheId = firstActiveTrancheId + 7;
+
+  // Set allowance to token controller
+  const token = await ethers.getContractAt('NXMToken', TOKEN);
+  await token.connect(owner).approve(TOKEN_CONTROLLER, stakingAmount);
+  await token.connect(staker).approve(TOKEN_CONTROLLER, stakingAmount);
 
   console.log('Deposit to staking pool id: 0');
-  let poolId = 0;
-  let salt = Buffer.from(poolId.toString(16).padStart(64, '0'), 'hex');
-  let requiredHash = '203b477dc328f1ceb7187b20e5b1b0f0bc871114ada7e9020c9ac112bbfb6920';
-  let initCodeHash = Buffer.from(requiredHash, 'hex');
-  let stakingPoolAddress = ethers.utils.getCreate2Address(stakingPoolFactory.address, salt, initCodeHash);
-  let stakingPoolInstance = await ethers.getContractAt('StakingPool', stakingPoolAddress);
-  console.log('stakingPoolInstance', stakingPoolInstance.address);
-  console.log('firstTrancheId', firstTrancheId);
-  let staker = owner;
-  console.log('staker', staker.address);
-  // Stake to open up capacity
-  await stakingPoolInstance.connect(staker).depositTo(
+  const stakingPoolZero = await getStakingPool(0, owner);
+  await stakingPoolZero.depositTo(
     stakingAmount,
-    firstTrancheId,
+    lastActiveTrancheId,
     MaxUint256, // new position
     AddressZero, // destination
   );
-  console.log('Deposited successfully!');
 
   console.log('Deposit to staking pool id: 1');
-  poolId = 1;
-  salt = Buffer.from(poolId.toString(16).padStart(64, '0'), 'hex');
-  requiredHash = '203b477dc328f1ceb7187b20e5b1b0f0bc871114ada7e9020c9ac112bbfb6920';
-  initCodeHash = Buffer.from(requiredHash, 'hex');
-  stakingPoolAddress = ethers.utils.getCreate2Address(stakingPoolFactory.address, salt, initCodeHash);
-  stakingPoolInstance = await ethers.getContractAt('StakingPool', stakingPoolAddress);
-  console.log('stakingPoolInstance', stakingPoolInstance.address);
-  console.log('firstTrancheId', firstTrancheId);
-  staker = await ethers.getSigner('0x15d34AAf54267DB7D7c367839AAf71A00a2C6A65');
-  console.log('staker', staker.address);
-  // Stake to open up capacity
-  await stakingPoolInstance.connect(staker).depositTo(
+  const stakingPoolOne = await getStakingPool(1, owner);
+  await stakingPoolOne.depositTo(
     stakingAmount,
-    firstTrancheId,
+    lastActiveTrancheId,
     MaxUint256, // new position
     AddressZero, // destination
   );
-  console.log('Deposited successfully!');
 
-  console.log('Done!');
+  console.log('Deposited successfully!');
   process.exit(0);
 }
 

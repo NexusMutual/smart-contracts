fdd00d24e1c3d563a5ed6102cea601b19f29e05b
shark0der <commits@shark0der.com>
2023-01-23 19:00:40 +0200

    Add scripts to fund addresses

---
+++ scripts/mocks/fund.js
@@ -0,0 +1,50 @@
+const { network, ethers } = require('hardhat');
+
+const { BigNumber } = ethers;
+const { hexValue, isAddress, formatEther, parseEther } = ethers.utils;
+
+// this hex function produces evm-compatible hex strings:
+// - strips leading zeroes (0x01 -> 0x1)
+// - keeps one zero if the value is zero (0x00 -> 0x0)
+const hex = n => hexValue(BigNumber.from(n));
+
+const send = ethers.provider.send.bind(ethers.provider);
+
+const tests = [
+  { node: 'hardhat', regex: /HardhatNetwork/ },
+  { node: 'tenderly', regex: /Tenderly/ },
+];
+
+async function main() {
+  console.log(`Using network: ${network.name}`);
+
+  const charities = process.argv.slice(2);
+  const invalidAddresses = charities.filter(charity => !isAddress(charity));
+
+  if (invalidAddresses.length > 0) {
+    console.log(`Invalid addresses: ${invalidAddresses.join(', ')}`);
+    process.exit(2);
+  }
+
+  const clientVersion = await send('web3_clientVersion', []);
+  const test = tests.find(test => test.regex.test(clientVersion));
+
+  if (test === undefined) {
+    console.log('Error: Can only fund accounts on hardhat or tenderly');
+    process.exit(3);
+  }
+
+  const { node } = test;
+  const setBalance = async (account, value) => send(`${node}_setBalance`, [account, hex(value)]);
+  const amount = parseEther('100');
+
+  for (const charity of charities) {
+    console.log(`Funding ${charity} with ${formatEther(amount)} ETH`);
+    await setBalance(charity, amount);
+  }
+}
+
+main().catch(error => {
+  console.error('An unexpected error encountered:', error);
+  process.exit(1);
+});

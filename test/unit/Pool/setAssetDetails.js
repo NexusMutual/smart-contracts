92fe1aaf17a82dc31fa1d9d0219500ba9107d7ae
shark0der <commits@shark0der.com>
2023-01-26 12:21:23 +0200

    Fix pool.setAssetDetails unit tests

--- test/unit/Pool/setAssetDetails.js
+++ test/unit/Pool/setAssetDetails.js
@@ -18,15 +18,28 @@ describe('setAssetDetails', function () {
     ).to.be.revertedWith('Pool: Asset does not exist');
   });
 
-  // TODO: revise test below
   it('marks asset as cover asset, or abandoned', async function () {
-    const { pool } = this;
     const [governance] = this.accounts.governanceContracts;
+    const pool = this.pool.connect(governance);
 
     const assets = await pool.getAssets();
     const lastAssetId = assets.length - 1;
 
-    await pool.connect(governance).setAssetDetails(lastAssetId, false, false);
+    await pool.setAssetDetails(lastAssetId, false, false);
+    expect((await pool.getAsset(lastAssetId)).isCoverAsset).to.be.equal(false);
+    expect((await pool.getAsset(lastAssetId)).isAbandoned).to.be.equal(false);
+
+    await pool.setAssetDetails(lastAssetId, true, false);
+    expect((await pool.getAsset(lastAssetId)).isCoverAsset).to.be.equal(true);
+    expect((await pool.getAsset(lastAssetId)).isAbandoned).to.be.equal(false);
+
+    await pool.setAssetDetails(lastAssetId, false, true);
+    expect((await pool.getAsset(lastAssetId)).isCoverAsset).to.be.equal(false);
+    expect((await pool.getAsset(lastAssetId)).isAbandoned).to.be.equal(true);
+
+    await pool.setAssetDetails(lastAssetId, true, true);
+    expect((await pool.getAsset(lastAssetId)).isCoverAsset).to.be.equal(true);
+    expect((await pool.getAsset(lastAssetId)).isAbandoned).to.be.equal(true);
 
     await expect(
       pool.connect(governance).setAssetDetails(lastAssetId + 1, false, false), // assetId out of bounds

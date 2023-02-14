81837b0bec95698c54c4fa8b8b08c998325d3602
danoctavian <danoctavian91@gmail.com>
2023-02-14 07:41:45 +0200

    add StakingProducts to migration test

--- contracts/utils/InternalProxyInitializer.sol
+++ contracts/utils/InternalProxyInitializer.sol
@@ -4,10 +4,10 @@ pragma solidity ^0.8.0;
 
 import "../abstract/MasterAwareV2.sol";
 
-// This contract is required to initialize master (see MasterAwareV2). Also see
+// This contract is required to initialize p (see MasterAwareV2). Also see
 // addNewInternalContracts from NXMaster.sol which calls changeMasterAddress.
 // Without it the transaction would fail.
-contract CoverInitializer is MasterAwareV2 {
+contract InternalProxyInitializer is MasterAwareV2 {
 
   function changeDependentContractAddress() external override {
     //noop

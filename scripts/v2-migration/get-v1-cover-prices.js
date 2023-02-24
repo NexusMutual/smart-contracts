4d87e4b2bd531fee2a6472f46d73d10694b629c1
shark0der <commits@shark0der.com>
2023-02-24 18:44:03 +0200

    Change prices format and extracts the prices to a separate inherited contract

--- scripts/v2-migration/get-v1-cover-prices.js
+++ scripts/v2-migration/get-v1-cover-prices.js
@@ -3,48 +3,58 @@ const fs = require('fs');
 const fetch = require('node-fetch');
 const path = require('path');
 
-// These products are not allowed to be covered in DAI
-const DAI_COVER_BLACKLIST = [
-  // Maker DAO
-  '0x35d1b3f3d7966a1dfe207aa4514c12a259a0492b',
-  // Curve sETH LP (eCrv)
-  '0x0000000000000000000000000000000000000010',
-  // Convex stethCrv (cvxstethCrv)
-  '0x0000000000000000000000000000000000000013',
-];
-
 const PRODUCT_ADDRESSES = require(path.join(__dirname, 'output/product-addresses.json'));
-const PS_CONTRACT_PATH = path.join(__dirname, '../../contracts/modules/legacy/LegacyPooledStaking.sol');
+const PRICES_CONTRACT_PATH = path.join(__dirname, '../../contracts/modules/legacy/PricesV1.sol');
 
-const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
+const generatePricesCode = (priceMap, productIds) => {
+  const prices = Object.keys(priceMap);
+  const indent = '    ';
 
-const getPrices = (priceMap, products) => `// {V1_PRICES_HELPER_BEGIN}
-${Object.keys(priceMap)
-  .map(price => {
-    if (priceMap[price].length > 1) {
-      return `
-    if (
-${priceMap[price]
-  .map((address, index) => {
-    const lineEnd = index < priceMap[price].length - 1 ? ' ||\n' : '';
-    const productId = products.findIndex(x => x === address);
-    return `      // ${address} 
-      id == ${productId}${lineEnd}`;
-  })
-  .join('')}
-    ) {
-      return ${price}; // ${price / 1e18} %
-    }`;
+  const priceToBps = price => Math.ceil(price / 1e14);
+  const priceToPercent = price => (price / 1e16).toFixed(6);
+
+  const codeBlocks = prices.map(price => {
+    // single item
+    if (priceMap[price].length === 1) {
+      const [productAddress] = priceMap[price];
+      const productId = productIds.indexOf(productAddress);
+      const lines = [
+        `if (id == ${productId}) { // ${productAddress}`,
+        `  return ${priceToBps(price)}; // ${priceToPercent(price)} %`,
+        `}`,
+      ];
+      return lines.map(line => indent + line).join('\n');
     }
-    const productId = products.findIndex(x => x === priceMap[price][0]);
-    return `
-    // ${products[productId]} 
-    if (id == ${productId}) {
-      return ${price}; // ${price / 1e18} %
-    }`;
-  })
-  .join('\n')}
-    // {V1_PRICES_HELPER_END}`;
+
+    // multi
+    const productAddresses = priceMap[price];
+    const lastIdx = productAddresses.length - 1;
+
+    const items = productAddresses.map((productAddress, idx) => {
+      const productId = productIds.indexOf(productAddress);
+      const operator = idx === lastIdx ? '   ' : ' ||';
+      return `  id == ${productId}${operator} // ${productAddress}`;
+    });
+
+    const lines = [
+      'if (',
+      ...items, // checks for each product
+      ') {',
+      `  return ${priceToBps(price)}; // ${priceToPercent(price)} %`,
+      '}',
+    ];
+
+    return lines.map(line => indent + line).join('\n');
+  });
+
+  return (
+    [
+      indent + '// V1_PRICES_HELPER_BEGIN',
+      ...codeBlocks, // price blocks
+      indent + '// V1_PRICES_HELPER_END',
+    ].join('\n\n') + '\n'
+  );
+};
 
 const main = async (useCache = true) => {
   // check the cache first
@@ -53,64 +63,55 @@ const main = async (useCache = true) => {
     return;
   }
 
-  const priceToProductMap = {};
-  const productToPriceMap = {};
-
   console.log(`Processing ${PRODUCT_ADDRESSES.length} products`);
-  for (const productAddress of PRODUCT_ADDRESSES) {
-    // Fetch capacity
-    const capacityURL = `https://api.nexusmutual.io/v1/contracts/${productAddress}/capacity`;
-    console.log(`Calling ${capacityURL}`);
-    const res = await fetch(capacityURL, {
-      headers: {
-        'x-api-key': 'c904-42c7-2f90-a561',
-        Origin: 'https://app.nexusmutual.io',
-      },
-    });
-    await sleep(100);
-    const productState = await res.json();
-    console.log(productState);
+  const priceToProductMap = {};
 
-    // Skip if product is deprecated
-    if (productState.reason === 'Uncoverable') {
-      console.log(`Product ${productAddress} is Uncoverable. Skipping.`);
-      continue;
-    }
+  // These products are not allowed to be covered in DAI
+  const DAI_COVER_BLACKLIST = [
+    '0x35d1b3f3d7966a1dfe207aa4514c12a259a0492b', // Maker DAO
+    '0x0000000000000000000000000000000000000010', // Curve sETH LP (eCrv)
+    '0x0000000000000000000000000000000000000013', // Convex stethCrv (cvxstethCrv)
+  ];
+
+  for (const productAddress of PRODUCT_ADDRESSES) {
+    console.log(`Fetching price for ${productAddress}`);
 
     // Fetch annual price from the quote API
     const currency = DAI_COVER_BLACKLIST.includes(productAddress.toLowerCase()) ? 'ETH' : 'DAI';
+
     // eslint-disable-next-line max-len
     const quoteURL = `https://api.nexusmutual.io/v1/quote?coverAmount=1&currency=${currency}&period=365&contractAddress=${productAddress}`;
-    const quoteRes = await fetch(quoteURL, {
-      headers: {
-        'x-api-key': 'c904-42c7-2f90-a561',
-        Origin: 'https://app.nexusmutual.io',
-      },
-    });
-    await sleep(100);
-    const quote = await quoteRes.json();
-    const annualPrice = quote.price * 100; // highest is 100.
+    const quoteRes = await fetch(quoteURL, { headers: { Origin: 'https://app.nexusmutual.io' } });
+    const { price = false } = await quoteRes.json();
+
+    if (price === false) {
+      // price missing, quote engine didn't return a price
+      continue;
+    }
 
     // TODO there are currently 3 products that have price 0.
     // This is because there isn't enough stake on them.
     // Solution: create a new quote api endpoint that provides
     // **unsigned** quotes and ignores capacity limits
 
-    productToPriceMap[productAddress] = annualPrice;
-    if (!priceToProductMap[annualPrice]) {
-      priceToProductMap[annualPrice] = [];
+    if (!priceToProductMap[price]) {
+      priceToProductMap[price] = [];
     }
-    priceToProductMap[annualPrice].push(productAddress);
+
+    priceToProductMap[price].push(productAddress);
   }
 
-  const snippet = getPrices(priceToProductMap, PRODUCT_ADDRESSES);
-  console.log({ snippet });
-  const templateHelperRegex = /\/\/ \{V1_PRICES_HELPER_BEGIN\}([\s\S]*?)\/\/ \{V1_PRICES_HELPER_END\}/;
-  const psContractFile = fs.readFileSync(PS_CONTRACT_PATH, 'utf8');
-  const newPSContract = psContractFile.toString().replace(templateHelperRegex, snippet);
-  fs.writeFileSync(PS_CONTRACT_PATH, newPSContract);
+  const snippet = generatePricesCode(priceToProductMap, PRODUCT_ADDRESSES);
+  console.log('Generated solidity:\n', snippet);
 
-  console.log({ priceToProductMap, productToPriceMap });
+  const templateHelperRegex = / {4}\/\/ V1_PRICES_HELPER_BEGIN.+V1_PRICES_HELPER_END\n/s;
+  const pricesContractFile = fs.readFileSync(PRICES_CONTRACT_PATH, 'utf8').toString();
+  const newPricesContract = pricesContractFile.replace(templateHelperRegex, snippet);
+  fs.writeFileSync(PRICES_CONTRACT_PATH, newPricesContract);
+  console.log('contract matches:', !!pricesContractFile.match(templateHelperRegex));
+  console.log('new contract:\n', newPricesContract);
+
+  console.log({ priceToProductMap });
 };
 
 if (require.main === module) {

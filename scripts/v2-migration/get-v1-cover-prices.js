14f54bdbb8fb04b417dd20392d8668b34d61178d
shark0der <commits@shark0der.com>
2023-02-27 13:59:54 +0200

    Use local quote engine for price contract generation

--- scripts/v2-migration/get-v1-cover-prices.js
+++ scripts/v2-migration/get-v1-cover-prices.js
@@ -1,7 +1,9 @@
-require('dotenv').config();
+const { ethers } = require('hardhat');
+const { assert } = require('chai');
 const fs = require('fs');
-const fetch = require('node-fetch');
 const path = require('path');
+
+const QuoteEngine = require('./v1-quote-engine');
 
 const PRODUCT_ADDRESSES = require(path.join(__dirname, 'output/product-addresses.json'));
 const PRICES_CONTRACT_PATH = path.join(__dirname, '../../contracts/modules/legacy/PricesV1.sol');
@@ -10,8 +12,8 @@ const generatePricesCode = (priceMap, productIds) => {
   const prices = Object.keys(priceMap);
   const indent = '    ';
 
-  const priceToBps = price => Math.ceil(price / 1e14);
-  const priceToPercent = price => (price / 1e16).toFixed(6);
+  const priceToBps = price => Math.ceil(Number(price) / 1e14);
+  const priceToPercent = price => (Number(price) / 1e16).toFixed(6);
 
   const codeBlocks = prices.map(price => {
     // single item
@@ -56,43 +58,23 @@ const generatePricesCode = (priceMap, productIds) => {
   );
 };
 
-const main = async (useCache = true) => {
+const main = async (provider, useCache = true) => {
   // check the cache first
   if (useCache) {
     console.log('Using cached data for get V1 cover prices');
     return;
   }
 
+  const quoteEngine = await QuoteEngine(provider);
+
   console.log(`Processing ${PRODUCT_ADDRESSES.length} products`);
   const priceToProductMap = {};
 
-  // These products are not allowed to be covered in DAI
-  const DAI_COVER_BLACKLIST = [
-    '0x35d1b3f3d7966a1dfe207aa4514c12a259a0492b', // Maker DAO
-    '0x0000000000000000000000000000000000000010', // Curve sETH LP (eCrv)
-    '0x0000000000000000000000000000000000000013', // Convex stethCrv (cvxstethCrv)
-  ];
-
   for (const productAddress of PRODUCT_ADDRESSES) {
     console.log(`Fetching price for ${productAddress}`);
+    const price = await quoteEngine(productAddress);
 
-    // Fetch annual price from the quote API
-    const currency = DAI_COVER_BLACKLIST.includes(productAddress.toLowerCase()) ? 'ETH' : 'DAI';
-
-    // eslint-disable-next-line max-len
-    const quoteURL = `https://api.nexusmutual.io/v1/quote?coverAmount=1&currency=${currency}&period=365&contractAddress=${productAddress}`;
-    const quoteRes = await fetch(quoteURL, { headers: { Origin: 'https://app.nexusmutual.io' } });
-    const { price = false } = await quoteRes.json();
-
-    if (price === false) {
-      // price missing, quote engine didn't return a price
-      continue;
-    }
-
-    // TODO there are currently 3 products that have price 0.
-    // This is because there isn't enough stake on them.
-    // Solution: create a new quote api endpoint that provides
-    // **unsigned** quotes and ignores capacity limits
+    assert(price !== '0');
 
     if (!priceToProductMap[price]) {
       priceToProductMap[price] = [];
@@ -108,15 +90,14 @@ const main = async (useCache = true) => {
   const pricesContractFile = fs.readFileSync(PRICES_CONTRACT_PATH, 'utf8').toString();
   const newPricesContract = pricesContractFile.replace(templateHelperRegex, snippet);
   fs.writeFileSync(PRICES_CONTRACT_PATH, newPricesContract);
-  console.log('contract matches:', !!pricesContractFile.match(templateHelperRegex));
-  console.log('new contract:\n', newPricesContract);
+  assert(!!pricesContractFile.match(templateHelperRegex), 'Template regex did not match anything');
 
   console.log({ priceToProductMap });
 };
 
 if (require.main === module) {
   // bypass cache when run via cli
-  main(false)
+  main(ethers.provider, false)
     .then(() => process.exit(0))
     .catch(e => {
       console.log('Unhandled error encountered: ', e.stack);

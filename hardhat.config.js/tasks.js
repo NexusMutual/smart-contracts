3160b71301cf1d0e096e29340d86061ae65bc8a0
shark0der <commits@shark0der.com>
2023-03-03 18:52:43 +0200

    Override compile task to add storage layout flag

--- hardhat.config.js/tasks.js
+++ hardhat.config.js/tasks.js
@@ -1,5 +1,6 @@
 const { task } = require('hardhat/config');
 const { TASK_TYPECHAIN } = require('@typechain/hardhat/dist/constants');
+const { TASK_COMPILE } = require('hardhat/builtin-tasks/task-names');
 
 task('test', async (args, hre, runSuper) => {
   const testFiles = args.testFiles.length ? args.testFiles : ['test/index.js'];
@@ -14,3 +15,31 @@ task(TASK_TYPECHAIN, async (args, hre, runSuper) => {
   hre.config.typechain.dontOverrideCompile = false;
   await runSuper();
 });
+
+task(TASK_COMPILE)
+  .addFlag('generateStorageLayout', 'Generate storage layout')
+  .setAction(async function ({ generateStorageLayout }, hre, runSuper) {
+    if (!generateStorageLayout) {
+      return runSuper();
+    }
+
+    const { compilers, overrides } = hre.config.solidity;
+
+    // add storageLayout to compilers if missing
+    for (const compiler of compilers) {
+      const output = compiler.settings.outputSelection['*']['*'];
+      if (!output.includes('storageLayout')) {
+        output.push('storageLayout');
+      }
+    }
+
+    // add storageLayout to overrides if missing
+    for (const source of Object.keys(overrides)) {
+      const output = overrides[source].settings.outputSelection['*']['*'];
+      if (!output.includes('storageLayout')) {
+        output.push('storageLayout');
+      }
+    }
+
+    await runSuper();
+  });

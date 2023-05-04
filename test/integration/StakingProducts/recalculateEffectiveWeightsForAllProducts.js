d78ff01f99b3ab5a456d4d210101acdbbb030157
shark0der <commits@shark0der.com>
2023-05-04 12:24:49 +0300

    Remove redundant stake deposit in weights test

--- test/integration/StakingProducts/recalculateEffectiveWeightsForAllProducts.js
+++ test/integration/StakingProducts/recalculateEffectiveWeightsForAllProducts.js
@@ -32,31 +32,15 @@ describe('recalculateEffectiveWeightsForAllProducts', function () {
   });
 
   it('recalculates effective weights when there is 0 activeStake and targetWeight = 5', async function () {
-    const { stakingProducts, stakingPool1 } = this.contracts;
-    const staker = this.accounts.defaultSender;
-    const [manager1] = this.accounts.stakingPoolManagers;
+    const { stakingProducts } = this.contracts;
+    const [manager] = this.accounts.stakingPoolManagers;
 
     const poolId = 1;
     const productId = 1;
-
-    const stakeAmount = parseEther('9000000');
-
     const targetWeight = 5;
-    await stakingProducts.connect(manager1).setProducts(productId, [
-      {
-        ...stakedProductParamTemplate,
-        targetWeight,
-      },
-    ]);
 
-    // stake
-    const firstActiveTrancheId = await calculateFirstTrancheId(
-      await ethers.provider.getBlock('latest'),
-      daysToSeconds(30),
-      0,
-    );
-    await stakingPool1.connect(staker).depositTo(stakeAmount, firstActiveTrancheId + 5, 0, staker.address);
-
+    const productParams = { ...stakedProductParamTemplate, targetWeight };
+    await stakingProducts.connect(manager).setProducts(poolId, [productParams]);
     await stakingProducts.recalculateEffectiveWeightsForAllProducts(poolId);
 
     const product = await stakingProducts.getProduct(poolId, productId);

ba9c5e7dd77885c07031f7781b3eb27c33a6dd97
Dan Octavian <dan@MacBook-Pro.local>
2023-05-17 18:09:59 +0300

    fix logging + remove address params from the script

--- scripts/products/generate-setProducts-tx.js
+++ scripts/products/generate-setProducts-tx.js
@@ -15,14 +15,20 @@ const OUTPUT_FILE = path.join(
 
 const YIELD_TOKEN_PRODUCT_TYPE_ID = '2';
 
+const COVER_PROXY_ADDRESS = '0xcafeac0fF5dA0A2777d915531bfA6B29d282Ee62';
+
 /**
  *
  * Generate the tx data for the Cover.setProducts transaction based using the data
  * in the productsDataFile CSV.
  *
- * CLI parameters in this order: productsDataFilePath coverAddress signerAddress
+ * CLI parameters in this order: productsDataFilePath
  *
  * NOTE: Product editing not yet supported. 'Product Id' is ignored.
+ *
+ * The output is written to OUTPUT_FILE.
+ *
+ * Use setProductsTransaction.data as the transaction data in your wallet of choice.
  *
  * @param provider
  * @param productsDataFilePath path for file of products
@@ -30,12 +36,10 @@ const YIELD_TOKEN_PRODUCT_TYPE_ID = '2';
  * @param signerAddress address of the AB signer - not encoded in the transaction blob
  * @returns {Promise<{setProductsTransaction: *}>}
  */
-const main = async (provider, productsDataFilePath, coverAddress) => {
-  console.log(`Using cover address: ${coverAddress}.`);
+const main = async (provider, productsDataFilePath) => {
+  console.log(`Using cover address: ${COVER_PROXY_ADDRESS}.`);
 
-  const { abi } = JSON.parse(fs.readFileSync('./artifacts/contracts/modules/cover/Cover.sol/Cover.json'));
-
-  const cover = new ethers.Contract(coverAddress, abi);
+  const cover = await ethers.getContractAt('Cover', COVER_PROXY_ADDRESS);
 
   const productData = csvParse(fs.readFileSync(productsDataFilePath, 'utf8'), {
     columns: true,
@@ -72,21 +76,19 @@ const main = async (provider, productsDataFilePath, coverAddress) => {
     return productParams;
   });
 
-  console.log(productEntries);
+  console.log('Tx input: ', productEntries);
 
   const setProductsTransaction = await cover.populateTransaction.setProducts(productEntries);
 
-  const txs = {
-    setProductsTransaction,
-  };
+  console.log(`Tx data ${setProductsTransaction.data}`);
 
-  fs.writeFileSync(OUTPUT_FILE, JSON.stringify(txs, null, 2), 'utf8');
+  fs.writeFileSync(OUTPUT_FILE, JSON.stringify(setProductsTransaction, null, 2), 'utf8');
 
-  return txs;
+  return setProductsTransaction;
 };
 
 if (require.main === module) {
-  main(ethers.provider, process.argv[2], process.argv[3]).catch(e => {
+  main(ethers.provider, process.argv[2]).catch(e => {
     console.log('Unhandled error encountered: ', e.stack);
     process.exit(1);
   });

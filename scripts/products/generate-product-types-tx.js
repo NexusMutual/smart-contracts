914e58f93ccd33659be51a0e0f0b637683800f73
Dan Octavian <dan@MacBook-Pro.local>
2023-07-04 10:59:32 +0300

    wip product types

--- scripts/products/generate-product-types-tx.js
+++ scripts/products/generate-product-types-tx.js
@@ -2,8 +2,13 @@ require('dotenv').config();
 const { ethers, config } = require('hardhat');
 const fs = require('fs');
 const path = require('path');
+const { MaxUint256 } = ethers.constants;
 
 const { parse: csvParse } = require('csv-parse/sync');
+const ipfsClient = require('ipfs-http-client');
+const fetch = require('node-fetch');
+
+const IPFS_API_URL = 'https://api.nexusmutual.io/ipfs-api/api/v0';
 
 const OUTPUT_FILE = path.join(
   config.paths.root,
@@ -13,36 +18,62 @@ const OUTPUT_FILE = path.join(
 
 const COVER_ADDRESS = '0xcafeac0fF5dA0A2777d915531bfA6B29d282Ee62';
 
+const ipfs = ipfsClient({ url: IPFS_API_URL });
+
 /**
- * How to use from CLI:
- *  node scripts/products/generate-product-types-tx.js
- *  The script uses product-type-data.csv as an input. Currently, supports only updates to existing products.
- *  file output is written to scripts/products/setProductTypes-txs.json
- *  Use that transaction data in your MetaMask extension to sign.
+ *
+ * Generate the tx data for the Cover.setProductsTypes transaction based on the data
+ * in the productsDataFile CSV.
+ *
+ * CLI parameters in this order: productsTypesDataFilePath
+ *
+ * NOTE: Product type editing not yet supported. 'Id' is ignored.
+ *
+ * The output is written to OUTPUT_FILE.
+ *
+ * @param productsTypesDataFilePath path for file of products
+ * @returns {Promise<{setProductsTransaction: *}>}
  */
-const main = async () => {
+const main = async productsTypesDataFilePath => {
   const cover = await ethers.getContractAt('Cover', COVER_ADDRESS);
 
-  const V2OnChainProductTypeDataProductsPath = path.join(__dirname, '../v2-migration/input/product-type-data.csv');
-
-  const productTypeData = csvParse(fs.readFileSync(V2OnChainProductTypeDataProductsPath, 'utf8'), {
+  const productTypeData = csvParse(fs.readFileSync(productsTypesDataFilePath, 'utf8'), {
     columns: true,
     skip_empty_lines: true,
   });
-  const productTypeEntries = productTypeData.map((data, i) => {
-    return {
-      productTypeName: '',
-      productTypeId: data.Id,
-      ipfsMetadata: '',
-      productType: {
-        claimMethod: data['Claim Method'],
-        gracePeriod: data['Grace Period (days)'] * 24 * 3600, // This MUST be in seconds
-      },
-      expectedProductTypeId: i,
-    };
-  });
+
+  const productTypeEntries = await Promise.all(
+    productTypeData.map(async (data, i) => {
+      const url = data['Cover Wording URL'];
+
+      const productTypeName = data.Name;
+      console.log(`Fetching ${productTypeName} cover wording from ${url}`);
+      const agreementBuffer = await fetch(url).then(x => x.buffer());
+
+      console.log(`Uploading ${productTypeName} cover wording to IPFS`);
+      const agreement = await ipfs.add(agreementBuffer);
+      const productTypeHash = agreement.path;
+
+      console.log(`Pinning ${productTypeHash}`);
+      await ipfs.pin.add(productTypeHash);
+
+      return {
+        productTypeName: data.Name,
+        productTypeId: MaxUint256, // create new product type
+        ipfsMetadata: productTypeHash,
+        productType: {
+          claimMethod: data['Claim Method'],
+          gracePeriod: data['Grace Period (days)'] * 24 * 3600, // This MUST be in seconds
+        },
+        expectedProductTypeId: i,
+      };
+    }),
+  );
 
   const setProductTypesTransaction = await cover.populateTransaction.setProductTypes(productTypeEntries);
+
+  console.log(`Tx data ${setProductTypesTransaction.data}`);
+
   fs.writeFileSync(OUTPUT_FILE, JSON.stringify({ setProductTypesTransaction }, null, 2), 'utf8');
 
   console.log(`Wrote transaction data to ${OUTPUT_FILE}`);
@@ -51,7 +82,7 @@ const main = async () => {
 };
 
 if (require.main === module) {
-  main().catch(e => {
+  main(process.argv[2]).catch(e => {
     console.log('Unhandled error encountered: ', e.stack);
     process.exit(1);
   });

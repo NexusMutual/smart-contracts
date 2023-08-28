166c1db5da94b536835589ff2400124c00cf1a94
danoctavian <danoctavian91@gmail.com>
2023-08-28 06:38:51 +0300

    add migration for product and product types

--- contracts/mocks/CoverProducts/CoverProductsMockCover.sol
+++ contracts/mocks/CoverProducts/CoverProductsMockCover.sol
@@ -11,6 +11,33 @@ import "../../interfaces/ICoverProducts.sol";
 contract CoverProductsMockCover is ICover {
   uint public constant GLOBAL_MIN_PRICE_RATIO = 100; // 1%
 
+  Product[] internal _products;
+  ProductType[] internal _productTypes;
+
+  mapping(uint => string) public productNames;
+  mapping(uint => string) public productTypeNames;
+  mapping(uint => uint[]) public _allowedPools;
+
+  function setProductsAndProductTypes(
+    Product[] memory products,
+    ProductType[] memory productTypes,
+    string[] memory _productNames,
+    string[] memory _productTypeNames,
+    uint[][] memory allowedPoolsList
+  ) external {
+
+    for (uint i = 0; i < products.length; i++) {
+      _products.push(products[i]);
+      productNames[i] = _productNames[i];
+      _allowedPools[i] = allowedPoolsList[i];
+    }
+
+    for (uint i = 0; i < products.length; i++) {
+      _productTypes.push(productTypes[i]);
+      productTypeNames[i] = _productTypeNames[i];
+    }
+  }
+
   function coverData(uint /* coverId */) external pure returns (CoverData memory) {
     revert("Unsupported");
   }
@@ -53,6 +80,18 @@ contract CoverProductsMockCover is ICover {
     uint[] memory /* _capacityReductionRatios */
   ) {
     revert("Unsupported");
+  }
+
+  function getProductsToMigrate() external view returns (Product[] memory) {
+    return _products;
+  }
+
+  function getProductTypesToMigrate() external view returns (ProductType[] memory) {
+    return _productTypes;
+  }
+
+  function allowedPools(uint productId) external view returns (uint[] memory) {
+    return _allowedPools[productId];
   }
 
   /* === MUTATIVE FUNCTIONS ==== */

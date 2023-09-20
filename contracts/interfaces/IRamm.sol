0b76a6ce3a0147ade6b04a1d9aca82545a84ac58
shark0der <commits@shark0der.com>
2023-09-20 18:49:32 +0300

    Add RammLib and Slot0/1 structs

--- contracts/interfaces/IRamm.sol
+++ contracts/interfaces/IRamm.sol
@@ -6,21 +6,48 @@ import "./IPool.sol";
 import "./INXMToken.sol";
 import "./ITokenController.sol";
 
+// storage structs
+
+struct Slot0 {
+  uint128 nxmReserveA;
+  uint128 nxmReserveB;
+}
+
+struct Slot1 {
+  uint128 ethReserve;
+  uint96 budget;
+  uint32 updatedAt;
+}
+
 struct Observation {
   uint32 timestamp;
   uint64 priceCumulativeAbove;
   uint64 priceCumulativeBelow;
 }
 
-interface IRamm {
+// memory structs
 
-  struct State {
-    uint nxmReserveA;
-    uint nxmReserveB;
-    uint ethReserve;
-    uint budget;
-    uint lastUpdateTimestamp;
+struct State {
+  uint nxmA;
+  uint nxmB;
+  uint eth;
+  uint budget;
+  uint timestamp;
+}
+
+library RammLib {
+
+  function clone(Observation memory src) internal pure returns (Observation memory) {
+    return Observation(src.timestamp, src.priceCumulativeAbove, src.priceCumulativeBelow);
   }
+
+  function clone(State memory state) internal pure returns (State memory) {
+    return State(state.nxmA, state.nxmB, state.eth, state.budget, state.timestamp);
+  }
+
+}
+
+interface IRamm {
 
   struct CumulativePriceCalculationProps {
     uint previousEthReserve;
@@ -29,8 +56,6 @@ interface IRamm {
     uint currentNxmA;
     uint previousNxmB;
     uint currentNxmB;
-    uint ratchetSpeedA;
-    uint ratchetSpeedB;
     uint previousTimestamp;
     uint observationTimestamp;
   }
@@ -47,17 +72,18 @@ interface IRamm {
 
   /* ========== VIEWS ========== */
 
-  function getReserves(
-    uint capital,
-    uint supply,
-    uint timestamp
-  ) external view returns (uint ethReserve, uint nxmA, uint nxmB, uint remainingBudget);
+  function getReserves() external view returns (
+    uint ethReserve,
+    uint nxmA,
+    uint nxmB,
+    uint remainingBudget
+  );
 
   function getSpotPrices() external view returns (uint spotPriceA, uint spotPriceB);
 
   function getBookValue() external view returns (uint bookValue);
 
-  function getInternalPrice() external view returns (uint price);
+  function getInternalPrice() external returns (uint price);
 
   /* === MUTATIVE FUNCTIONS ==== */
 

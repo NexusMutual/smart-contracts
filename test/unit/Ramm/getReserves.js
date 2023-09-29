8ab6aa965845bf2569780fc9c69e5b060100ffa5
Rocky Murdoch <rockymurdoch.tech@gmail.com>
2023-09-29 19:44:12 +0300

    Add getReserves unit tests

--- test/unit/Ramm/getReserves.js
+++ test/unit/Ramm/getReserves.js
@@ -1,21 +1,45 @@
+const { ethers } = require('hardhat');
 const { expect } = require('chai');
 const { loadFixture } = require('@nomicfoundation/hardhat-network-helpers');
 const { setup } = require('./setup');
+const { getReserves } = require('../../utils/getReserves');
 const { setNextBlockTime, mineNextBlock } = require('../../utils/evm');
 
 describe('getReserves', function () {
-  it('should return current state in the pools', async function () {
+  it('should return current state in the pools - ratchet value', async function () {
     const fixture = await loadFixture(setup);
-    const { ramm } = fixture.contracts;
+    const { ramm, pool, tokenController } = fixture.contracts;
 
-    const timestamp = await ramm.lastSwapTimestamp();
-    await setNextBlockTime(timestamp.add(6 * 60 * 60).toNumber());
+    const { timestamp } = await ethers.provider.getBlock('latest');
+    const nextBlockTimestamp = timestamp + 1 * 60 * 60;
+    await setNextBlockTime(nextBlockTimestamp);
     await mineNextBlock();
-    const { currentLiquidity, nxmA, nxmB, currentBudget } = await ramm.getReserves();
 
-    expect(currentLiquidity).to.be.equal('2050000000000000000000'); // extracted from the prototype simulation
-    expect(nxmA).to.be.equal('68826162646107933349888'); // extracted from the prototype simulation
-    expect(nxmB).to.be.equal('200688905003625815808560'); // extracted from the prototype simulation
-    expect(currentBudget).to.be.equal('200000000000000000000'); // extracted from the prototype simulation
+    const { _ethReserve, nxmA, nxmB, _budget } = await ramm.getReserves();
+    const expectedReserves = await getReserves(fixture.state, pool, tokenController, nextBlockTimestamp);
+
+    expect(_ethReserve).to.be.equal(expectedReserves.eth);
+    expect(nxmA).to.be.equal(expectedReserves.nxmA);
+    expect(nxmB).to.be.equal(expectedReserves.nxmB);
+    expect(_budget).to.be.equal(expectedReserves.budget);
+  });
+  it('should return current state in the pools - book value', async function () {
+    const fixture = await loadFixture(setup);
+    const { ramm, pool, tokenController } = fixture.contracts;
+
+    // set next block time far enough to reach book value (e.g. 5 days)
+    const { timestamp } = await ethers.provider.getBlock('latest');
+    const timeElapsed = 5 * 24 * 60 * 60;
+    const nextBlockTime = timestamp + timeElapsed;
+    await setNextBlockTime(nextBlockTime);
+    await mineNextBlock();
+
+    const { _ethReserve, nxmA, nxmB, _budget } = await ramm.getReserves();
+    const expectedReserves = await getReserves(fixture.state, pool, tokenController, nextBlockTime);
+
+    expect(_ethReserve).to.be.equal(expectedReserves.eth);
+    expect(nxmA).to.be.equal(expectedReserves.nxmA);
+    expect(nxmB).to.be.equal(expectedReserves.nxmB);
+    expect(_budget).to.be.equal(expectedReserves.budget);
   });
 });

ba1b5d4f7568ea797f2053abccfdc5544aea0817
Rocky Murdoch <rockymurdoch.tech@gmail.com>
2023-10-11 16:46:19 +0300

    Add loadState unit test

---
+++ test/unit/Ramm/loadState.js
@@ -0,0 +1,24 @@
+const { expect } = require('chai');
+const { loadFixture } = require('@nomicfoundation/hardhat-network-helpers');
+const { setup } = require('./setup');
+
+describe('loadState', function () {
+  it('should correctly return the current state', async function () {
+    const fixture = await loadFixture(setup);
+    const { ramm } = fixture.contracts;
+
+    const state = await ramm.loadState();
+
+    // Expected state
+    const { nxmReserveA, nxmReserveB } = await ramm.slot0();
+    const { ethReserve, budget, updatedAt } = await ramm.slot1();
+    const ratchetSpeed = await ramm.ratchetSpeed();
+
+    expect(state.nxmA).to.be.equal(nxmReserveA);
+    expect(state.nxmB).to.be.equal(nxmReserveB);
+    expect(state.eth).to.equal(ethReserve);
+    expect(state.budget).to.equal(budget);
+    expect(state.ratchetSpeed).to.equal(ratchetSpeed);
+    expect(state.timestamp).to.equal(updatedAt);
+  });
+});

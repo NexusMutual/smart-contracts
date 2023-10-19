c3731776298ef78d45de37144a7e7755ce59c1cd
Rocky Murdoch <rockymurdoch.tech@gmail.com>
2023-10-19 22:39:53 +0300

    Add BudgetRemoved event unit test

--- test/unit/Ramm/removeBudget.js
+++ test/unit/Ramm/removeBudget.js
@@ -3,7 +3,6 @@ const { expect } = require('chai');
 const { loadFixture } = require('@nomicfoundation/hardhat-network-helpers');
 
 const { setup } = require('./setup');
-const { setNextBlockBaseFee } = require('../../utils/evm');
 
 describe('removeBudget', function () {
   it('should set the budget to 0', async function () {
@@ -11,14 +10,22 @@ describe('removeBudget', function () {
     const { ramm } = fixture.contracts;
     const [governance] = fixture.accounts.governanceContracts;
 
-    await setNextBlockBaseFee(0);
-    const governanceSigner = await ethers.provider.getSigner(governance.address);
-
     const before = await ramm.slot1();
+    const governanceSigner = await ethers.provider.getSigner(governance.address);
     await ramm.connect(governanceSigner).removeBudget(); // onlyGovernance
     const after = await ramm.slot1();
 
     expect(before.budget).to.be.not.equal(0);
     expect(after.budget).to.be.equal(0);
+  });
+
+  it('should emit event BudgetRemoved', async function () {
+    const fixture = await loadFixture(setup);
+    const { ramm } = fixture.contracts;
+    const [governance] = fixture.accounts.governanceContracts;
+
+    const governanceSigner = await ethers.provider.getSigner(governance.address);
+
+    await expect(ramm.connect(governanceSigner).removeBudget()).to.emit(ramm, 'BudgetRemoved');
   });
 });

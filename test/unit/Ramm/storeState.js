1283aabe7a5c7f92b3424e0341340afc603c43f4
Rocky Murdoch <rockymurdoch.tech@gmail.com>
2023-10-31 18:49:18 +0200

    Remove assert + fix linting issues

--- test/unit/Ramm/storeState.js
+++ test/unit/Ramm/storeState.js
@@ -2,7 +2,7 @@ const { ethers } = require('hardhat');
 const { expect } = require('chai');
 const { loadFixture } = require('@nomicfoundation/hardhat-network-helpers');
 
-const { getState, setup } = require('./setup');
+const { setup } = require('./setup');
 const { setNextBlockTime } = require('../../utils/evm');
 
 const { parseEther } = ethers.utils;

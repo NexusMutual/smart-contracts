0315418bf399ba38851242b91d1e4d13ce42f7ed
Miljan Milidrag <miljan.milidrag@protonmail.com>
2023-11-14 09:49:32 +0100

    Add sanity test for initial observations

--- test/unit/Ramm/getObservation.js
+++ test/unit/Ramm/getObservation.js
@@ -3,11 +3,62 @@ const { expect } = require('chai');
 const { loadFixture } = require('@nomicfoundation/hardhat-network-helpers');
 
 const { getState, setup } = require('./setup');
-const { timeTillBv, calculateTwapAboveForPeriod, calculateTwapBelowForPeriod } = require('./helpers');
+const { timeTillBv, calculateTwapAboveForPeriod, calculateTwapBelowForPeriod, divCeil } = require('./helpers');
+const { parseEther } = require('ethers/lib/utils');
 
 const { BigNumber } = ethers;
 
 describe('getObservation', function () {
+  it('should check initial observation', async function () {
+    const fixture = await loadFixture(setup);
+    const { ramm, pool, tokenController } = fixture.contracts;
+    const { PRICE_BUFFER_DENOMINATOR, PRICE_BUFFER, PERIOD_SIZE } = fixture.constants;
+
+    const { updatedAt } = await ramm.slot1();
+    const timestamp = BigNumber.from(updatedAt);
+    const capital = await pool.getPoolValueInEth();
+    const supply = await tokenController.totalSupply();
+    const bondingCurvePrice = await pool.getTokenPrice();
+
+    const initialPriceA = bondingCurvePrice.add(
+      parseEther('1').mul(capital).mul(PRICE_BUFFER).div(PRICE_BUFFER_DENOMINATOR).div(supply),
+    );
+    const initialPriceB = parseEther('1')
+      .mul(capital)
+      .mul(PRICE_BUFFER_DENOMINATOR.sub(PRICE_BUFFER))
+      .div(PRICE_BUFFER_DENOMINATOR)
+      .div(supply);
+
+    let priceCumulativeAbove = BigNumber.from(0);
+    let priceCumulativeBelow = BigNumber.from(0);
+    const endIdx = divCeil(timestamp, PERIOD_SIZE);
+    let previousTimestamp = endIdx.sub(11).mul(PERIOD_SIZE);
+    const expectedObservations = [];
+
+    for (let idx = endIdx.sub(2); idx.lte(endIdx); idx = idx.add(1)) {
+      const observationTimestamp = timestamp.lte(idx.mul(PERIOD_SIZE)) ? timestamp : idx.mul(PERIOD_SIZE);
+      const observationIndex = idx.mod(3).toNumber();
+      const timeElapsed = observationTimestamp.sub(previousTimestamp);
+
+      priceCumulativeAbove = priceCumulativeAbove.add(initialPriceA.mul(timeElapsed).div(1e9));
+      priceCumulativeBelow = priceCumulativeBelow.add(initialPriceB.mul(timeElapsed).div(1e9));
+
+      expectedObservations[observationIndex] = {
+        timestamp: observationTimestamp,
+        priceCumulativeAbove,
+        priceCumulativeBelow,
+      };
+      previousTimestamp = observationTimestamp;
+    }
+
+    const observations = await Promise.all([0, 1, 2].map(index => ramm.observations(index)));
+
+    for (const i in observations) {
+      expect(observations[i].timestamp).to.be.equal(expectedObservations[i].timestamp);
+      expect(observations[i].priceCumulativeAbove).to.be.equal(expectedObservations[i].priceCumulativeAbove);
+      expect(observations[i].priceCumulativeBelow).to.be.equal(expectedObservations[i].priceCumulativeBelow);
+    }
+  });
   it('should do underflow/overflow sanity check for get observation', async function () {
     const fixture = await loadFixture(setup);
     const { ramm, pool, tokenController, mcr } = fixture.contracts;

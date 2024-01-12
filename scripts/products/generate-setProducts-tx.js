814102ad35ab9a735ef42b075bdb7817e349da66
Roxana Danila <roxanadanila24@gmail.com>
2024-01-12 11:47:23 +0200

    Fix product and product type related scripts

--- scripts/products/generate-setProducts-tx.js
+++ scripts/products/generate-setProducts-tx.js
@@ -4,6 +4,10 @@ const fs = require('fs');
 const path = require('path');
 
 const { parse: csvParse } = require('csv-parse/sync');
+const ipfsClient = require('ipfs-http-client');
+
+const IPFS_API_URL = 'https://api.nexusmutual.io/ipfs-api/api/v0';
+const ipfs = ipfsClient({ url: IPFS_API_URL });
 
 const { MaxUint256 } = ethers.constants;
 
@@ -14,7 +18,6 @@ const OUTPUT_FILE = path.join(
 );
 
 const YIELD_TOKEN_PRODUCT_TYPE_ID = '2';
-
 const COVER_PROXY_ADDRESS = '0xcafeac0fF5dA0A2777d915531bfA6B29d282Ee62';
 
 /**
@@ -46,35 +49,49 @@ const main = async (provider, productsDataFilePath) => {
     skip_empty_lines: true,
   });
 
-  const productEntries = productData.map(data => {
-    const coverAssetsAsText = data['Cover Assets'];
-    const coverAssets =
-      (coverAssetsAsText === 'DAI' && 0b10) || // Yield token cover that uses DAI
-      (coverAssetsAsText === 'ETH' && 0b01) || // Yield token cover that uses ETH
-      0; // The default is 0 - this means all assets are allowed (no whitelist)
+  const productEntries = await Promise.all(
+    productData.map(async data => {
+      const coverAssetsAsText = data['Cover Assets'];
+      const coverAssets =
+        (coverAssetsAsText === 'DAI' && 0b10) || // Yield token cover that uses DAI
+        (coverAssetsAsText === 'ETH' && 0b01) || // Yield token cover that uses ETH
+        0; // The default is 0 - this means all assets are allowed (no whitelist)
 
-    const productParams = {
-      productName: data['Product Name'],
-      productId: MaxUint256, // create new product - use Max Uint.
-      ipfsMetadata: data['IPFS Metadata'], // IPFS metadata is optional.
-      product: {
-        productType: data['Product Type'],
-        yieldTokenAddress:
-          data['Product Type'] === YIELD_TOKEN_PRODUCT_TYPE_ID
-            ? data['Yield Token Address']
-            : '0x0000000000000000000000000000000000000000',
-        coverAssets,
-        // works for integers: parseInt('10%') === 10; to convert it to 4 decimal ratio you multiply by 100
-        initialPriceRatio: parseInt(data['Initial Price Ratio']) * 100,
-        // works for integers: parseInt('0%') === 0
-        capacityReductionRatio: parseInt(data['Capacity Reduction Ratio']),
-        useFixedPrice: data['Use fixed price'].trim() === 'yes',
-      },
-      allowedPools: data['Allowed Pools'].split(',').map(parseInt),
-    };
+      const filePath = data['IPFS Metadata'];
 
-    return productParams;
-  });
+      console.log(`Uploading ${data['Product Name']} annex from ${filePath} to IPFS`);
+      const annex = await ipfs.add(fs.readFileSync(filePath));
+      console.log(`Pinning ${annex.path}`);
+      await ipfs.pin.add(annex.path);
+
+      console.log(`Appending ${annex.path} to ${data['Product Name']} metadata on IPFS`);
+      const metadata = await ipfs.add(Buffer.from(JSON.stringify({ annex: annex.path })));
+      console.log(`Pinning ${metadata.path}`);
+      await ipfs.pin.add(metadata.path);
+
+      const productParams = {
+        productName: data['Product Name'],
+        productId: data['Product Id'] || MaxUint256, // create new product - use Max Uint.
+        ipfsMetadata: metadata.path, // IPFS metadata is optional.
+        product: {
+          productType: data['Product Type'],
+          yieldTokenAddress:
+            data['Product Type'] === YIELD_TOKEN_PRODUCT_TYPE_ID
+              ? data['Yield Token Address']
+              : '0x0000000000000000000000000000000000000000',
+          coverAssets,
+          // works for integers: parseInt('10%') === 10; to convert it to 4 decimal ratio you multiply by 100
+          initialPriceRatio: parseInt(data['Initial Price Ratio']) * 100,
+          // works for integers: parseInt('0%') === 0
+          capacityReductionRatio: parseInt(data['Capacity Reduction Ratio']),
+          useFixedPrice: data['Use fixed price'].trim() === 'TRUE',
+        },
+        allowedPools: [11],
+      };
+
+      return productParams;
+    }),
+  );
 
   console.log('Tx input: ', productEntries);
 

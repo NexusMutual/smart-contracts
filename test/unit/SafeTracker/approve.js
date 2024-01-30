c5834e35579d6c4bf9596f05e66dab6b2aee61ad
Miljan Milidrag <miljan.milidrag@protonmail.com>
2024-01-30 08:14:42 +0100

    Fix approve method on SafeTracker

--- test/unit/SafeTracker/approve.js
+++ test/unit/SafeTracker/approve.js
@@ -4,14 +4,6 @@ const { loadFixture } = require('@nomicfoundation/hardhat-network-helpers');
 const { setup } = require('./setup');
 
 describe('approve', function () {
-  it('should revert if amount it greater than 0', async function () {
-    const fixture = await loadFixture(setup);
-    const { safeTracker } = fixture.contracts;
-    const { accounts } = fixture;
-
-    await expect(safeTracker.approve(accounts.members[0].address, 100)).to.be.revertedWith('Amount exceeds balance');
-  });
-
   it('should emit Approval event if value is 0', async function () {
     const fixture = await loadFixture(setup);
     const { safeTracker } = fixture.contracts;
@@ -20,6 +12,6 @@ describe('approve', function () {
       members: [member],
     } = fixture.accounts;
 
-    await expect(safeTracker.connect(defaultSender).approve(member.address, 0)).to.emit(safeTracker, 'Approval');
+    await expect(safeTracker.connect(defaultSender).approve(member.address, 100)).to.emit(safeTracker, 'Approval');
   });
 });

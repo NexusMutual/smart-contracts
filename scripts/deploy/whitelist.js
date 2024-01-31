a69699412732c5226abdfd6a05e89a37b4ede69d
shark0der <commits@shark0der.com>
2024-01-31 07:42:40 +0100

    Add testnet MR patching code

---
+++ scripts/deploy/whitelist.js
@@ -0,0 +1,20 @@
+const { ethers, artifacts } = require('hardhat');
+
+const MR = '0x055CC48f7968FD8640EF140610dd4038e1b03926';
+
+const main = async () => {
+  const mrProxy = await ethers.getContractAt('OwnedUpgradeabilityProxy', MR);
+  const mrImplementationAddress = await mrProxy.implementation();
+
+  const { deployedBytecode } = await artifacts.readArtifact('TestnetMemberRoles');
+  await ethers.provider.send('tenderly_setCode', [mrImplementationAddress, deployedBytecode]);
+
+  console.log('MR patched');
+};
+
+main()
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error);
+    process.exit(1);
+  });

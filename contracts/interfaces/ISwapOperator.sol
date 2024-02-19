f3ac8eb84666a5e179e1e7b83275d38e0e949f26
Rocky Murdoch <rockymurdoch.tech@gmail.com>
2024-02-19 12:35:27 +0200

    Update ISwapOperator interface

--- contracts/interfaces/ISwapOperator.sol
+++ contracts/interfaces/ISwapOperator.sol
@@ -2,6 +2,17 @@
 
 pragma solidity >=0.5.0;
 
+import "../external/cow/GPv2Order.sol";
+
 interface ISwapOperator {
+
+  function getDigest(GPv2Order.Data calldata order) external view returns (bytes32);
+
+  function getUID(GPv2Order.Data calldata order) external view returns (bytes memory);
+
+  function placeOrder(GPv2Order.Data calldata order, bytes calldata orderUID) external;
+
   function orderInProgress() external returns (bool);
+
+  function recoverAsset(address assetAddress, address receiver) external;
 }

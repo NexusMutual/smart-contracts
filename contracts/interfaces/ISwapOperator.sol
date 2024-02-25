02c2203bdcafc11f1a6c95513c3b0cafef915bf2
Rocky Murdoch <rockymurdoch.tech@gmail.com>
2024-02-25 20:17:53 +0200

    Update ISwapOperator interface
    * add enum SwapOperationType
    * add struct SwapOperation
    * add events
    * add custom errors

--- contracts/interfaces/ISwapOperator.sol
+++ contracts/interfaces/ISwapOperator.sol
@@ -3,16 +3,59 @@
 pragma solidity >=0.5.0;
 
 import "../external/cow/GPv2Order.sol";
+import "../interfaces/IPool.sol";
 
 interface ISwapOperator {
+
+  enum SwapOperationType { WethToAsset, AssetToWeth, AssetToAsset }
+
+  struct SwapOperation {
+    GPv2Order.Data order;
+    SwapDetails sellSwapDetails;
+    SwapDetails buySwapDetails;
+    SwapOperationType swapType;
+  }
+
+  /* ========== VIEWS ========== */
 
   function getDigest(GPv2Order.Data calldata order) external view returns (bytes32);
 
   function getUID(GPv2Order.Data calldata order) external view returns (bytes memory);
+  
+  function orderInProgress() external view returns (bool);
+
+  /* ==== MUTATIVE FUNCTIONS ==== */
 
   function placeOrder(GPv2Order.Data calldata order, bytes calldata orderUID) external;
 
-  function orderInProgress() external returns (bool);
-
   function recoverAsset(address assetAddress, address receiver) external;
+
+  /* ========== EVENTS AND ERRORS ========== */
+
+  event OrderPlaced(GPv2Order.Data order);
+  event OrderClosed(GPv2Order.Data order, uint filledAmount);
+  event Swapped(address indexed fromAsset, address indexed toAsset, uint amountIn, uint amountOut);
+
+  // Order 
+  error OrderInProgress();
+  error OrderUidMismatch(bytes providedOrderUID, bytes expectedOrderUID);
+  error UnsupportedTokenBalance(string kind);
+  error InvalidReceiver();
+  error OrderTokenIsDisabled(address token);
+
+  // Valid To
+  error BelowMinValidTo(uint minValidTo);
+  error AboveMaxValidTo(uint maxValidTo);
+  
+  // Cool down
+  error InsufficientTimeBetweenSwaps(uint minValidSwapTime);
+  
+  // Balance
+  error EthReserveBelowMin(uint ethPostSwap, uint minEthReserve);
+  error InvalidBalance(uint tokenBalance, uint limit, string limitType);
+  error InvalidPostSwapBalance(uint postSwapBalance, uint limit, string limitType);
+  error MaxSlippageExceeded(uint minAmount);
+
+  // Fee
+  error AboveMaxFee(uint maxFee);
 }

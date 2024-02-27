2ac90a97cf6006282b0d5e1a2f9249cd1df52a3d
shark0der <commits@shark0der.com>
2024-02-27 17:49:28 +0200

    Upgrade MemberRoles to TestnetMemberRoles instead of setting its bycode using cheat codes

--- scripts/deploy/whitelist.js
+++ scripts/deploy/whitelist.js
@@ -1,17 +1,27 @@
-const { ethers, artifacts } = require('hardhat');
+const { ethers, network } = require('hardhat');
 
 const MR = '0x055CC48f7968FD8640EF140610dd4038e1b03926';
+const TK = '0xd7c49CEE7E9188cCa6AD8FF264C1DA2e69D4Cf3B';
 
 const main = async () => {
+  console.log(`Starting patch script on ${network.name} network`);
+
   const mrProxy = await ethers.getContractAt('OwnedUpgradeabilityProxy', MR);
-  const mrImplementationAddress = await mrProxy.implementation();
 
-  const { deployedBytecode } = await artifacts.readArtifact('TestnetMemberRoles');
-  await ethers.provider.send('tenderly_setCode', [mrImplementationAddress, deployedBytecode]);
+  console.log('Reading owner');
+  const owner = await mrProxy.proxyOwner();
 
-  console.log('MR patched');
+  console.log('Getting a signer');
+  const [signer] = await ethers.getSigners();
+
+  console.log('Deploying new implementation contract');
+  const mrImplementation = await ethers.deployContract('TestnetMemberRoles', [TK], signer);
+
+  console.log('Upgrading proxy');
+  const provider = new ethers.providers.JsonRpcProvider(network.config.url);
+  const mrProxyAsOwner = new ethers.Contract(MR, mrProxy.interface, provider.getSigner(owner));
+  await mrProxyAsOwner.upgradeTo(mrImplementation.address);
 };
-
 main()
   .then(() => process.exit(0))
   .catch(error => {

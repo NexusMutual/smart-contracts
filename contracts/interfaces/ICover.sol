46756b5148a09f3b2d04f5b0950208f483ec9350
Miljan Milidrag <miljan.milidrag@protonmail.com>
2024-03-14 08:58:15 +0100

    Add `recalculateActiveCoverInAsset` to Cover contract

--- contracts/interfaces/ICover.sol
+++ contracts/interfaces/ICover.sol
@@ -120,6 +120,8 @@ interface ICover {
     uint segmentId
   ) external view returns (CoverSegment memory);
 
+  function recalculateActiveCoverInAsset(uint coverAsset) external;
+
   function totalActiveCoverInAsset(uint coverAsset) external view returns (uint);
 
   function getGlobalCapacityRatio() external view returns (uint);

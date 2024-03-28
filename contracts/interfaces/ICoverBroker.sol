69bc86e26ecbac1770cfb228cd29eb4b600b9b13
Rocky Murdoch <rockymurdoch.tech@gmail.com>
2024-03-28 18:07:48 +0200

    Add ICoverBroker

---
+++ contracts/interfaces/ICoverBroker.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: GPL-3.0-only
+
+pragma solidity ^0.8.18;
+
+import "../interfaces/ICover.sol";
+
+interface ICoverBroker {
+  /* ==== FUNCTIONS ==== */
+
+  function buyCover(
+    BuyCoverParams memory params,
+    PoolAllocationRequest[] calldata poolAllocationRequests
+  ) external payable returns (uint coverId);
+
+  function switchMembership(address newAddress) external;
+
+  function transferFunds(address assetAddress) external;
+
+  /* ==== ERRORS ==== */
+
+  error OnlyOwner();
+  error TransferFailed(address to, uint value, address token);
+  error ZeroBalance(address token);
+}

e1abf2f6c60cec12f77ff2e83f2705724758a662
Miljan Milidrag <miljan.milidrag@protonmail.com>
2024-04-08 13:24:11 +0200

    Restructure mocks directory

---
+++ contracts/mocks/modules/Incidents/ICMockPool.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: GPL-3.0-only
+
+pragma solidity ^0.8.18;
+
+import "../../common/PoolMock.sol";
+import "../../../interfaces/IPriceFeedOracle.sol";
+
+contract ICMockPool is PoolMock {
+
+  IPriceFeedOracle public override priceFeedOracle;
+
+  constructor (address _priceFeedOracle) {
+    priceFeedOracle = IPriceFeedOracle(_priceFeedOracle);
+  }
+
+}

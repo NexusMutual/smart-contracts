d9bcca3534d8b47c5d7299efe14096bf4b419136
Rocky Murdoch <rockymurdoch.tech@gmail.com>
2024-05-06 10:34:59 +0300

    Add IYieldDeposit interface

---
+++ contracts/interfaces/IYieldDeposit.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: GPL-3.0-only
+
+pragma solidity ^0.8.18;
+
+interface IYieldDeposit {
+  /* ========== VIEWS ========== */
+
+  function getCurrentTokenPrice() external returns (uint);
+
+  function getAvailableYield() external returns (uint);
+
+  /* ==== MUTATIVE FUNCTIONS ==== */
+
+  function deposit(uint256 amount) external;
+
+  function withdraw() external;
+
+  function withdrawAvailableYield() external;
+
+  function updateCoverPricePercentage(uint16 newCoverPricePercentage) external;
+
+  /* ========== EVENTS AND ERRORS ========== */
+
+  event TokenDeposited(address from, uint256 depositAmount, uint256 coverAmountBought);
+  event TokenWithdrawn(address from, uint256 withdrawalAmount);
+
+  error InvalidDepositAmount();
+  error InvalidTokenRate();
+  error NoYieldAvailable();
+  error InsufficientDepositForWithdrawal();
+  error WithdrawBeforeMakingNewDeposit();
+}

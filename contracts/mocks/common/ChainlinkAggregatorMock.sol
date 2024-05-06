14d2766b030880241c92606f56a10c5a4648ef3c
Rocky Murdoch <rockymurdoch.tech@gmail.com>
2024-05-06 10:39:43 +0300

    Update ChainlinkAggregatorMock with latestRoundData - v3 interface

--- contracts/mocks/common/ChainlinkAggregatorMock.sol
+++ contracts/mocks/common/ChainlinkAggregatorMock.sol
@@ -3,9 +3,9 @@
 pragma solidity ^0.8.18;
 
 contract ChainlinkAggregatorMock {
-
   uint public latestAnswer;
   uint public decimals;
+  uint80 public _roundId;
 
   function setDecimals(uint _decimals) public {
     decimals = _decimals;
@@ -15,4 +15,10 @@ contract ChainlinkAggregatorMock {
     latestAnswer = _latestAnswer;
   }
 
+  function latestRoundData()
+    public view
+    returns (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound)
+  {
+    return (_roundId, int256(latestAnswer), block.timestamp, block.timestamp, _roundId);
+  }
 }

f7c225bade0b43e9bd99f6984280584ddcad20d5
Rocky Murdoch <rockymurdoch.tech@gmail.com>
2024-05-10 01:14:52 +0300

    Update setup

--- test/unit/YieldDeposit/setup.js
+++ test/unit/YieldDeposit/setup.js
@@ -4,31 +4,41 @@ const { setEtherBalance } = require('../utils').evm;
 const { getAccounts } = require('../utils').accounts;
 const { parseEther } = ethers.utils;
 
+const PRICE_DENOMINATOR = 10000;
+
 async function setup() {
   const accounts = await getAccounts();
   const manager = accounts.defaultSender;
   await setEtherBalance(manager.address, parseEther('1000000'));
 
+  const yieldDeposit = await ethers.deployContract('YieldDeposit', [manager.address]);
+
+  const coverPricePercentage = 500;
   const weEth = await ethers.deployContract('ERC20Mock');
-  const chainLinkPriceFeed = await ethers.deployContract('ChainlinkAggregatorMock');
-  const yieldDeposit = await ethers.deployContract('YieldDeposit', [
-    manager.address,
-    weEth.address,
-    18,
-    chainLinkPriceFeed.address,
-    500, // 5%
-  ]);
+  const stEth = await ethers.deployContract('ERC20Mock');
+  const chainLinkPriceFeedWeEth = await ethers.deployContract('ChainlinkAggregatorMock');
+  const chainLinkPriceFeedStEth = await ethers.deployContract('ChainlinkAggregatorMock');
+
+  await yieldDeposit.connect(manager).listToken(weEth.address, chainLinkPriceFeedWeEth.address, coverPricePercentage);
+  await yieldDeposit.connect(manager).listToken(stEth.address, chainLinkPriceFeedStEth.address, coverPricePercentage);
 
   await weEth.mint(accounts.members[0].address, parseEther('1000000'));
-  await chainLinkPriceFeed.setLatestAnswer(parseEther('1.0374')); // TODO: '1.0374'?
+  await stEth.mint(accounts.members[0].address, parseEther('1000000'));
+
+  await chainLinkPriceFeedWeEth.setLatestAnswer(parseEther('1.0374'));
+  await chainLinkPriceFeedStEth.setLatestAnswer(parseEther('1.1365'));
 
   return {
     manager,
     accounts,
+    coverPricePercentage,
+    priceDenominator: PRICE_DENOMINATOR,
     contracts: {
       weEth,
+      stEth,
       yieldDeposit,
-      chainLinkPriceFeed,
+      chainLinkPriceFeedStEth,
+      chainLinkPriceFeedWeEth,
     },
   };
 }

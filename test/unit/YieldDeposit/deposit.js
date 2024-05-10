405ae6425445f27b42dd4e3ec4aef7c48f036878
Rocky Murdoch <rockymurdoch.tech@gmail.com>
2024-05-10 18:13:56 +0300

    Fix deposit unit tests

--- test/unit/YieldDeposit/deposit.js
+++ test/unit/YieldDeposit/deposit.js
@@ -5,8 +5,6 @@ const { loadFixture } = require('@nomicfoundation/hardhat-network-helpers');
 const { increasePriceFeedRate } = require('./helper');
 const { setup } = require('./setup');
 
-const RATE_DENOMINATOR = ethers.BigNumber.from('10').pow(18);
-
 describe('YieldDeposit - deposit', function () {
   it('should revert InvalidDepositAmount if deposit amount is less than or equal to zero', async function () {
     const fixture = await loadFixture(setup);
@@ -16,7 +14,7 @@ describe('YieldDeposit - deposit', function () {
     const depositError = yieldDeposit.connect(member).deposit(weEth.address, '0');
     await expect(depositError).to.revertedWithCustomError(yieldDeposit, 'InvalidDepositAmount');
 
-    expect(await yieldDeposit.totalDepositValue()).to.be.equal('0');
+    expect(await yieldDeposit.totalDepositValue(weEth.address)).to.be.equal('0');
     expect(await yieldDeposit.userTokenDepositValue(member.address, weEth.address)).to.be.equal('0');
   });
 
@@ -29,7 +27,7 @@ describe('YieldDeposit - deposit', function () {
     const depositError = yieldDeposit.connect(member).deposit(unsupportedToken, '1');
     await expect(depositError).to.revertedWithCustomError(yieldDeposit, 'TokenNotSupported');
 
-    expect(await yieldDeposit.totalDepositValue()).to.be.equal('0');
+    expect(await yieldDeposit.totalDepositValue(weEth.address)).to.be.equal('0');
     expect(await yieldDeposit.userTokenDepositValue(member.address, weEth.address)).to.be.equal('0');
   });
 
@@ -37,10 +35,11 @@ describe('YieldDeposit - deposit', function () {
     const fixture = await loadFixture(setup);
     const { yieldDeposit, weEth, chainLinkPriceFeedWeEth } = fixture.contracts;
     const [member] = fixture.accounts.members;
+    const { rateDenominator } = fixture;
 
     const userWeEthBalanceBefore = await weEth.balanceOf(member.address);
     expect(await weEth.balanceOf(yieldDeposit.address)).to.be.equal('0');
-    expect(await yieldDeposit.totalDepositValue()).to.be.equal('0');
+    expect(await yieldDeposit.totalDepositValue(weEth.address)).to.be.equal('0');
     expect(await yieldDeposit.userTokenDepositValue(member.address, weEth.address)).to.be.equal('0');
 
     const depositAmount = ethers.utils.parseEther('10');
@@ -48,12 +47,12 @@ describe('YieldDeposit - deposit', function () {
     await yieldDeposit.connect(member).deposit(weEth.address, depositAmount);
 
     const priceRate = await chainLinkPriceFeedWeEth.latestAnswer();
-    const userDepositValue = depositAmount.mul(priceRate).div(RATE_DENOMINATOR);
+    const userDepositValue = depositAmount.mul(priceRate).div(rateDenominator);
 
     const userWeEthBalanceAfter = await weEth.balanceOf(member.address);
     expect(userWeEthBalanceAfter).to.be.equal(userWeEthBalanceBefore.sub(depositAmount));
     expect(await weEth.balanceOf(yieldDeposit.address)).to.be.equal(depositAmount);
-    expect(await yieldDeposit.totalDepositValue()).to.be.equal(userDepositValue);
+    expect(await yieldDeposit.totalDepositValue(weEth.address)).to.be.equal(userDepositValue);
     expect(await yieldDeposit.userTokenDepositValue(member.address, weEth.address)).to.be.equal(userDepositValue);
   });
 
@@ -69,7 +68,7 @@ describe('YieldDeposit - deposit', function () {
     await yieldDeposit.connect(member).deposit(weEth.address, depositAmount1);
 
     const priceRate1 = await chainLinkPriceFeedWeEth.latestAnswer();
-    const userDeposit1Value = depositAmount1.mul(priceRate1).div(RATE_DENOMINATOR);
+    const userDeposit1Value = depositAmount1.mul(priceRate1).div(fixture.rateDenominator);
 
     await increasePriceFeedRate(chainLinkPriceFeedWeEth);
 
@@ -78,26 +77,27 @@ describe('YieldDeposit - deposit', function () {
     await yieldDeposit.connect(member).deposit(weEth.address, depositAmount2);
 
     const priceRate2 = await chainLinkPriceFeedWeEth.latestAnswer();
-    const userDeposit2Value = depositAmount2.mul(priceRate2).div(RATE_DENOMINATOR);
+    const userDeposit2Value = depositAmount2.mul(priceRate2).div(fixture.rateDenominator);
 
     const userWeEthBalanceAfter = await weEth.balanceOf(member.address);
     const totalUserDepositAmount = depositAmount1.add(depositAmount2);
-    const totalUserDepositValue = userDeposit1Value.add(userDeposit2Value)
+    const totalUserDepositValue = userDeposit1Value.add(userDeposit2Value);
 
     expect(userWeEthBalanceAfter).to.be.equal(userWeEthBalanceBefore.sub(totalUserDepositAmount));
     expect(await weEth.balanceOf(yieldDeposit.address)).to.be.equal(totalUserDepositAmount);
-    expect(await yieldDeposit.totalDepositValue()).to.be.equal(totalUserDepositValue);
+    expect(await yieldDeposit.totalDepositValue(weEth.address)).to.be.equal(totalUserDepositValue);
     expect(await yieldDeposit.userTokenDepositValue(member.address, weEth.address)).to.be.equal(totalUserDepositValue);
   });
 
   it('should be able to do a second deposit on top of existing deposit - different token', async function () {
     const fixture = await loadFixture(setup);
-    const { yieldDeposit, weEth, stEth, chainLinkPriceFeedWeEth, chainLinkPriceFeedStEth } = fixture.contracts;
+    const { yieldDeposit, weEth, wstEth, chainLinkPriceFeedWeEth, chainLinkPriceFeedWstEth } = fixture.contracts;
     const [member] = fixture.accounts.members;
 
     const userWeEthBalanceBefore = await weEth.balanceOf(member.address);
-    const userStEthBalanceBefore = await stEth.balanceOf(member.address);
+    const userWstEthBalanceBefore = await wstEth.balanceOf(member.address);
 
+    // weEth deposit
     const weEthDepositAmount = ethers.utils.parseEther('10');
     await weEth.connect(member).approve(yieldDeposit.address, weEthDepositAmount);
     await yieldDeposit.connect(member).deposit(weEth.address, weEthDepositAmount);
@@ -105,26 +105,24 @@ describe('YieldDeposit - deposit', function () {
     expect(await weEth.balanceOf(member.address)).to.equal(userWeEthBalanceBefore.sub(weEthDepositAmount));
 
     const weEthPriceRate = await chainLinkPriceFeedWeEth.latestAnswer();
-    const weEthDepositValue = weEthDepositAmount.mul(weEthPriceRate).div(RATE_DENOMINATOR);
+    const weEthDepositValue = weEthDepositAmount.mul(weEthPriceRate).div(fixture.rateDenominator);
 
+    // wstEth deposit
     const stEthDepositAmount = ethers.utils.parseEther('30');
-    await stEth.connect(member).approve(yieldDeposit.address, stEthDepositAmount);
-    await yieldDeposit.connect(member).deposit(stEth.address, stEthDepositAmount);
+    await wstEth.connect(member).approve(yieldDeposit.address, stEthDepositAmount);
+    await yieldDeposit.connect(member).deposit(wstEth.address, stEthDepositAmount);
 
-    expect(await stEth.balanceOf(member.address)).to.equal(userStEthBalanceBefore.sub(stEthDepositAmount));
-    
-    const stEthPriceRate = await chainLinkPriceFeedStEth.latestAnswer();
-    const stEthDepositValue = stEthDepositAmount.mul(stEthPriceRate).div(RATE_DENOMINATOR);
-    console.log('stEthDepositValue: ', stEthDepositValue);
+    expect(await wstEth.balanceOf(member.address)).to.equal(userWstEthBalanceBefore.sub(stEthDepositAmount));
 
-    const totalUserDepositAmount = weEthDepositAmount.add(stEthDepositAmount);
-    const totalUserDepositValue = weEthDepositValue.add(stEthDepositValue)
+    const stEthPriceRate = await chainLinkPriceFeedWstEth.latestAnswer();
+    const stEthDepositValue = stEthDepositAmount.mul(stEthPriceRate).div(fixture.rateDenominator);
 
     expect(await weEth.balanceOf(yieldDeposit.address)).to.be.equal(weEthDepositAmount);
-    expect(await stEth.balanceOf(yieldDeposit.address)).to.be.equal(stEthDepositAmount);
-    expect(await yieldDeposit.totalDepositValue()).to.be.equal(totalUserDepositValue);
+    expect(await wstEth.balanceOf(yieldDeposit.address)).to.be.equal(stEthDepositAmount);
+    expect(await yieldDeposit.totalDepositValue(wstEth.address)).to.be.equal(stEthDepositValue);
+    expect(await yieldDeposit.totalDepositValue(weEth.address)).to.be.equal(weEthDepositValue);
     expect(await yieldDeposit.userTokenDepositValue(member.address, weEth.address)).to.be.equal(weEthDepositValue);
-    expect(await yieldDeposit.userTokenDepositValue(member.address, stEth.address)).to.be.equal(stEthDepositValue);
+    expect(await yieldDeposit.userTokenDepositValue(member.address, wstEth.address)).to.be.equal(stEthDepositValue);
   });
 
   it('should emit TokenDeposited on successful deposit', async function () {

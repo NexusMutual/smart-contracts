79fbb021c3dfa7f4215e70505ef5887c8d53f2d4
Rocky Murdoch <rockymurdoch.tech@gmail.com>
2024-05-10 18:13:17 +0300

    Fix compounded daily yield calculation

--- test/unit/YieldDeposit/helper.js
+++ test/unit/YieldDeposit/helper.js
@@ -2,13 +2,17 @@ const { ethers } = require('hardhat');
 
 const APY = ethers.BigNumber.from(50); // 5000% APY
 const YIELD_SCALING_FACTOR = ethers.BigNumber.from('1000000');
-const DAILY_PERCENTAGE_YIELD = APY.mul(YIELD_SCALING_FACTOR)
-  .div(365)
-  .add(1 * YIELD_SCALING_FACTOR);
+const ONE = ethers.BigNumber.from('1000000'); // Same scale as YIELD_SCALING_FACTOR
 
+const DAILY_PERCENTAGE_YIELD = APY.mul(YIELD_SCALING_FACTOR).div(365).add(ONE);
+
+/**
+ * Increases the price feed rate by day based on 5000% APY
+ */
 const increasePriceFeedRate = async (chainLinkPriceFeed, days = 1) => {
   const priceRate = await chainLinkPriceFeed.latestAnswer();
-  const newPriceRate = priceRate.mul(DAILY_PERCENTAGE_YIELD).div(YIELD_SCALING_FACTOR).pow(days);
+  const compoundedDailyYield = DAILY_PERCENTAGE_YIELD.pow(days).div(YIELD_SCALING_FACTOR.pow(days - 1));
+  const newPriceRate = priceRate.mul(compoundedDailyYield).div(YIELD_SCALING_FACTOR);
   await chainLinkPriceFeed.setLatestAnswer(newPriceRate);
 };
 

f58cd9a1a65a3cc6de89edecdfd7d781ae420d54
Miljan Milidrag <miljan.milidrag@protonmail.com>
2024-06-17 19:50:01 +0200

    Add comment for `ICompleteStakingPoolFactory`

--- contracts/interfaces/ICompleteStakingPoolFactory.sol
+++ contracts/interfaces/ICompleteStakingPoolFactory.sol
@@ -4,6 +4,11 @@ pragma solidity >=0.5.0;
 
 import "./IStakingPoolFactory.sol";
 
+/*
+* IStakingPoolFactory is missing the changeOperator function
+* any change to the original interface will affect staking pool addresses
+* This interface is created to add the changeOperator function so it can be used in other contracts
+*/
 interface ICompleteStakingPoolFactory is IStakingPoolFactory {
 
   function changeOperator(address newOperator) external;

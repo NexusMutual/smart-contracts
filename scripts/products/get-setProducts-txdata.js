af10559b163ed7b7f3d7c0034eb08a3c9e6e639e
Rocky Murdoch <rockymurdoch.tech@gmail.com>
2024-06-17 20:58:30 +0300

    Verify decodedTxInput

--- scripts/products/get-setProducts-txdata.js
+++ scripts/products/get-setProducts-txdata.js
@@ -42,14 +42,95 @@ const simulateTransaction = async input => {
   );
 
   const { transaction, simulation } = response.data;
-  console.info(
-    'cover.setProducts input:\n',
-    inspect(transaction.transaction_info.call_trace.decoded_input[0].value, { depth: null }),
-  );
+  const [{ value: decodedTxInputs }] = transaction.transaction_info.call_trace.decoded_input;
+  console.info('cover.setProducts input:\n', inspect(decodedTxInputs, { depth: null }));
   console.info(
     '\nTenderly Simulated transaction:\n',
     `https://dashboard.tenderly.co/NexusMutual/nexusmutual/simulator/${simulation.id}`,
   );
+
+  return decodedTxInputs;
+};
+
+/**
+ * ETH,DAI,USDC - i.e. all cover assets defaults to 0 (see: verifyDecodedTxInputs)
+ */
+const COVER_ASSETS_ID_MAPPING = {
+  ETH: 1,
+  DAI: 2,
+  USDC: 32,
+  'ETH,USDC': 65,
+  'USDC,ETH': 65,
+  'DAI,USDC': 66,
+  'USDC,DAI': 66,
+};
+
+const verifyDecodedTxInputs = (csvProductData, decodedTxInputs) => {
+  for (const csvProduct of csvProductData) {
+    // Find matching object in decodedTxInputs by 'Product Name'
+    const productName = csvProduct['Product Name'];
+    const decodedProduct = decodedTxInputs.find(decoded => decoded.productName === productName);
+
+    if (!decodedProduct) {
+      console.log(`\nVerification failed. No matching product found for: ${productName}`);
+      return;
+    }
+
+    const { productType, coverAssets, initialPriceRatio, capacityReductionRatio } = decodedProduct.product;
+
+    // Set csv default values
+    const csvCoverAssetsId = COVER_ASSETS_ID_MAPPING[csvProduct['Cover Assets']] ?? 0; // default to 0 (all coverAssets)
+    const csvProductId = csvProduct['Product Id'] || MaxUint256.toString();
+    const csvUseFixedPrice = csvProduct['Use fixed price'].trim() === 'TRUE';
+    const csvIsDeprecated = csvProduct['Is deprecated'].trim() === 'TRUE';
+    const csvAllowedPools = csvProduct['Allowed Pools']
+      ? csvProduct['Allowed Pools']?.split(',').map(pool => pool.trim())
+      : [];
+
+    // Verify and match properties
+    if (decodedProduct.productId !== csvProductId) {
+      console.log(decodedProduct.productId, csvProductId);
+      throw new Error(`Product Id mismatch for: ${productName}`);
+    }
+
+    if (productType !== parseInt(csvProduct['Product Type'], 10)) {
+      console.log(productType, parseInt(csvProduct['Product Type'], 10));
+      throw new Error(`Product Type mismatch for: ${productName}, productType`);
+    }
+
+    if (coverAssets !== csvCoverAssetsId) {
+      console.log(coverAssets, csvCoverAssetsId);
+      throw new Error(`Cover Assets mismatch for: ${productName}`);
+    }
+
+    if (initialPriceRatio !== parseInt(parseFloat(csvProduct['Initial Price Ratio']) * 100)) {
+      console.log(initialPriceRatio, parseFloat(csvProduct['Initial Price Ratio']) * 100);
+      throw new Error(`Initial Price Ratio mismatch for: ${productName}`);
+    }
+
+    if (decodedProduct.product.useFixedPrice !== csvUseFixedPrice) {
+      console.log(decodedProduct.product.useFixedPrice, csvUseFixedPrice);
+      throw new Error(`Use Fixed Price mismatch for: ${productName}`);
+    }
+
+    if (decodedProduct.product.isDeprecated !== csvIsDeprecated) {
+      console.log(decodedProduct.product.isDeprecated, csvIsDeprecated);
+      throw new Error(`Is Deprecated mismatch for: ${productName}`);
+    }
+
+    if (JSON.stringify(decodedProduct.allowedPools) !== JSON.stringify(csvAllowedPools)) {
+      console.log(decodedProduct.allowedPools, csvAllowedPools);
+      console.log(JSON.stringify(decodedProduct.allowedPools), JSON.stringify(csvAllowedPools));
+      throw new Error(`Allowed Pools mismatch for: ${productName}`);
+    }
+
+    if (capacityReductionRatio !== parseFloat(csvProduct['Capacity Reduction Ratio']) * 100) {
+      console.log(capacityReductionRatio, parseFloat(csvProduct['Capacity Reduction Ratio']) * 100);
+      throw new Error(`Capacity Reduction Ratio mismatch for: ${productName}`);
+    }
+  }
+
+  console.info('\nSuccessfully verified all csv data matches decoded simulated tx inputs');
 };
 
 const retryUpload = async (filePath, retries = 3) => {
@@ -79,7 +160,7 @@ const retryUpload = async (filePath, retries = 3) => {
  * @param productsDataFilePath path for file of products
  * @returns {Promise<{setProductsTransaction: *}>}
  */
-const main = async (provider, productsDataFilePath) => {
+const main = async productsDataFilePath => {
   const cover = await ethers.getContractAt('Cover', COVER_PROXY_ADDRESS);
   const productData = csvParse(fs.readFileSync(productsDataFilePath, 'utf8'), {
     columns: true,
@@ -95,8 +176,8 @@ const main = async (provider, productsDataFilePath) => {
         (coverAssetsAsText === 'DAI' && 0b10) || // DAI only (2)
         (coverAssetsAsText === 'ETH' && 0b01) || // ETH only (1)
         (coverAssetsAsText === 'USDC' && 0b100000) || // USDC only (32)
-        (coverAssetsAsText === 'DAI,USDC' && 0b1000010) || // DAI & USDC (66)
-        (coverAssetsAsText === 'ETH,USDC' && 0b1000001) || // ETH & USDC (65)
+        ((coverAssetsAsText === 'DAI,USDC' || coverAssetsAsText === 'USDC,DAI') && 0b1000010) || // DAI & USDC (66)
+        ((coverAssetsAsText === 'ETH,USDC' || coverAssetsAsText === 'USDC,ETH') && 0b1000001) || // ETH & USDC (65)
         0; // The default is 0 - this means all assets are allowed (no whitelist)
 
       const annexPath = data.Annex;
@@ -149,14 +230,16 @@ const main = async (provider, productsDataFilePath) => {
   console.log('Tx input: ', productEntries);
 
   const setProductsTransaction = await cover.populateTransaction.setProducts(productEntries);
+  console.log(`Tx data:\n${setProductsTransaction.data}`);
 
-  await simulateTransaction(setProductsTransaction.data);
+  const decodedTxInputs = await simulateTransaction(setProductsTransaction.data);
+  verifyDecodedTxInputs(productData, decodedTxInputs);
 
   return setProductsTransaction;
 };
 
 if (require.main === module) {
-  main(ethers.provider, process.argv[2]).catch(e => {
+  main(process.argv[2]).catch(e => {
     console.log('Unhandled error encountered: ', e.stack);
     process.exit(1);
   });

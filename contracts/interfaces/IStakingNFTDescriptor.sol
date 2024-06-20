e2dd99f5443ba63e16e93e1007c0a67cec474323
shark0der <commits@shark0der.com>
2024-06-20 19:30:08 +0300

    Move most structs to interfaces

--- contracts/interfaces/IStakingNFTDescriptor.sol
+++ contracts/interfaces/IStakingNFTDescriptor.sol
@@ -4,6 +4,12 @@ pragma solidity >=0.5.0;
 
 interface IStakingNFTDescriptor {
 
+  struct StakeData {
+    uint poolId;
+    uint stakeAmount;
+    uint tokenId;
+  }
+
   function tokenURI(uint tokenId) external view returns (string memory);
 
 }

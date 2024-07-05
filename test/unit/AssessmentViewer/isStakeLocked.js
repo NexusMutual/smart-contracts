774b1524eeb8106d0994f4001a3a79ce414efd1f
Rocky Murdoch <rockymurdoch.tech@gmail.com>
2024-07-05 10:11:19 +0300

    Rename AssessmentMock to ASMockAssessment

--- test/unit/AssessmentViewer/isStakeLocked.js
+++ test/unit/AssessmentViewer/isStakeLocked.js
@@ -1,4 +1,4 @@
-const { loadFixture, time } = require('@nomicfoundation/hardhat-network-helpers');
+const { loadFixture } = require('@nomicfoundation/hardhat-network-helpers');
 const { ethers } = require('hardhat');
 const { expect } = require('chai');
 

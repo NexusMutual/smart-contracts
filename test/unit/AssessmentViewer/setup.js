68213d3f0fcdd08c102d86257d1c226cd7c3a035
Rocky Murdoch <rockymurdoch.tech@gmail.com>
2024-07-09 16:54:04 +0300

    Fix isStakeLocked unit tests

--- test/unit/AssessmentViewer/setup.js
+++ test/unit/AssessmentViewer/setup.js
@@ -5,19 +5,17 @@ const { getAccounts } = require('../utils').accounts;
 
 async function setup() {
   const accounts = await getAccounts();
-  const nxm = await ethers.deployContract('NXMTokenMock');
   const master = await ethers.deployContract('MasterMock');
   const stakeLockupPeriodInDays = 2;
   const assessment = await ethers.deployContract('ASMockAssessment', [1, stakeLockupPeriodInDays, 3, 4]);
 
   await master.setLatestAddress(hex('AS'), assessment.address);
 
-  const assessmentViewer = await ethers.deployContract('AssessmentViewer', [master.address, nxm.address]);
+  const assessmentViewer = await ethers.deployContract('AssessmentViewer', [master.address]);
 
   return {
     accounts,
     contracts: {
-      nxm,
       assessment,
       assessmentViewer,
     },

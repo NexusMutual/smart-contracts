dd465ba3d378f62f58b607202cfdea03c638883e
Rocky Murdoch <rockymurdoch.tech@gmail.com>
2024-07-10 11:11:39 +0300

    Update ASMockAssessment

--- contracts/mocks/modules/Assessment/ASMockAssessment.sol
+++ contracts/mocks/modules/Assessment/ASMockAssessment.sol
@@ -8,6 +8,9 @@ contract ASMockAssessment is IAssessment {
   Configuration public override config;
   mapping(address => Stake) public override stakeOf;
   mapping(address => Vote[]) public override votesOf;
+  uint totalPendingAmountInNXM;
+  uint withdrawableAmountInNXM;
+  uint withdrawableUntilIndex;
 
   constructor(
     uint8 minVotingPeriodInDays,
@@ -29,67 +32,92 @@ contract ASMockAssessment is IAssessment {
     votesOf[assessor].push(Vote(uint80(assessmentId), isAcceptVote, uint32(block.timestamp), stakeAmount));
   }
 
+  function setStakeOf(
+    address assessor,
+    uint96 amount,
+    uint104 rewardsWithdrawableFromIndex,
+    uint16 fraudCount
+  ) external {
+    stakeOf[assessor] = Stake({
+      amount: amount,
+      rewardsWithdrawableFromIndex: rewardsWithdrawableFromIndex,
+      fraudCount: fraudCount
+    });
+  }
+
+  function setRewards(
+    uint _totalPendingAmountInNXM,
+    uint _withdrawableAmountInNXM,
+    uint _withdrawableUntilIndex
+  ) external {
+    totalPendingAmountInNXM = _totalPendingAmountInNXM;
+    withdrawableAmountInNXM = _withdrawableAmountInNXM;
+    withdrawableUntilIndex = _withdrawableUntilIndex;
+  }
+
   /* ========== VIEWS ========== */
 
   function getVoteCountOfAssessor(address assessor) external view override returns (uint) {
     return votesOf[assessor].length;
   }
 
+  function getRewards(address) external view override returns (uint, uint, uint) {
+    return (totalPendingAmountInNXM, withdrawableAmountInNXM, withdrawableUntilIndex);
+  }
+
+  /* ========== NOT YET IMPLEMENTED ========== */
+
   function getAssessmentsCount() external pure override returns (uint) {
-    revert("Not yet implemented");
+    revert("getAssessmentsCount not yet implemented");
   }
 
   function assessments(uint) external pure override returns (Poll memory, uint128, uint128) {
-    revert("Not yet implemented");
+    revert("assessments not yet implemented");
   }
 
   function getPoll(uint) external pure override returns (Poll memory) {
-    revert("Not yet implemented");
-  }
-
-  function getRewards(address) external pure override returns (uint, uint, uint) {
-    revert("Not yet implemented");
+    revert("getPoll not yet implemented");
   }
 
   function hasAlreadyVotedOn(address, uint) external pure override returns (bool) {
-    revert("Not yet implemented");
+    revert("hasAlreadyVotedOn not yet implemented");
   }
 
   /* === MUTATIVE FUNCTIONS ==== */
 
   function stake(uint96) external pure override {
-    revert("Not yet implemented");
+    revert("stake not yet implemented");
   }
 
   function unstake(uint96, address) external pure override {
-    revert("Not yet implemented");
+    revert("unstake not yet implemented");
   }
 
   function withdrawRewards(address, uint104) external pure override returns (uint, uint) {
-    revert("Not yet implemented");
+    revert("withdrawRewards not yet implemented");
   }
 
   function withdrawRewardsTo(address, uint104) external pure override returns (uint, uint) {
-    revert("Not yet implemented");
+    revert("withdrawRewardsTo not yet implemented");
   }
 
   function startAssessment(uint, uint) external pure override returns (uint) {
-    revert("Not yet implemented");
+    revert("startAssessment not yet implemented");
   }
 
   function castVotes(uint[] calldata, bool[] calldata, string[] calldata, uint96) external pure override {
-    revert("Not yet implemented");
+    revert("castVotes not yet implemented");
   }
 
   function submitFraud(bytes32) external pure override {
-    revert("Not yet implemented");
+    revert("submitFraud not yet implemented");
   }
 
   function processFraud(uint256, bytes32[] calldata, address, uint256, uint96, uint16, uint256) external pure override {
-    revert("Not yet implemented");
+    revert("processFraud not yet implemented");
   }
 
   function updateUintParameters(UintParams[] calldata, uint[] calldata) external pure override {
-    revert("Not yet implemented");
+    revert("updateUintParameters not yet implemented");
   }
 }

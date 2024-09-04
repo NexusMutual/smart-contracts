00a190b2b7cd5ad7ce0a50458707db88a735d0d6
Rocky Murdoch <rockymurdoch.tech@gmail.com>
2024-09-04 13:33:21 +0300

    Add v1-nxm-coordinator script

--- scripts/v1-nxm/v1-nxm-coordinator.js
+++ scripts/v1-nxm/v1-nxm-coordinator.js
@@ -1,23 +1,26 @@
 require('dotenv').config();
-const { ethers } = require('ethers');
-const { Sema } = require('async-sema');
-const fs = require('fs/promises');
+const fs = require('node:fs/promises');
+const util = require('node:util');
+
 const { addresses, TokenController, LegacyPooledStaking } = require('@nexusmutual/deployments');
+const { Sema } = require('async-sema');
+const { ethers } = require('ethers');
+
 const { pushCoverNotes, pushClaimsAssessment, pushV1StakingStake, pushV1StakingRewards } = require('./v1-nxm-push');
 
 const PROGRESS_FILE = 'v1-nxm-progress.json';
 
-async function getGasFees(provider, maxGasFee, priorityFee) {
+const waitFor = util.promisify(setTimeout);
+
+async function getGasFees(provider, priorityFee) {
   const { baseFeePerGas } = await provider.getBlock('pending');
   if (!baseFeePerGas) {
     throw new Error('Failed to get baseFeePerGas. Please try again');
   }
   const priorityFeeWei = ethers.utils.parseUnits(priorityFee.toString(), 'gwei');
-  const maxFeeWei = ethers.utils.parseUnits(maxGasFee.toString(), 'gwei');
-  return {
-    maxFeePerGas: maxFeeWei,
-    maxPriorityFeePerGas: priorityFeeWei,
-  };
+  const maxFeePerGas = baseFeePerGas.add(priorityFeeWei);
+
+  return maxFeePerGas;
 }
 
 async function loadProgress() {
@@ -29,57 +32,63 @@ async function loadProgress() {
   }
 }
 
-async function saveProgress(progress) {
-  await fs.writeFile(PROGRESS_FILE, JSON.stringify(progress, null, 2));
-}
-
-async function processV1NXM(provider, maxGasFee, priorityFee, txPerBlock) {
+async function processV1NXM(provider, userMaxFeePerGasGwei, priorityFeeGwei, txPerBlock) {
+  const userMaxFeePerGas = ethers.utils.parseUnits(userMaxFeePerGasGwei.toString(), 'gwei');
   // TODO: use AWS KMS signer
   const signer = provider.getSigner('0x87B2a7559d85f4653f13E6546A14189cd5455d45');
   const tc = new ethers.Contract(addresses.TokenController, TokenController, signer);
   const ps = new ethers.Contract(addresses.LegacyPooledStaking, LegacyPooledStaking, signer);
 
   const types = [
+    // { name: 'ClaimsAssessment', data: require('../../v1-cla-locked-amount.json'), func: pushClaimsAssessment },
+    // { name: 'StakingStake', data: require('../../v1-pooled-staking-stake.json'), func: pushV1StakingStake },
+    // { name: 'StakingRewards', data: require('../../v1-pooled-staking-rewards.json'), func: pushV1StakingRewards },
     { name: 'CoverNotes', data: require('../../v1-cn-locked-amount.json'), func: pushCoverNotes },
-    { name: 'ClaimsAssessment', data: require('../../v1-cla-locked-amount.json'), func: pushClaimsAssessment },
-    { name: 'StakingStake', data: require('../../v1-pooled-staking-stake.json'), func: pushV1StakingStake },
-    { name: 'StakingRewards', data: require('../../v1-pooled-staking-rewards.json'), func: pushV1StakingRewards },
   ];
 
-  let progress = await loadProgress();
+  const progress = await loadProgress();
 
   for (const type of types) {
+    console.log('type.name: ', type.name);
     progress[type.name] ||= { processedCount: 0 };
     const remainingData = type.data.slice(progress[type.name].processedCount);
+    console.log('remainingData: ', remainingData);
+    const totalData = type.data.length;
+    let counter = progress[type.name].processedCount;
 
     while (remainingData.length > 0) {
-      const gasFees = await getGasFees(provider, maxGasFee, priorityFee);
-      if (gasFees.maxFeePerGas.gt(ethers.utils.parseUnits(maxGasFee.toString(), 'gwei'))) {
-        console.log('Gas fee too high. Waiting for next block...');
-        await new Promise(resolve => setTimeout(resolve, 15000)); // Wait for ~15 seconds (average Ethereum block time)
+      counter += Math.min(txPerBlock, counter - progress[type.name].processedCount);
+      process.stdout.write(`\r[${type.name}] Processing members ${counter} of ${totalData}`);
+
+      const maxFeePerGas = await getGasFees(provider, priorityFeeGwei);
+      if (maxFeePerGas.gt(userMaxFeePerGas)) {
+        console.log('Gas fee too high. Waiting for next block...', {
+          maxFeePerGas: ethers.utils.formatUnits(maxFeePerGas, 'gwei') + ' gwei',
+          userMaxFeePerGas: ethers.utils.formatUnits(userMaxFeePerGas, 'gwei') + ' gwei',
+        });
+        await waitFor(15000);
         continue;
       }
 
       const batch = remainingData.slice(0, txPerBlock);
-      const semaphore = new Sema(txPerBlock);
 
-      const promises = batch.map(async (item) => {
-        await semaphore.acquire();
-        try {
-          await type.func({ tc, ps }, [item], txPerBlock);
-        } finally {
-          semaphore.release();
+      try {
+        if (type.name === 'ClaimsAssessment') {
+          await type.func({ tc, ps }, batch, type.data.length);
+          progress[type.name].processedCount += batch.length;
+        } else {
+          const promises = batch.map(item => type.func({ tc, ps }, item));
+          await Promise.all(promises);
+          progress[type.name].processedCount += batch.length;
         }
-      });
 
-      await Promise.all(promises);
-      
-      // Update progress
-      progress[type.name].processedCount += batch.length;
-      
-      await saveProgress(progress);
-      
-      // splice is not performant, but the data is small so its fine (prefer readability)
+        // Save progress after processing the batch
+        await fs.writeFile(PROGRESS_FILE, JSON.stringify(progress, null, 2));
+      } catch (error) {
+        console.error(`Error processing ${type.name}:`, error);
+        throw error; // This will stop the process
+      }
+
       remainingData.splice(0, txPerBlock);
     }
   }

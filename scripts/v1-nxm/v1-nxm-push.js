33f8912110b4afd76d1fd9895121cccf6111fd6b
Rocky Murdoch <rockymurdoch.tech@gmail.com>
2024-09-04 20:13:15 +0300

    Code clean up

--- scripts/v1-nxm/v1-nxm-push.js
+++ scripts/v1-nxm/v1-nxm-push.js
@@ -13,10 +13,16 @@ const {
   pushV1StakingRewards,
 } = require('./v1-nxm-push-utils');
 
+const types = [
+  { name: 'ClaimsAssessment', data: require('../../v1-cla-locked-amount.json'), func: pushClaimsAssessment },
+  { name: 'StakingStake', data: require('../../v1-pooled-staking-stake.json'), func: pushV1StakingStake },
+  { name: 'StakingRewards', data: require('../../v1-pooled-staking-rewards.json'), func: pushV1StakingRewards },
+  { name: 'CoverNotes', data: require('../../v1-cn-locked-amount.json'), func: pushCoverNotes },
+];
+
 /**
  * Processes V1 NXM push tokens for different types (ClaimsAssessment, StakingStake, StakingRewards, CoverNotes).
  *
- * @param {ethers.providers.JsonRpcProvider} provider - Ethereum provider
  * @param {number} userMaxFeePerGasGwei - Maximum gas fee user is willing to pay (in Gwei)
  * @param {number} priorityFeeGwei - Priority fee (in Gwei)
  * @param {number} txPerBlock - Number of transactions to process per block
@@ -26,18 +32,14 @@ const {
  * - Batches transactions for efficiency
  * - Tracks progress and allows resuming from last processed item (in case of any tx errors)
  */
-async function processV1NXM(provider, userMaxFeePerGasGwei, priorityFeeGwei, txPerBlock) {
+async function processV1NXM(userMaxFeePerGasGwei, priorityFeeGwei, txPerBlock) {
   const userMaxFeePerGas = ethers.utils.parseUnits(userMaxFeePerGasGwei.toString(), 'gwei');
+
+  const provider = new ethers.providers.JsonRpcProvider(process.env.TEST_ENV_FORK);
   const signer = new ethers.Wallet(process.env.WALLET_PK, provider);
+
   const tc = getContract('TokenController', signer);
   const ps = getContract('LegacyPooledStaking', signer);
-
-  const types = [
-    { name: 'ClaimsAssessment', data: require('../../v1-cla-locked-amount.json'), func: pushClaimsAssessment },
-    { name: 'StakingStake', data: require('../../v1-pooled-staking-stake.json'), func: pushV1StakingStake },
-    { name: 'StakingRewards', data: require('../../v1-pooled-staking-rewards.json'), func: pushV1StakingRewards },
-    { name: 'CoverNotes', data: require('../../v1-cn-locked-amount.json'), func: pushCoverNotes },
-  ];
 
   const progress = await loadProgress();
 
@@ -98,9 +100,8 @@ if (require.main === module) {
   }
 
   const [maxGasFeeGwei, priorityFeeGwei, txPerBlock] = args.map(Number);
-  const provider = new ethers.providers.JsonRpcProvider(process.env.TEST_ENV_FORK);
 
-  processV1NXM(provider, maxGasFeeGwei, priorityFeeGwei, txPerBlock)
+  processV1NXM(maxGasFeeGwei, priorityFeeGwei, txPerBlock)
     .then(() => process.exit(0))
     .catch(error => {
       console.error('Unhandled error:', error);

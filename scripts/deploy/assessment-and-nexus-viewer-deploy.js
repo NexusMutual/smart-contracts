1c624c33d2060f4c1ffecd5e545cebf657b4612a
mixplore <9848598+mixplore@users.noreply.github.com>
2024-10-21 17:10:54 +0300

    chore: add assessment and nexus viewer contracts deploy script for testing

---
+++ scripts/deploy/assessment-and-nexus-viewer-deploy.js
@@ -0,0 +1,28 @@
+const { ethers, network } = require('hardhat');
+
+const STV = '0xcafea5E8a7a54dd14Bb225b66C7a016dfd7F236b'; // StakingViewer
+const MS = '0x01BFd82675DBCc7762C84019cA518e701C0cD07e'; // NXMaster
+
+const main = async () => {
+  console.log(`Starting deploy script on ${network.name} network`);
+
+  console.log('Getting a signer');
+  const [signer] = await ethers.getSigners();
+
+  console.log('Deploying contracts');
+  const assessmentViewerImplementation = await ethers.deployContract('AssessmentViewer', [MS], signer);
+  const nexusViewerImplementation = await ethers.deployContract(
+    'NexusViewer',
+    [MS, STV, assessmentViewerImplementation.address],
+    signer,
+  );
+
+  console.log('AssessmentViewer implementation address:', assessmentViewerImplementation.address);
+  console.log('NexusViewer implementation address:', nexusViewerImplementation.address);
+};
+main()
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error);
+    process.exit(1);
+  });

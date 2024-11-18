1f44def3308b95b9caec113998a573d1647d9564
Rocky Murdoch <rockymurdoch.tech@gmail.com>
2024-11-18 16:48:57 +0200

    docs: add PROPOSAL_CATEGORY constant annotations

--- scripts/governance/constants.js
+++ scripts/governance/constants.js
@@ -3,6 +3,10 @@ const nexusSdk = require('@nexusmutual/deployments');
 const AB_MEMBER = '0x87B2a7559d85f4653f13E6546A14189cd5455d45';
 const GOVERNANCE_ADDRESS = nexusSdk.addresses.Governance;
 const IPFS_API_URL = 'https://api.nexusmutual.io/ipfs-api/api/v0';
+
+/**
+ * @dev Extend with other Governance Proposal Categories as necessary
+ */
 const PROPOSAL_CATEGORY = {
   4: {
     actionParamTypes: [

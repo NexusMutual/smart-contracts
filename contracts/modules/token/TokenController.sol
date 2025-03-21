ed862e75ab484eeb267611de70afd94ec3dde7cc
DuXXuD <duxxud@gmail.com>
2025-03-21 15:33:12 +0100

    chore: change solidity requirement to ^0.8.28

--- contracts/modules/token/TokenController.sol
+++ contracts/modules/token/TokenController.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-3.0-only
 
-pragma solidity ^0.8.27;
+pragma solidity ^0.8.28;
 
 import "../../interfaces/IAssessment.sol";
 import "../../interfaces/IGovernance.sol";

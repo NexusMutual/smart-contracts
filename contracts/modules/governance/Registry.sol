dce1722c5807c55494a837515567425285d8560f
shark0der <commits@shark0der.com>
2025-05-16 15:44:37 +0300

    --wip-- [skip ci]

--- contracts/modules/governance/Registry.sol
+++ contracts/modules/governance/Registry.sol
@@ -7,27 +7,34 @@ import "./UpgradeableProxy.sol";
 
 contract Registry is IRegistry {
 
-  // contracts
-
-  // index = 1 << code
-  mapping(uint index => address contractAddress) internal contractAddresses;
-  mapping(address contractAddress => uint index) internal contractIndexes;
-
-  // pause
-
-  uint constant PAUSE_GLOBAL = 1; // 0b0000000000000001
-  uint constant PAUSE_RAMM = 2;   // 0b0000000000000010
-  uint constant PAUSE_X = 4;      // 0b0000000000000100
-  uint constant PAUSE_Y = 8;      // 0b0000000000001000
-
   struct SystemPause {
+    // could consider switching to uint48
     uint config;
     uint proposedConfig;
     address proposer;
   }
 
+  // index = 1 << code
+  mapping(uint index => address contractAddress) internal contractAddresses;
+  mapping(address contractAddress => uint index) internal contractIndexes;
+
+  // consider changing to uint32 or similar
+  // could shove into a struct
+  uint public memberCount;
+  uint public lastMemberId;
+
+  mapping(uint memberId => address member) internal members;
+  mapping(address member => uint memberId) internal memberIds;
+  mapping(bytes32 signature => bool used) internal usedSignatures;
+  mapping(uint memberId => bool isAdvisoryBoardMember) public isAdvisoryBoardMember;
+
   mapping(address => bool) isEmergencyAdmin; // 1 slot
   SystemPause internal systemPause; // 3 slots
+
+  uint constant PAUSE_GLOBAL = 1; // 0b0000000000000001
+  uint constant PAUSE_RAMM = 2;   // 0b0000000000000010
+  uint constant PAUSE_X = 4;      // 0b0000000000000100
+  uint constant PAUSE_Y = 8;      // 0b0000000000001000
 
   function proposePauseConfig(uint config) external {
     require(isEmergencyAdmin[msg.sender], NotEmergencyAdmin());
@@ -49,25 +56,18 @@ contract Registry is IRegistry {
     // emit event
   }
 
-  function getPauseConfig() external view returns (SystemPause memory pauseConfig) {
+  function getSystemPause() external view returns (SystemPause memory) {
     return systemPause;
+  }
+
+  function getPauseConfig() external view returns (uint config) {
+    return systemPause.config;
   }
 
   function isPaused(uint mask) external view returns (bool) {
     uint config = systemPause.config;
     return (PAUSE_GLOBAL & config) != 0 || (config & mask) != 0;
   }
-
-  // membership
-
-  uint public memberCount;
-  uint public lastMemberId;
-
-  /// WITHOUT DESIGNATED ROLES
-
-  mapping(uint memberId => address member) internal members;
-  mapping(address member => uint memberId) internal memberIds;
-  mapping(uint memberId => bool isAdvisoryBoardMember) public isAdvisoryBoardMember;
 
   function isMember(address member) external view returns (bool) {
     return memberIds[member] != 0;
@@ -93,15 +93,20 @@ contract Registry is IRegistry {
     memberIds[member] = memberId;
     members[memberId] = member;
     // todo:
+    // validate signature
+    // mark signature as used
     // TC.addToWhitelist(member)
   }
 
   function swap(address to) external {
-    require(memberIds[to] == 0, AlreadyMember());
     uint memberId = memberIds[msg.sender];
-    members[memberId] = to;
-    memberIds[to] = memberId;
+    require(memberId != 0, NotMember());
+    require(memberIds[to] == 0, AlreadyMember());
+
     delete memberIds[msg.sender];
+    memberIds[to] = memberId;
+    members[memberId] = to;
+
     // todo:
     // TC.removeFromWhitelist(msg.sender)
     // TC.addToWhitelist(to)
@@ -113,6 +118,7 @@ contract Registry is IRegistry {
     uint memberId = memberIds[msg.sender];
     require(memberId != 0, NotMember());
     require(!isAdvisoryBoardMember[memberId], AdvisoryBoardMemberCannotLeave());
+
     // todo:
     // address[] memory pools = TC.getManagerStakingPools(memberId)
     // require(pools.length == 0, StakingPoolManagersCannotLeave());
@@ -122,78 +128,14 @@ contract Registry is IRegistry {
 
     // todo:
     // TC.removeFromWhitelist(msg.sender)
-    // TK.burnFrom(msg.sender)
-  }
-
-  /// WITH DESIGNATED ROLES
-
-  struct AddressInfo {
-    uint32 id;     // member id
-    uint8 role;    // role id
-  }
-
-  struct MemberRole {
-    uint32 id;
-    uint8 role;
-    address addr;
-  }
-
-  mapping(uint id => mapping(uint role => address)) internal roleAddresses;
-  mapping(address => AddressInfo) internal addressToInfo;
-  mapping(uint id => mapping(uint role => address nominee)) internal roleOffers;
-
-  /* == MEMBER MANAGEMENT == */
-
-  function getAddress(uint id, uint role) external view returns (address) {
-    return roleAddresses[id][role];
-  }
-
-  function getMemberAddresses(uint id) external view returns (MemberRole[] memory memberRoles) {
-    memberRoles = new MemberRole[](256);
-
-    for (uint role = 1; role <= 256; role++) {
-      AddressInfo memory info = addressToInfo[roleAddresses[id][role]];
-      memberRoles[role - 1] = MemberRole({
-        id: info.id,
-        role: info.role,
-        addr: roleAddresses[id][role]
-      });
-    }
-
-    return memberRoles;
-  }
-
-  function nominateAddress(uint role, address nominee) external {
-    AddressInfo memory info = addressToInfo[msg.sender];
-    require(info.id != 0, NotMember());
-    require(info.role == 1, NotMainAddress());
-
-    require(role > 0 && role <= 256, InvalidRole());
-    roleOffers[info.id][role] = nominee;
-  }
-
-  function acceptNomination(uint memberId, uint role) external {
-
-    require(memberId != 0, InvalidMemberId());
-    require(role > 0 && role <= 256, InvalidRole());
-
-    address nominee = roleOffers[memberId][role];
-    require(nominee == msg.sender, NotNominee());
-
-    roleAddresses[memberId][role] = nominee;
-    addressToInfo[nominee] = AddressInfo({ id: memberId, role: role });
-
-    delete roleOffers[memberId][role];
-  }
-
-  function getMember(address addr) external view returns (uint id, uint role) {
-    AddressInfo memory info = addressToInfo[addr];
-    return (info.id, info.role);
+    // uint balance = TK.balanceOf(msg.sender)
+    // TK.burnFrom(msg.sender, balance) // or revert?
   }
 
   /* == CONTRACT MANAGEMENT == */
 
   function addContract(uint code, bytes32 salt, address implementation) external {
+    require(code <= type(uint8).max, InvalidContractCode());
     require(contractAddresses[1 << code] == address(0), ContractAlreadyExists());
     UpgradeableProxy proxy = new UpgradeableProxy{salt: bytes32(salt)}();
     proxy.upgradeTo(implementation);
@@ -202,7 +144,7 @@ contract Registry is IRegistry {
   }
 
   function upgradeContract(uint code, address implementation) external {
-    require(code <= uint(type(Contract).max), InvalidContractCode());
+    require(code <= type(uint8).max, InvalidContractCode());
     address contractAddress = contractAddresses[1 << code];
     require(contractAddress != address(0), ContractDoesNotExist());
     UpgradeableProxy proxy = UpgradeableProxy(payable(contractAddress));
@@ -210,7 +152,7 @@ contract Registry is IRegistry {
   }
 
   function deprecateContract(uint code) external {
-    require(code <= uint(type(Contract).max), InvalidContractCode());
+    require(code <= type(uint8).max, InvalidContractCode());
     address contractAddress = contractAddresses[1 << code];
     require(contractAddress != address(0), ContractDoesNotExist());
     contractIndexes[contractAddress] = 0;
@@ -218,14 +160,14 @@ contract Registry is IRegistry {
   }
 
   function getContractByCode(uint code) external view returns (address) {
-    require(code <= uint(type(Contract).max), InvalidContractCode());
+    require(code <= type(uint8).max, InvalidContractCode());
     return contractAddresses[1 << code];
   }
 
   function getContractsByCode(uint[] memory codes) external view returns (address[] memory addresses) {
     addresses = new address[](codes.length);
     for (uint i = 0; i < codes.length; i++) {
-      require(codes[i] <= uint(type(Contract).max), InvalidContractCode());
+      require(codes[i] <= type(uint8).max, InvalidContractCode());
       addresses[i] = contractAddresses[1 << codes[i]];
     }
   }

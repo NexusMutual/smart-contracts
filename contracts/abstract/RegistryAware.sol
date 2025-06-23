dcee347211ea304b7becbf182724023ee693ea7c
DuXXuD <duxxud@gmail.com>
2025-06-23 12:30:11 +0200

    feat: separate early closing, add draw outcome, fix structs, fix events and errors

--- contracts/abstract/RegistryAware.sol
+++ contracts/abstract/RegistryAware.sol
@@ -7,29 +7,30 @@ pragma solidity ^0.8.28;
 import "../interfaces/IRegistry.sol";
 
 // contract indexes
-uint constant C_REGISTRY         = 1;
-uint constant C_GOVERNOR         = 2;
-uint constant C_TOKEN            = 4;
-uint constant C_TOKEN_CONTROLLER = 8;
-uint constant C_POOL             = 16;
-uint constant C_MEMBER_ROLES     = 32;
-uint constant C_COVER            = 64;
-uint constant C_COVER_PRODUCTS   = 128;
-uint constant C_STAKING_PRODUCTS = 256;
-uint constant C_RAMM             = 512;
-uint constant C_SAFE_TRACKER     = 1024;
-uint constant C_LIMIT_ORDERS     = 2048;
-uint constant C_STAKING_NFT      = 4096;
-uint constant C_COVER_NFT        = 8192;
-uint constant C_SWAP_OPERATOR    = 16384;
-uint constant C_ASSESSMENT       = 32768;
-uint constant C_CLAIMS           = 65536;
+uint constant C_REGISTRY          = 1;
+uint constant C_GOVERNOR          = 2;
+uint constant C_TOKEN             = 4;
+uint constant C_TOKEN_CONTROLLER  = 8;
+uint constant C_POOL              = 16;
+uint constant C_MEMBER_ROLES      = 32;
+uint constant C_COVER             = 64;
+uint constant C_COVER_PRODUCTS    = 128;
+uint constant C_STAKING_PRODUCTS  = 256;
+uint constant C_RAMM              = 512;
+uint constant C_SAFE_TRACKER      = 1024;
+uint constant C_LIMIT_ORDERS      = 2048;
+uint constant C_STAKING_NFT       = 4096;
+uint constant C_COVER_NFT         = 8192;
+uint constant C_SWAP_OPERATOR     = 16384;
+uint constant C_ASSESSMENT        = 32768;
+uint constant C_CLAIMS            = 65536;
 
 // pause types constants
-uint constant PAUSE_GLOBAL       = 1;
-uint constant PAUSE_RAMM         = 2;
-uint constant PAUSE_SWAPS        = 4;
-uint constant PAUSE_ASSESSMENTS  = 8;
+uint constant PAUSE_GLOBAL        = 1;
+uint constant PAUSE_RAMM          = 2;
+uint constant PAUSE_SWAPS         = 4;
+uint constant PAUSE_ASSESSMENTS   = 8;
+uint constant PAUSE_CLAIMS_PAYOUT = 16;
 
 contract RegistryAware {
 

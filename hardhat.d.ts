988ec2966891f2efaad504abcf8a7197c38d2da0
shark0der <commits@shark0der.com>
2025-07-11 13:24:25 +0300

    chore: move hardhat.d.ts to root folder and add tsconfig.json so IDEs are aware of hre.nexus

--- hardhat.d.ts
+++ hardhat.d.ts
@@ -2,6 +2,6 @@ import "hardhat/types/runtime";
 
 declare module "hardhat/types/runtime" {
   interface HardhatRuntimeEnvironment {
-    nexus: typeof import("../lib/index.js");
+    nexus: typeof import("./lib/index.js");
   }
 }

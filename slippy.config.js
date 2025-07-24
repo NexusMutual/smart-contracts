70ddea9e91147e99561c7a2b45edc35d475809fd
shark0der <commits@shark0der.com>
2025-07-24 17:29:16 +0300

    chore: add slippy solidity linter

---
+++ slippy.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  rules: {
+    'imports-on-top': 'error',
+    'naming-convention': [
+      'error',
+      [
+        {
+          selector: 'stateVariable',
+          format: ['UPPER_CASE', 'camelCase'],
+          modifiers: ['constant'],
+        },
+        {
+          selector: 'stateVariable',
+          format: ['UPPER_CASE', 'camelCase'],
+          modifiers: ['immutable'],
+        },
+      ],
+    ],
+    'no-console': 'error',
+    'no-default-visibility': 'error',
+    'no-duplicate-imports': 'error',
+    'no-uninitialized-immutable-references': 'error',
+    'no-unused-vars': 'error',
+    'sort-imports': 'error', // disabled for now
+  },
+};

7f340b2884c13cfdb165a369436a590456ebbe11
Rocky Murdoch <rockymurdoch.tech@gmail.com>
2025-08-04 16:46:29 +0300

    refactor: make sendEth address parameter payable

--- contracts/modules/capital/Pool.sol
+++ contracts/modules/capital/Pool.sol
@@ -283,7 +283,7 @@ contract Pool is IPool, ReentrancyGuard, RegistryAware {
   /// @param member  Member address
   /// @param amount  Amount of ETH to send
   ///
-  function sendEth(address member, uint amount) external override onlyContracts(C_RAMM | C_CLAIMS) nonReentrant {
+  function sendEth(address payable member, uint amount) external override onlyContracts(C_RAMM | C_CLAIMS) nonReentrant {
     (bool transferSucceeded, /* data */) = member.call{value: amount}("");
     require(transferSucceeded, "Pool: ETH transfer failed");
   }

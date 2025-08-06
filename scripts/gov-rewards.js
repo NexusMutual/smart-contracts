218f9d3481f00183c628d4e810e7f1d51921945f
Rocky Murdoch <rockymurdoch.tech@gmail.com>
2025-08-06 15:08:39 +0300

    fix: fix gov-rewards and get-signer scripts

--- scripts/gov-rewards.js
+++ scripts/gov-rewards.js
@@ -1,35 +1,40 @@
 const fs = require('node:fs');
+const path = require('node:path');
 const { ethers } = require('hardhat');
 
 const fetchRewards = async () => {
   const abi = ['function getPendingReward(address member) public view returns (uint)'];
   const governance = await ethers.getContractAt(abi, '0x4A5C681dDC32acC6ccA51ac17e9d461e6be87900');
 
-  const voters = require('./voters.json');
+  const votersPath = path.join(__dirname, 'voters.json');
+  const voters = require(votersPath);
   const rewards = {};
 
   for (const voter of voters) {
     console.log('Fetching rewards for', voter);
     const reward = await governance.getPendingReward(voter);
-
-    if (!reward.isZero()) {
-      rewards[voter] = reward.toString();
-      console.log(`${voter}: ${reward}`);
+    if (reward !== 0n) {
+      const rewardsFormatted = ethers.formatEther(reward);
+      rewards[voter] = rewardsFormatted;
+      console.log(`${voter}: ${rewardsFormatted}`);
     }
   }
 
-  fs.writeFileSync('./gov-rewards.json', JSON.stringify(rewards, null, 2));
+  const govRewardsPath = path.join(__dirname, 'gov-rewards.json');
+  fs.writeFileSync(govRewardsPath, JSON.stringify(rewards, null, 2));
 };
 
 const main = async () => {
-  if (!fs.existsSync('./gov-rewards.json')) {
+  const govRewardsPath = path.join(__dirname, 'gov-rewards.json');
+
+  if (!fs.existsSync(govRewardsPath)) {
     await fetchRewards();
   }
 
-  const rewards = JSON.parse(fs.readFileSync('./gov-rewards.json', 'utf8'));
+  const rewards = JSON.parse(fs.readFileSync(govRewardsPath, 'utf8'));
   const keys = Object.keys(rewards);
-  const total = keys.reduce((acc, key) => acc.add(rewards[key]), ethers.constants.Zero);
-  console.log(`Total rewards: ${ethers.utils.formatEther(total)} NXM`);
+  const total = keys.reduce((acc, key) => acc + Number(rewards[key]), 0);
+  console.log(`Total rewards: ${total} NXM`);
 };
 
 main()

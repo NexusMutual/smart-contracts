4b33685eec6fb71aa32bf250d79f85e0a6cdfdb7
Rocky Murdoch <rockymurdoch.tech@gmail.com>
2026-03-09 10:22:47 +0200

    test: migrate LimitOrders unit tests to ethers v6

---
+++ test/unit/LimitOrders/setup.js
@@ -0,0 +1,54 @@
+const { ethers } = require('hardhat');
+const { toBytes2 } = require('../../../lib/helpers');
+
+const { setEtherBalance } = require('../../utils/evm');
+
+const { ZeroAddress, parseEther } = ethers;
+
+async function setup() {
+  const limitOrderOwner = ethers.Wallet.createRandom().connect(ethers.provider);
+  await setEtherBalance(limitOrderOwner.address, parseEther('1000000'));
+
+  const notOwner = ethers.Wallet.createRandom().connect(ethers.provider);
+  await setEtherBalance(notOwner.address, parseEther('1000000'));
+
+  const limitOrdersSettler = ethers.Wallet.createRandom().connect(ethers.provider);
+  await setEtherBalance(limitOrdersSettler.address, parseEther('1000000'));
+
+  const dai = await ethers.deployContract('ERC20Mock');
+  const memberRoles = await ethers.deployContract('MemberRolesMock');
+  const cover = await ethers.deployContract('LimitOrdersCoverMock');
+  const pool = await ethers.deployContract('PoolMock');
+  const master = await ethers.deployContract('MasterMock');
+
+  const limitOrders = await ethers.deployContract('LimitOrders', [ZeroAddress, ZeroAddress, limitOrdersSettler.address]);
+
+  await master.setLatestAddress(toBytes2('CO'), cover.target);
+  await master.setLatestAddress(toBytes2('MR'), memberRoles.target);
+  await master.setLatestAddress(toBytes2('P1'), pool.target);
+  await master.setLatestAddress(toBytes2('LO'), limitOrders.target);
+
+  await limitOrders.changeMasterAddress(master.target);
+  await limitOrders.changeDependentContractAddress();
+
+  await pool.addAsset({ assetAddress: dai.target, isCoverAsset: true, isAbandoned: false });
+
+  await dai.mint(limitOrderOwner.address, parseEther('1000000'));
+
+  await memberRoles.setRole(limitOrdersSettler.address, 2);
+  await memberRoles.setRole(limitOrderOwner.address, 2);
+
+  return {
+    accounts: {
+      limitOrderOwner,
+      limitOrdersSettler,
+      notOwner,
+    },
+    contracts: {
+      dai,
+      limitOrders,
+    },
+  };
+}
+
+module.exports = { setup };

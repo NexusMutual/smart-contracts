82e237b0f11e12fe77b0e7185806df16abb08b98
Rocky Murdoch <rockymurdoch.tech@gmail.com>
2026-03-09 10:48:26 +0200

    test: migrate StakingNFT and StakingPoolFactory unit tests to ethers v6

---
+++ test/unit/StakingPoolFactory/setup.js
@@ -0,0 +1,17 @@
+const { ethers } = require('hardhat');
+const { getAccounts } = require('../../utils/accounts');
+
+async function setup() {
+  const accounts = await getAccounts();
+  const [operator] = accounts.nonMembers;
+
+  const stakingPoolFactory = await ethers.deployContract('StakingPoolFactory', [operator.address]);
+
+  return {
+    operator,
+    stakingPoolFactory,
+    accounts,
+  };
+}
+
+module.exports = setup;

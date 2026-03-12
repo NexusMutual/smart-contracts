9cd5b553e2157e5bd47bd83492f76e95884d99ae
Rocky Murdoch <rockymurdoch.tech@gmail.com>
2026-03-12 14:23:20 +0200

    chore: drop unused Mock contract files

--- contracts/mocks/modules/MemberRoles/MRMockGovernance.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: GPL-3.0-only
-
-pragma solidity ^0.8.18;
-
-import "../../generic/GovernanceGeneric.sol";
-
-contract MRMockGovernance is GovernanceGeneric {
-  function removeDelegation(address) public {}
-}

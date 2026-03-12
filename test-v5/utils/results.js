28b06ed4bf8fdbb2c22d130ffced30a0b8064f11
Rocky Murdoch <rockymurdoch.tech@gmail.com>
2026-03-12 14:21:16 +0200

    chore: drop test-v5 dir

--- test-v5/utils/results.js
+++
@@ -1,8 +0,0 @@
-const isNumeric = k => k === (+k).toString();
-
-const resultAsObject = result => {
-  const entries = Object.entries(result).filter(([k]) => !isNumeric(k));
-  return Object.fromEntries(entries);
-};
-
-module.exports = { resultAsObject };
